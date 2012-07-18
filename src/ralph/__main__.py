bfcdd224535898d1fb81c2b1eff33da05b01e916
Łukasz Langa <lukasz@langa.pl>
2012-07-18 10:30:04 +0200

    use an absolute path for the module name so that subprocesses can be spawned

--- src/ralph/__main__.py
+++ src/ralph/__main__.py
@@ -7,7 +7,7 @@ def main():
 
     from django.core.management import execute_from_command_line
 
-    sys.argv[0] = 'ralph'
+    sys.argv[0] = os.path.dirname(__file__)
     execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
