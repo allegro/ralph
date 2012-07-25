b2b4b13ccb778185c5175fc7ef4255a47333cd6f
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-07-25 10:58:45 +0200

    Further split discovery.tests into modules for particular plugins

---
+++ src/ralph/discovery/tests/plugins/__init__.py
@@ -0,0 +1,7 @@
+from ralph.discovery.tests.plugins.ipmi_tests import IpmiPluginTest
+from ralph.discovery.tests.plugins.http_tests import HttpPluginTest, HttpSupermicroPluginTest
+from ralph.discovery.tests.plugins.ilohp_tests import IloHpPluginTest
+from ralph.discovery.tests.plugins.hpoa_tests import HpOaPluginTest
+from ralph.discovery.tests.plugins.snmp_tests import SnmpPluginTest, SnmpMacPluginTest
+from ralph.discovery.tests.plugins.ssg_tests import SshSsgPluginTest
+from ralph.discovery.tests.plugins.aix_tests import SshAixPluginTest
