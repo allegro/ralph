4daa687aa7b257c78f1575f55f95eae831c55b32
Marcin Kliks <marcin.kliks@allegro.pl>
2012-07-31 11:11:05 +0200

    Plugins reworked for Ralph celery/chains mechanism

--- src/ralph/cmdb/management/commands/cmdb_integration.py
+++ src/ralph/cmdb/management/commands/cmdb_integration.py
@@ -20,36 +20,40 @@ class Command(BaseCommand):
     help = textwrap.dedent(__doc__).strip()
     requires_model_validation = True
 
+    def get_cmdb_plugins(self):
+        return dict([(x,plugin.BY_NAME[x]) for x in plugin.BY_NAME.keys() if x.startswith('cmdb')])
+
     def __init__(self, *args, **kwargs):
-        required_plugin_name = re.compile('[^-]+')
-        cmdb_plugins = plugin.BY_NAME['cmdb'].iteritems()
         self.option_list = []
         self.option_list.extend(BaseCommand.option_list)
-        for p in cmdb_plugins:
+        for p in self.get_cmdb_plugins().iteritems():
             plugin_name = p[0]
-            if not required_plugin_name.match(plugin_name):
-                raise UserWarning("Invalid plugin name: %s " % plugin_name)
             self.option_list.append(make_option('--%s' % plugin_name,
                 dest="%s" % plugin_name,
                 action="store_true",
                 help="Trigger %s plugin" % plugin_name,
                 default=False
             ))
+        self.option_list.append(make_option('--remote',
+            dest="remote",
+            action="store_true",
+            help="Runs on remote worker",
+            default=False
+        ))
 
     def handle(self, *args, **options):
-        cmdb_plugins = plugin.BY_NAME['cmdb'].iteritems()
         specified_option = False
         if options.get('remote'):
             run = run_chain.delay
         else:
             run = partial(run_chain, interactive=True, clear_down=False)
-        context = {}
-        for p in cmdb_plugins:
+        context = {'context' : ''}
+        for p in self.get_cmdb_plugins().iteritems():
             if options.get(p[0]):
-                func_name = p[1]
-                logger.debug('Executing %s plugin.' % p[0])
-                run(context, func_name)
-                logger.debug('Finished executing %s plugin.' % p[0])
+                chain_name=p[0]
+                logger.debug('Executing %s chain .' % p[0])
+                run(context, chain_name)
+                logger.debug('Finished executing %s chain.' % p[0])
                 specified_option = True
         if not specified_option:
             print('Please specify option. ')
