0ef473fda9caece5055de1d784dd7a3740e20530
Łukasz Langa <lukasz@langa.pl>
2012-08-02 21:29:09 +0200

    updated fabfile in contrib

--- contrib/fabconf.py
+++ contrib/fabconf.py
@@ -10,6 +10,9 @@ from __future__ import unicode_literals
 PROD_HOSTS = ('ralph01.prod', 'ralph04.prod')
 DEV_HOSTS = ('ralph02.dev', 'ralph03.dev')
 
+PROJECT_DIR = '/home/ralph/project'
+SITE_PACKAGES_DIR = '/home/ralph/lib/python2.7/site-packages'
+
 def PIP_INSTALL(env):
     if env.host.endswith('.dev'):
         return 'pip install --proxy=proxy.dev:8000 -e .'
