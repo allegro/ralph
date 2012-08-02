beabc15b4ca54b91b12dc94316e055ec1f344481
Łukasz Langa <lukasz@langa.pl>
2012-08-02 21:29:09 +0200

    updated fabfile in contrib

--- contrib/fabfile.py
+++ contrib/fabfile.py
@@ -6,10 +6,13 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+
 from fabric import colors
 from fabric.api import cd, get, env, local, run
 from fabric.context_managers import hide
 from fabric.decorators import hosts, parallel
+from fabric.operations import put
 from fabric.utils import abort
 
 import fabconf
@@ -22,16 +25,16 @@ def _reset_title():
 
 def _git (command):
     if command == 'pull':
-        local('git push ssh://ralph@{}/home/ralph/project master:master'
-                ''.format(env.host))
-        with cd('/home/ralph/project/'):
+        local('git push ssh://ralph@{}{} master:master'
+                ''.format(env.host, fabconf.PROJECT_DIR))
+        with cd(fabconf.PROJECT_DIR):
             run('git checkout -f'.format(command))
             if hasattr(fabconf, 'PIP_INSTALL'):
                 run(fabconf.PIP_INSTALL(env))
             else:
                 run('pip install -e .')
     elif command == 'status':
-        with cd('/home/ralph/project/'):
+        with cd(fabconf.PROJECT_DIR):
             run('git {}'.format(command))
     else:
         abort(colors.red('Invalid remote git command.'))
@@ -66,7 +69,7 @@ def _celery(command):
         return
     if command not in ('start', 'stop', 'restart', 'kill', 'status'):
         abort(colors.red('Invalid celeryd command.'))
-    with cd('/home/ralph/project/src/ralph'):
+    with cd(fabconf.PROJECT_DIR):
         run('/etc/init.d/celeryd {}'.format(command))
     _reset_title()
 
@@ -87,3 +90,27 @@ def celery_dev(command):
 @hosts(*fabconf.DEV_HOSTS)
 def pcelery_dev(command):
     _celery(command)
+
+def _patch(path):
+    filename = os.path.split(path)[1] or path
+    put(path, '/tmp/' + filename)
+    with cd(fabconf.SITE_PACKAGES_DIR):
+        run('patch -p2 < /tmp/' + filename)
+
+@hosts(*fabconf.PROD_HOSTS)
+def patch_prod(command):
+    _patch(command)
+
+@parallel
+@hosts(*fabconf.PROD_HOSTS)
+def ppatch_prod(command):
+    _patch(command)
+
+@hosts(*fabconf.DEV_HOSTS)
+def patch_dev(command):
+    _patch(command)
+
+@parallel
+@hosts(*fabconf.DEV_HOSTS)
+def ppatch_dev(command):
+    _patch(command)
