266697059619151a95922f48195344183096dbaf
Marcin Kliks <marcin.kliks@allegro.pl>
2012-08-13 11:30:37 +0200

    Change commit hook to async, and removed old zabbix code

--- src/ralph/cmdb/rest/rest.py
+++ src/ralph/cmdb/rest/rest.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.views.decorators.csrf import csrf_exempt
-import ralph.cmdb.models as db
-from ralph.util.views import jsonify
 import os
+from django.views.decorators.csrf import csrf_exempt
+from ralph.util.views import jsonify
+from ralph.util import plugin
 
 from ralph.cmdb.integration.puppet import PuppetAgentsImporter
 from ralph.cmdb.integration.puppet import PuppetGitImporter
+from ralph.discovery.tasks import run_chain
+import ralph.cmdb.models as db
 
 """ Web hooks from Jira lands here. """
+
 
 def unique_file(file_name):
     counter = 1
@@ -34,32 +37,15 @@ def notify_puppet_agent(request):
 @csrf_exempt
 @jsonify
 def notify_zabbix(request):
-    from cmdb.integration.zabbix import  get_all_hosts
-    hosts = get_all_hosts()
-    found = 0
-    notfound = 0
-    notfound_list = []
-    for host in hosts:
-        hostname = host.get('name')
-        hid = host.get('hostid') + "-"
-        objs = db.CI.objects.filter(uid__contains = hostname).all()
-        if objs:
-            objs = objs[0]
-            objs[0].zabbix_id = hid
-            objs.save()
-            found +=1
-        else:
-            notfound +=1
-            notfound_list.append(hostname)
-
-    print "Found: %s" % found
-    print "Not found: %s" % notfound
-    print ','.join(notfound_list)
+    # to be continued
+    pass
 
 @csrf_exempt
 @jsonify
 def commit_hook(request):
-    p = PuppetGitImporter()
-    p.import_git()
-    return dict(status='ok')
+    context={'context': ''}
+    #deferred run
+    run = run_chain.delay
+    run(context, 'cmdb_git')
+    return dict(status='Queued.')
 
