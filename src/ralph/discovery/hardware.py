3ef5a1e1bbc0bc010ffc8984b189232f50c3a288
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-08-21 13:12:51 +0200

    Move smbios parsing out of the puppet plugin

--- src/ralph/discovery/hardware.py
+++ src/ralph/discovery/hardware.py
@@ -6,8 +6,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import hashlib
+import re
+import zlib
+
+from ralph.util import units
+from ralph.discovery.models import (Memory, Processor, ComponentModel,
+                                    ComponentType)
+
+
 
 SMBIOS_BANNER = 'ID    SIZE TYPE'
+DENSE_SPEED_REGEX = re.compile(r'(\d+)\s*([GgHhKkMmZz]+)')
 
 
 def normalize_wwn(wwn):
@@ -57,8 +67,7 @@ def normalize_wwn(wwn):
         raise ValueError('Unknown WWN format %r' % wwn)
     return wwn
 
-
-def smbios(as_string):
+def parse_smbios(as_string):
     if not as_string.startswith(SMBIOS_BANNER):
         raise ValueError("Incompatible SMBIOS answer.")
     smb = {}
@@ -81,7 +90,87 @@ def smbios(as_string):
                 current[key.strip()] = value.strip()
             else:
                 current.setdefault('capabilities', []).append(line)
-    return smb, as_string
+    return smb
+
+def handle_smbios(dev, smbios, is_virtual=False, priority=0):
+    # memory
+    for memory in smbios.get('MEMDEVICE', ()):
+        try:
+            size, size_units = memory.get('Size', '').split(' ', 1)
+            size = int(size)
+            size /= units.size_divisor[size_units]
+            size = int(size)
+        except ValueError:
+            continue # empty slot
+        for split_key in ('BANK', 'Slot '):
+            try:
+                bank = memory.get('Bank Locator').split(split_key)[1]
+                bank = int(bank) + 1
+                break
+            except (IndexError, ValueError):
+                bank = None # unknown bank
+        if bank is None:
+            continue
+        mem, created = Memory.concurrent_get_or_create(device=dev, index=bank)
+        if created:
+            mem.speed = 0
+        mem.label = "{} {}".format(memory.get('Device Locator',
+            memory.get('Location Tag', 'DIMM')), memory.get('Part Number', ''))
+        mem.size = size
+        manufacturer = memory.get('Manufacturer', 'Manufacturer')
+        if not manufacturer.startswith('Manufacturer'):
+            mem.label = manufacturer + ' ' + mem.label
+        family = 'Virtual' if is_virtual else ''
+        mem.model, c = ComponentModel.concurrent_get_or_create(
+            size=mem.size, speed=0, type=ComponentType.memory.id,
+            family=family, extra_hash='')
+        name = 'RAM %dMiB' % mem.size
+        if family:
+            name = '%s %s' % (family, name)
+        mem.model.name = name
+        mem.model.save(priority=priority)
+        mem.save(priority=priority)
+    # CPUs
+    detected_cpus = {}
+    for cpu in smbios.get('PROCESSOR', ()):
+        m = DENSE_SPEED_REGEX.match(cpu.get('Maximum Speed', ''))
+        if not m:
+            continue
+        if 'enabled' not in cpu.get('Processor Status', ''):
+            continue
+        speed = int(m.group(1))
+        speed_units = m.group(2)
+        speed /= units.speed_divisor[speed_units]
+        speed = int(speed)
+        label = cpu['Location Tag']
+        family = cpu['Family']
+        if '(other)' in family:
+            family = cpu['Manufacturer']
+        index_parts = []
+        for cpu_part in cpu['Location Tag'].replace('CPU', '').split():
+            try:
+                index_parts.append(int(cpu_part.strip()))
+            except ValueError:
+                continue
+        index = reduce(lambda x, y: x*y, index_parts)
+        extra = "CPUID: {}".format(cpu['CPUID'])
+        model, c = ComponentModel.concurrent_get_or_create(
+            speed=speed, type=ComponentType.processor.id, extra=extra,
+            extra_hash=hashlib.md5(extra).hexdigest(), family=family,
+            cores=0)
+        model.name = " ".join(cpu.get('Version', family).split())
+        model.save(priority=priority)
+        detected_cpus[index] = label, model
+    for cpu in dev.processor_set.all():
+        label, model = detected_cpus.get(cpu.index, (None, None))
+        if cpu.label != label or cpu.model != model:
+            cpu.delete()
+    for index, (label, model) in detected_cpus.iteritems():
+        cpu, created = Processor.concurrent_get_or_create(
+            device=dev, index=index)
+        cpu.label = label
+        cpu.model = model
+        cpu.save(priority=priority)
 
 
 def get_disk_shares(ssh):
