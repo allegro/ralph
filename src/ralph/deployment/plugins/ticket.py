43680f98d8a43789a38919c7f60741c41de91757
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-08-31 08:37:22 +0200

    Fix the ticket deployment plugin

--- src/ralph/deployment/plugins/ticket.py
+++ src/ralph/deployment/plugins/ticket.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from ralph.util import plugin
-from ralph.cmdb.inytegration.issuetracker_plugins.jira import (JiraAcceptance,
+from ralph.cmdb.integration.issuetracker_plugins.jira import (JiraAcceptance,
                                                                IntegrityError)
 from ralph.deployment.models import DeploymentStatus
 
