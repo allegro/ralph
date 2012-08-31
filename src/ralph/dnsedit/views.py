09abc133e7654d2e6986cce513289ccde13829f5
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-08-31 11:48:01 +0200

    Add an url for synchronizing DHCP servers

--- src/ralph/dnsedit/views.py
+++ src/ralph/dnsedit/views.py
@@ -4,7 +4,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import datetime
+
+from django.http import HttpResponse
+
 from ralph.ui.views.common import Base
+from ralph.dnsedit.models import DHCPServer
 
 class Index(Base):
     template_name = 'dnsedit/index.html'
@@ -12,3 +17,10 @@ class Index(Base):
 
     def __init__(self, *args, **kwargs):
         super(Index, self).__init__(*args, **kwargs)
+
+
+def dhcp_synch(request):
+    address = request.META['REMOTE_ADDR']
+    server = DHCPServer.get_or_create(ip=address)
+    server.last_synchronized = datetime.datetime.now()
+    return HttpResponse('OK', content_type='text/plain')
