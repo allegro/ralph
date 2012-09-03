e583638cba2d099ea36e616782920b114df4dedc
Łukasz Langa <lukasz@langa.pl>
2012-09-03 14:08:30 +0200

    default link fixed

--- src/ralph/urls.py
+++ src/ralph/urls.py
@@ -79,7 +79,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pxe/_(?P<file_type>[^/]+)$', 'ralph.deployment.views.preboot_type_view', name='preboot-type-view'),
     url(r'^pxe/(?P<file_name>[^_][^/]+)$', 'ralph.deployment.views.preboot_raw_view', name='preboot-raw-view'),
-    url(r'^pxe/$', 'ralph.deployment.views.preboot_raw_view', name='preboot-default-view', kwargs={'file_name': 'boot'}),
+    url(r'^pxe/$', 'ralph.deployment.views.preboot_type_view', name='preboot-default-view', kwargs={'file_type': 'boot_ipxe'}),
     url(r'^pxe/DONE/$', 'ralph.deployment.views.preboot_complete_view', name='preboot-complete-view'),
 
     # include the lookup urls
