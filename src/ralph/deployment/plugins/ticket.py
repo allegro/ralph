7c81ba463e9932ed644f262a86976cbf07955ef9
Marcin Kliks <marcin.kliks@allegro.pl>
2012-09-03 16:55:13 +0200

    Moved issue registration into the ticket plugin

--- src/ralph/deployment/plugins/ticket.py
+++ src/ralph/deployment/plugins/ticket.py
@@ -7,21 +7,21 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from ralph.util import plugin
-from ralph.cmdb.integration.issuetracker_plugins.jira import (JiraAcceptance,
-                                                              IntegrityError)
+from ralph.cmdb.integration.issuetracker import IssueTracker
 from ralph.deployment.models import DeploymentStatus, Deployment
 
 @plugin.register(chain='deployment', requires=[], priority=100)
 def ticket(deployment):
-    try:
-        if JiraAcceptance().deployment_accepted(deployment):
-            deployment.status = DeploymentStatus.in_progress.id
-            deployment.save()
-            obj_id = deployment.id
-            x = Deployment.objects.get(id=obj_id)
-            x.status = DeploymentStatus.in_deployment.id
-            x.save()
-            return True
-    except IntegrityError:
-        pass
+    issue_tracker = IssueTracker()
+    if not deployment.issue_key:
+        deployment.fire_issue()
+        return True
+    if issue_tracker.deployment_accepted(deployment):
+        deployment.status = DeploymentStatus.in_progress.id
+        deployment.save()
+        obj_id = deployment.id
+        x = Deployment.objects.get(id=obj_id)
+        x.status = DeploymentStatus.in_deployment.id
+        x.save()
+        return True
     return False
