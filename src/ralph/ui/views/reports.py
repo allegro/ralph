173c424f1302e5e66f6a1abb5e6aeedcaf3fa983
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-09-05 11:24:05 +0200

    A report of costs of all ventures in the Reports main menu.

--- src/ralph/ui/views/reports.py
+++ src/ralph/ui/views/reports.py
@@ -7,15 +7,24 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import datetime
+import cStringIO as StringIO
+
+from django.db import models as db
+from django.http import HttpResponseForbidden, HttpResponse
+from django.conf import settings
 
 from bob.menu import MenuItem
 from dj.choices import Choices
-from django.template.defaultfilters import slugify
 
 from ralph.account.models import Perm
 from ralph.deployment.models import DeploymentStatus
 from ralph.ui.views.common import Base, DeviceDetailView
 from ralph.ui.views.devices import DEVICE_SORT_COLUMNS
+from ralph.ui.forms import DateRangeForm
+from ralph.business.models import Venture
+from ralph.discovery.models_history import HistoryCost
+from ralph.ui.reports import total_cost_count
+from ralph.util import csvutil
 
 
 def threshold(days):
@@ -100,37 +109,102 @@ class Reports(DeviceDetailView):
         return result
 
 class SidebarReports(object):
-    def __init__(self, *args, **kwargs):
-        super(SidebarReports, self).__init__(*args, **kwargs)
-        self.report_name = ''
-
-    def set_report(self):
-        self.report_name = self.kwargs.get('report')
+    section = 'reports'
+    subsection = ''
 
     def get_context_data(self, **kwargs):
-        self.set_report()
         context = super(SidebarReports, self).get_context_data(**kwargs)
         sidebar_items = [
-            MenuItem("Unknown", name='', fugue_icon='fugue-prohibition',
-                     view_name='reports', view_args=[self.report_name, '', ''])
+            MenuItem("Ventures", fugue_icon='fugue-store',
+                     view_name='reports_ventures')
         ]
         context.update({
             'sidebar_items': sidebar_items,
-            'sidebar_selected': slugify(self.report_name),
+            'sidebar_selected': self.subsection,
             'section': 'reports',
-            'subsection': slugify(self.report_name),
+            'subsection': self.subsection,
         })
         return context
 
 
+class ReportVentures(SidebarReports, Base):
+    template_name = 'ui/report_ventures.html'
+    subsection = 'ventures'
 
-class ReportList(SidebarReports, Base):
-    section = 'reports'
-    template_name = 'ui/report_list.html'
+    def export_csv(self):
+        def iter_rows():
+            yield [
+                'Venture',
+                'Path',
+                'Department',
+                'Default margin',
+                'Total cost'
+            ]
+            for venture in self.ventures:
+                total = venture.total or 0
+                yield [
+                    venture.name,
+                    venture.path,
+                    unicode(venture.department) if venture.department else '',
+                    ('%d%%' % venture.margin_kind.margin
+                        ) if venture.margin_kind else '',
+                    '{:,.2f} {}'.format(total, settings.CURRENCY).replace(',', ' '),
+                ]
+        f = StringIO.StringIO()
+        csvutil.UnicodeWriter(f).writerows(iter_rows())
+        response = HttpResponse(f.getvalue(), content_type='application/csv')
+        response['Content-Disposition'] = 'attachment; filename=ventures.csv'
+        return response
+
+    def get(self, *args, **kwargs):
+        profile = self.request.user.get_profile()
+        has_perm = profile.has_perm
+        if not has_perm(Perm.read_device_info_reports):
+            return HttpResponseForbidden(
+                    "You don't have permission to see reports.")
+        if 'start' in self.request.GET:
+            self.form = DateRangeForm(self.request.GET)
+        else:
+            self.form = DateRangeForm(initial={
+                'start': datetime.date.today() - datetime.timedelta(days=30),
+                'end': datetime.date.today(),
+            })
+        if self.form.is_valid():
+            self.ventures = profile.perm_ventures(
+                    Perm.read_device_info_reports
+                ).filter(
+                    db.Q(parent=None) |
+                    db.Q(parent__parent=None),
+                    show_in_ralph=True
+                ).order_by('path')
+            for venture in self.ventures:
+                (venture.total, venture.count,
+                 venture.count_now) = total_cost_count(
+                         HistoryCost.objects.filter(
+                             db.Q(venture=venture) |
+                             db.Q(venture__parent=venture) |
+                             db.Q(venture__parent__parent=venture) |
+                             db.Q(venture__parent__parent__parent=venture) |
+                             db.Q(venture__parent__parent__parent__parent=venture)
+                         ).distinct(),
+                         self.form.cleaned_data['start'],
+                         self.form.cleaned_data['end'],
+                    )
+        else:
+            self.ventures = Venture.objects.none()
+        if self.request.GET.get('export') == 'csv':
+            return self.export_csv()
+        return super(ReportVentures, self).get(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        result = super(ReportList, self).get_context_data(**kwargs)
-        return result
+        context = super(ReportVentures, self).get_context_data(**kwargs)
+        context.update({
+            'form': self.form,
+            'ventures': self.ventures,
+            'profile': self.request.user.get_profile(),
+        })
+        return context
+
 
 class ReportDeviceList(object):
     template_name = 'ui/device_report_list.html'
