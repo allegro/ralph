faefe4e28f403bd79449c264f72c4ae995c05281
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-09-10 12:23:08 +0200

    fix import,

--- src/ralph/discovery/models_device.py
+++ src/ralph/discovery/models_device.py
@@ -8,7 +8,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from dateutil.relativedelta import *
+from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
 import re
 
