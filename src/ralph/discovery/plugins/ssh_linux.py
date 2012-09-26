d86a9d38d39b86f73333a401a6cd7551f25e27b4
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-09-26 17:36:32 +0200

    Make the ssh_linux plugin work for xen too

--- src/ralph/discovery/plugins/ssh_linux.py
+++ src/ralph/discovery/plugins/ssh_linux.py
@@ -28,11 +28,13 @@ logger = logging.getLogger(__name__)
 def run_ssh_linux(ssh, ip):
     # Create the device
     stdin, stdout, stderr = ssh.exec_command(
-            "/usr/bin/sudo /usr/sbin/ip addr show | /bin/grep 'link/ether'")
+            "/sbin/ip addr show | /bin/grep 'link/ether'")
     ethernets = [
         Eth(label='', mac=line.split(None, 3)[1], speed=0)
         for line in stdout
     ]
+    if not ethernets:
+        return ''
     dev = Device.create(ethernets=ethernets, model_name='Linux',
                         model_type=DeviceType.unknown)
     dev.save(update_last_seen=True, priority=SAVE_PRIORITY)
@@ -47,20 +49,25 @@ def run_ssh_linux(ssh, ip):
         if not mount.volume:
             mount.volume = lv
         mount.save()
-    for ds in dev.disksharemount_set(
+    for ds in dev.disksharemount_set.filter(
             is_virtual=False).exclude(share__wwn__in=wwns):
         ds.delete()
     # Handle smbios data
     stdin, stdout, stderr = ssh.exec_command(
-            "/usb/bin/sudo /usr/sbin/smbios")
-    smb = parse_smbios(stdin.read())
-    handle_smbios(dev, smb, priority=SAVE_PRIORITY)
+            "/usr/sbin/smbios")
+    try:
+        smb = parse_smbios(stdin.read())
+    except IOError:
+        pass
+    else:
+        handle_smbios(dev, smb, priority=SAVE_PRIORITY)
+    return dev.name
 
 
 @plugin.register(chain='discovery', requires=['ping', 'snmp'])
 def ssh_linux(**kwargs):
     kwargs['guessmodel'] = gvendor, gmodel = guessmodel.guessmodel(**kwargs)
-    if gmodel not in {'Linux', 'ESX'}:
+    if gmodel not in {'Linux', 'ESX', 'XEN'}:
         return False, 'no match: %s %s' % (gvendor, gmodel), kwargs
     ip = str(kwargs['ip'])
     if not network.check_tcp_port(ip, 22):
