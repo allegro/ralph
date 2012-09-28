f600f49539bfcce4e901d7899751f998b711f709
Marcin Kliks <marcin.kliks@allegro.pl>
2012-09-28 12:27:00 +0200

    Added processors information and minor review fixes

--- src/ralph/discovery/tests/plugins/system_x_tests.py
+++ src/ralph/discovery/tests/plugins/system_x_tests.py
@@ -7,26 +7,27 @@ from __future__ import unicode_literals
 
 from mock import patch
 from django.test import TestCase
-from django.conf import settings
 
-from ralph.discovery.models import Memory, Ethernet, Device
+from ralph.discovery.models import Memory, Ethernet, Device, Processor
 from ralph.discovery.plugins.http_ibm_system_x import (
-         _get_model_name, _get_sn, _get_memory,
-        _get_mac_addresses, _run_http_ibm_system_x
+    get_model_name, get_sn, get_memory,
+    get_mac_addresses, run_http_ibm_system_x,
+    get_processors
 )
 from ralph.discovery.tests.plugins.samples import http_ibm_system_x as data
 
 
-
 def _patched_send_soap(post_url, session_id, message):
-    if message.find('http://www.ibm.com/iBMC/sp/Monitors/GetVitalProductData')>=0:
+    if 'http://www.ibm.com/iBMC/sp/Monitors/GetVitalProductData' in message:
         return data.generic_data_response
-    elif message.find('http://www.ibm.com/iBMC/sp/iBMCControl/GetSPNameSettings')>=0:
+    elif 'http://www.ibm.com/iBMC/sp/iBMCControl/GetSPNameSettings' in message:
         return data.sn_response
-    elif message.find('http://www.ibm.com/iBMC/sp/Monitors/GetMemoryInfo')>=0:
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetMemoryInfo' in message:
         return data.memory_response
-    elif message.find('http://www.ibm.com/iBMC/sp/Monitors/GetHostMacAddresses')>=0:
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetHostMacAddresses' in message:
         return data.macs_response
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetProcessorInfo' in message:
+        return data.processors_response
     else:
         raise Exception('Unknown message type')
 
@@ -34,25 +35,28 @@ def _patched_send_soap(post_url, session_id, message):
 def _patched_get_session_id(ip):
     return '65c77c6b-af39-4901-8fef-2dec8c6dd24d'
 
+_patched_USER = 'test'
+_patched_PASSWORD = 'test'
+
 
 class SystemXPluginTest(TestCase):
     """ IBM System X Test case """
     def setUp(self):
-        settings.IBM_SYSTEM_X_USER = 'test'
-        settings.IBM_SYSTEM_X_PASSWORD = 'test'
         self.ip = '10.10.10.10'
         self.session_id = '123'
 
-    @patch('ralph.discovery.plugins.http_ibm_system_x._get_session_id', _patched_get_session_id)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.USER', _patched_USER)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.PASSWORD', _patched_PASSWORD)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.get_session_id', _patched_get_session_id)
     @patch('ralph.discovery.plugins.http_ibm_system_x._send_soap', _patched_send_soap)
     def test_import(self):
-        """ Try to save into the database. Check for fields required.
-        """
-        _run_http_ibm_system_x(self.ip)
+        """ Try to save into the database. Check for fields required."""
+        run_http_ibm_system_x(self.ip)
         dev = Device.objects.get(model__name='System x3550 M3')
         self.assertEqual(Memory.objects.filter(device=dev).count(), 8)
-        self.assertEqual([(x.model.name, x.label, x.size)
-            for x in Memory.objects.filter(device=dev).order_by('-id')],
+        self.assertEqual(
+            [(x.model.name, x.label, x.size) for x in Memory.objects.filter(
+                device=dev).order_by('-id')],
             [(u'RAM DIMM 18 4096MiB', u'DIMM 18', 4096),
                 (u'RAM DIMM 15 8192MiB', u'DIMM 15', 8192),
                 (u'RAM DIMM 12 8192MiB', u'DIMM 12', 8192),
@@ -61,22 +65,25 @@ class SystemXPluginTest(TestCase):
                 (u'RAM DIMM 6 8192MiB', u'DIMM 6', 8192),
                 (u'RAM DIMM 3 8192MiB', u'DIMM 3', 8192),
                 (u'RAM DIMM 2 4096MiB', u'DIMM 2', 4096)])
-        self.assertEqual([(x.label, x.mac)
-            for x in Ethernet.objects.filter(device=dev).order_by('-id')],
+        self.assertEqual(
+            [(x.label, x.mac) for x in Ethernet.objects.filter(
+                device=dev).order_by('-id')],
             [(u'Host Ethernet MAC Address 2', u'6EF3DDE59642'),
-            (u'Host Ethernet MAC Address 1', u'6EF3DDE59640')]
+             (u'Host Ethernet MAC Address 1', u'6EF3DDE59640')]
         )
+        self.assertEqual(Processor.objects.filter(device=dev).count(), 2)
 
-    @patch('ralph.discovery.plugins.http_ibm_system_x._get_session_id', _patched_get_session_id)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.USER', _patched_USER)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.PASSWORD', _patched_PASSWORD)
+    @patch('ralph.discovery.plugins.http_ibm_system_x.get_session_id', _patched_get_session_id)
     @patch('ralph.discovery.plugins.http_ibm_system_x._send_soap', _patched_send_soap)
     def test_soap_methods(self):
-        """ Check return values of particular soap methods.
-        """
-        model_name = _get_model_name(self.ip, self.session_id)
+        """ Check return values of particular soap methods."""
+        model_name = get_model_name(self.ip, self.session_id)
         self.assertEqual(model_name, 'System x3550 M3')
-        sn = _get_sn(self.ip, self.session_id)
+        sn = get_sn(self.ip, self.session_id)
         self.assertEqual(sn, 'SN# KD55ARA')
-        memory = _get_memory(self.ip, self.session_id)
+        memory = get_memory(self.ip, self.session_id)
         self.assertEqual(memory, [
             {'index': 2, 'size': 4096, 'label': 'DIMM 2'},
             {'index': 3, 'size': 8192, 'label': 'DIMM 3'},
@@ -87,7 +94,21 @@ class SystemXPluginTest(TestCase):
             {'index': 15, 'size': 8192, 'label': 'DIMM 15'},
             {'index': 18, 'size': 4096, 'label': 'DIMM 18'}
         ])
-        macs = _get_mac_addresses(self.ip, self.session_id)
-        self.assertEqual(macs, [['Host Ethernet MAC Address 1', '6E:F3:DD:E5:96:40'],
-            ['Host Ethernet MAC Address 2', '6E:F3:DD:E5:96:42']])
+        macs = get_mac_addresses(self.ip, self.session_id)
+        self.assertEqual(macs, [
+            ['Host Ethernet MAC Address 1', '6E:F3:DD:E5:96:40'],
+            ['Host Ethernet MAC Address 2', '6E:F3:DD:E5:96:42']]
+        )
+        detected_processors = get_processors(
+            self.ip, self.session_id
+        )
+        self.assertEqual(
+            detected_processors,
+            [{'cores': '8',
+                'label': u'Intel Xeon CPU 2666 MHz, 8 cores 1 threads',
+                'speed': '2666', 'family': 'Intel Xeon', 'index': '1'},
+                {'cores': '8', 'label': u'Intel Xeon CPU 2666 MHz, 8 cores 1 threads',
+                    'speed': '2666', 'family': 'Intel Xeon', 'index': '2'}]
+        )
+
 
