8afaac2b285450a87e5b438bbbf19be6aa53c90f
vi4m <marcin.kliks@gmail.com>
2012-10-01 17:38:03 +0200

    JSON uploading to the server implemented

--- contrib/donpedro/DonPedro/Logger.cs
+++ contrib/donpedro/DonPedro/Logger.cs
@@ -4,52 +4,60 @@ using System.Reflection;
 
 namespace DonPedro
 {
-	public class Logger : IDisposable
-    {
-        private StreamWriter sw;
-        public Logger()
-        {
-        	String path = Path.Combine(
-        		Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
-        		"logs.txt"
-        	);
-            sw = new StreamWriter(path, true);
-        }
- 
-        public void Log(string text)
-        {
+	public sealed class Logger :IDisposable
+	{
+		private static String path = Path.Combine(
+				Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
+				"logs.txt"
+		);
+		private static StreamWriter sw = new StreamWriter(path, true); 
+		private static readonly Logger instance = new Logger();
+		
+		public static Logger Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+		
+		public void Log(string text)
+		{
 			#if DEBUG
-            Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
+			Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
 			#endif
-            sw.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
-            sw.Flush();
-        }
- 
-        public void LogDebug(string text)
-        {
+			sw.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
+			sw.Flush();
+		}
+		
+		public void LogDebug(string text)
+		{
 			#if DEBUG
-            sw.WriteLine(string.Format("[{0}] [debug] {1}", DateTime.Now, text));
+			sw.WriteLine(string.Format("[{0}] [debug] {1}", DateTime.Now, text));
+			Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
 			#endif
 			sw.Flush();
-        }
- 
-        public void LogError(string text)
-        {
-            sw.WriteLine(string.Format("[{0}] [error] {1}", DateTime.Now, text));
-            sw.Flush();
-        }
- 
-        public void LogFatal(string text)
-        {
-            sw.WriteLine(string.Format("[{0}] [fatal] {1}", DateTime.Now, text));
-            sw.Flush();
-        }
- 
-        public void Dispose()
-        {
-            sw.Flush();
-            sw.Close();
-            sw.Dispose();
-        }
- 	}
+		}
+		
+		public void LogError(string text)
+		{
+			sw.WriteLine(string.Format("[{0}] [error] {1}", DateTime.Now, text));
+			Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
+			sw.Flush();
+		}
+		
+		public void LogFatal(string text)
+		{
+			sw.WriteLine(string.Format("[{0}] [fatal] {1}", DateTime.Now, text));
+			Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, text));
+			sw.Flush();
+		}
+		
+		public void Dispose()
+		{
+			sw.Flush();
+			sw.Close();
+			sw.Dispose();
+		}
+	}
 }
--- contrib/donpedro/DonPedro/Rest.cs
+++ contrib/donpedro/DonPedro/Rest.cs
@@ -14,38 +14,38 @@ namespace DonPedro
 		
 		public string Post(string url, string json_data)
 		{
-		  HttpWebRequest req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
-		  req.Method = "POST";  
-		  req.ContentType = "application/json";
-		  StringBuilder s = new StringBuilder();
-		  byte[] formData = UTF8Encoding.UTF8.GetBytes(json_data.ToString());
-		  req.ContentLength = formData.Length;
-		  using (Stream post = req.GetRequestStream())  
-		  {
-		    post.Write(formData, 0, formData.Length);  
-		  }
-		  string result = null;
-		  using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)  
-		  {  
-		    StreamReader reader = new StreamReader(resp.GetResponseStream());
-		    result = reader.ReadToEnd();
-		  }
-		  return result;
+			HttpWebRequest req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
+			req.Method = "POST";
+			req.ContentType = "application/json";
+			StringBuilder s = new StringBuilder();
+			byte[] formData = UTF8Encoding.UTF8.GetBytes(json_data.ToString());
+			req.ContentLength = formData.Length;
+			using (Stream post = req.GetRequestStream())
+			{
+				post.Write(formData, 0, formData.Length);
+			}
+			string result = null;
+			using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+			{
+				StreamReader reader = new StreamReader(resp.GetResponseStream());
+				result = reader.ReadToEnd();
+			}
+			return result;
 		}
 		
 		public string Get(string url)
 		{
-		  	HttpWebRequest req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
-		  	req.Method = "GET";  
-		  	req.ContentType = "application/json";
-		  	StringBuilder s = new StringBuilder();
-		  	string result = null;
-		  	using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)  
-		 	 {  
-		  	  StreamReader reader = new StreamReader(resp.GetResponseStream());
-		  	  result = reader.ReadToEnd();
-		 	 }
-			 return result;
+			HttpWebRequest req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
+			req.Method = "GET";
+			req.ContentType = "application/json";
+			StringBuilder s = new StringBuilder();
+			string result = null;
+			using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+			{
+				StreamReader reader = new StreamReader(resp.GetResponseStream());
+				result = reader.ReadToEnd();
+			}
+			return result;
 		}
 	}
 }
