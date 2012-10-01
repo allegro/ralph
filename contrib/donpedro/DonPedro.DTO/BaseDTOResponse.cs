df307b70266674b663fbf29add475f05a1711eb1
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2012-10-01 08:46:52 +0200

    ToJson method in DTO objects.

--- contrib/donpedro/DonPedro.DTO/BaseDTOResponse.cs
+++ contrib/donpedro/DonPedro.DTO/BaseDTOResponse.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections;
 
 namespace DonPedro.DTO
 {
 	public abstract class BaseDTOResponse
 	{
 		public string Label { get; set; }
+
+		public string ToJson()
+		{
+			ArrayList parts = new ArrayList();
+
+			foreach (var property in this.GetType().GetProperties())
+			{
+				parts.Add(
+					string.Format(
+						"\"{0}\":\"{1}\"", property.Name, property.GetValue(this, null)
+					)
+				);
+			}
+
+			return "{" + String.Join(",", (String[]) parts.ToArray(typeof(string))) + "}";
+		}
 	}
 }
