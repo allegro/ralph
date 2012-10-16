f0eb91fb6c5118bf452cd597152832a4bc771473
Marcin Kliks <marcin.kliks@allegro.pl>
2012-10-16 17:03:34 +0200

    Lame migration is less lame now

--- src/ralph/business/migrations/0006_auto__del_ventureowner__add_field_venturerole_path.py
+++ src/ralph/business/migrations/0006_auto__del_ventureowner__add_field_venturerole_path.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+            ("cmdb", "0007_auto__add_ciowner__add_ciownership.py"),
+    )
+
     def forwards(self, orm):
         # Deleting model 'VentureOwner'
         db.delete_table('business_ventureowner')
