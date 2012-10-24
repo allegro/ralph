c2322bf969097be8bd38bd2c96fb3fd6a9288f33
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-10-24 14:59:55 +0200

    tests for search

--- src/ralph/ui/tests.py
+++ src/ralph/ui/tests.py
@@ -10,177 +10,298 @@ from django.test import TestCase
 from django.test.client import Client
 from ralph.business.models import Venture, VentureRole
 from ralph.discovery.models import Device, DeviceType
-from ralph.discovery.models_component import Ethernet, DiskShare
+from ralph.discovery.models_component import (
+    Ethernet, DiskShare, GenericComponent, Processor, Software, ComponentModel,
+    DiskShareMount, Memory, FibreChannel, SplunkUsage, Storage, OperatingSystem
+)
 from ralph.discovery.models_history import HistoryChange
 from ralph.discovery.models_network import (
     IPAddress, NetworkTerminator, Network, DataCenter)
 
-DEVICE_NAME = 'SimpleDevice'
-DEVICE_IP = '10.0.0.1'
-DEVICE_REMARKS = 'Very important device'
-
-DEVICE_VENTURE = 'SimpleVenture'
-DEVICE_VENTURE_SYMBOL = 'simple_venture'
-VENTURE_ROLE = 'VentureRole'
-DEVICE_POSITION = '12'
-DEVICE_RACK = '13'
-DEVICE_BARCODE = 'bc_dev'
-DEVICE_SN = '0000000001'
-DEVICE_MAC = '00:00:00:00:00:00'
-
-DISKSHARE_DEVICE = 'DiskShareSrv'
-DISKSHARE_DEVICE_SN = '0000000002'
-DISKSHARE_DEVICE_BARCODE = 'bc_share'
-WWN = 'DiskShareWWN'
-
-NETWORK_NAME = 'SimpleNetwork'
-NETWORK_ADDRESS = '10.0.0.1/26'
+DEVICE = {
+    'name': 'SimpleDevice',
+    'ip': '10.0.0.1',
+    'remarks': 'Very important device',
+    'venture': 'SimpleVenture',
+    'ventureSymbol': 'simple_venture',
+    'venture_role': 'VentureRole',
+    'model_name': 'xxxx',
+    'position': '12',
+    'rack': '14',
+    'barcode': 'bc_dev',
+    'sn': '0000000001',
+    'mac': '00:00:00:00:00:00',
+}
+DISKSHARE = {
+    'device': 'DiskShareSrv',
+    'sn': '0000000002',
+    'barcode': 'bc_share',
+    'wwn': 'DiskShareWWN',
+}
+NETWORK = {
+    'name': 'SimpleNetwork',
+    'address': '10.0.0.1/26',
+}
 DATACENTER = 'dc1'
 
+COMPONENT = {
+    'GenericComponent': '__GenericComponent*',
+    'DiskShareMount': '__DiskShareMount*',
+    'Processor': '__Processor*',
+    'Memory': '__Memory*',
+    'Storage': '__Storage*',
+    'Fibre': '__Fibre*',
+    'Software': '__Ralph*',
+    'OS': '__Linux*'
+}
 
 class TestSearch(TestCase):
+    """
+    TODO:
+    1. when search return more than 1 result
+    2. verification objects in html
+    """
     def setUp(self):
         login = 'ralph'
         password = 'ralph'
-        user = User.objects.create_user(login, 'ralph@ralph.local', password)
-        self.user = user
+        self.user = User.objects.create_user(
+            login, 'ralph@ralph.local', password
+        )
         self.user.is_staff = True
         self.user.is_superuser = True
         self.user.save()
         self.client = Client()
         self.client.login(username=login, password=password)
         venture = Venture(
-            name=DEVICE_VENTURE, symbol=DEVICE_VENTURE_SYMBOL
+            name=DEVICE['venture'], symbol=DEVICE['ventureSymbol']
         )
         venture.save()
         self.venture = venture
-        venture_role = VentureRole(name=VENTURE_ROLE, venture=self.venture)
+        venture_role = VentureRole(
+            name=DEVICE['venture_role'], venture=self.venture
+        )
         venture_role.save()
         self.venture_role = venture_role
         self.device = Device.create(
-            sn=DEVICE_SN,
-            barcode=DEVICE_BARCODE,
-            remarks=DEVICE_REMARKS,
-            model_name='xxxx',
+            sn=DEVICE['sn'],
+            barcode=DEVICE['barcode'],
+            remarks=DEVICE['remarks'],
+            model_name=DEVICE['model_name'],
             model_type=DeviceType.unknown,
             venture=self.venture,
             venture_role=self.venture_role,
-            rack=DEVICE_RACK,
-            position=DEVICE_POSITION,
+            rack=DEVICE['rack'],
+            position=DEVICE['position'],
             dc=DATACENTER,
         )
-        self.device.name = DEVICE_NAME
+        self.device.name = DEVICE['name']
         self.device.save()
-        self.db_device = Device.objects.get(name=self.device.name)
-        self.ip = IPAddress(address=DEVICE_IP, device=self.device)
+        self.ip = IPAddress(address=DEVICE['ip'], device=self.device)
         self.ip.save()
-        self.db_ip = IPAddress.objects.get(address=DEVICE_IP)
+        self.db_ip = IPAddress.objects.get(address=DEVICE['ip'])
         self.network_terminator = NetworkTerminator(name='simple_terminator')
         self.network_terminator.save()
         self.network_datacenter = DataCenter(name=DATACENTER)
         self.network_datacenter.save()
         self.network = Network(
-            name=NETWORK_NAME,
-            address=NETWORK_ADDRESS,
+            name=NETWORK['name'],
+            address=NETWORK['address'],
             data_center=self.network_datacenter,
         )
-        self.ethernet = Ethernet.concurrent_get_or_create(
-            device=self.device, mac=DEVICE_MAC)
-
         self.diskshare_device = Device.create(
-            sn=DISKSHARE_DEVICE_SN,
-            barcode=DISKSHARE_DEVICE_BARCODE,
+            sn=DISKSHARE['sn'],
+            barcode=DISKSHARE['barcode'],
             model_name='xxx',
             model_type=DeviceType.storage,
         )
-        self.diskshare_device.name = DISKSHARE_DEVICE
+        self.diskshare_device.name = DISKSHARE['device']
         self.diskshare_device.save()
-
-        self.diskshare = DiskShare(share_id=self.diskshare_device, wwn=WWN)
-        self.device_list = self.client.get('/ui/search/info/', follow=True)
-        device_url = ('/ui/search/info/%s?name=%s') % (self.device.id,
-                                                       self.db_device.name)
-        self.response = self.client.get(device_url, follow=True)
-        self.object = self.response.context['object']
+        self.cm_generic = ComponentModel(name='GenericModel')
+        self.cm_diskshare = ComponentModel(name='DiskShareModel')
+        self.cm_processor = ComponentModel(name='ProcessorModel')
+        self.cm_memory = ComponentModel(name='MemoryModel')
+        self.cm_storage = ComponentModel(name='ComponentModel')
+        self.cm_fibre = ComponentModel(name='FibreChannalMidel')
+        self.cm_ethernet = ComponentModel(name='EthernetMidel')
+        self.cm_software = ComponentModel(name='SoftwareModel')
+        self.cm_splunkusage = ComponentModel(name='SplunkusageModel')
+        self.cm_operatingsystem = ComponentModel(name='OperatingSystemModel')
+        self.generic_component = GenericComponent(
+            device=self.device,
+            model=self.cm_generic,
+            label=COMPONENT['GenericComponent'],
+        )
+        self.generic_component.save()
+        self.diskshare = DiskShare(
+            device=self.device,
+            model=self.cm_diskshare,
+            share_id=self.device.id,
+            size=80,
+            wwn=DISKSHARE['wwn'],
+        )
+        self.diskshare.save()
+        self.disksharemount = DiskShareMount.concurrent_get_or_create(
+            share=self.diskshare,
+            device=self.device,
+            volume=COMPONENT['DiskShareMount'],
+        )
+        self.processor = Processor(
+            device=self.device,
+            model=self.cm_processor,
+            label=COMPONENT['Processor'],
+        )
+        self.processor.save()
+        self.memory = Memory(
+            device=self.device,
+            model=self.cm_memory,
+            label=COMPONENT['Memory'],
+        )
+        self.memory.save()
+        self.storage = Storage(
+            device=self.device,
+            model=self.cm_storage,
+            label=COMPONENT['Storage'],
+        )
+        self.storage.save()
+        self.fibrechannel = FibreChannel(
+            device=self.device,
+            model=self.cm_fibre,
+            label=COMPONENT['Fibre'],
+            physical_id='01234',
+        )
+        self.fibrechannel.save()
+        self.ethernet = Ethernet(
+            model=self.cm_ethernet,
+            device=self.device,
+            mac=DEVICE['mac'],
+        )
+        self.ethernet.save()
+        self.software = Software(
+            device=self.device,
+            model=self.cm_software,
+            label=COMPONENT['Software'],
+        )
+        self.software.save()
+        self.splunkusage = SplunkUsage(
+            device=self.device,
+            model=self.cm_splunkusage,
+        )
+        self.splunkusage.save()
+        self.operatingsystem = OperatingSystem(
+            device=self.device,
+            model=self.cm_operatingsystem,
+            label=COMPONENT['OS'],
+        )
+        self.operatingsystem.save()
 
     def test_access_to_device(self):
-        self.assertEqual(self.device_list.status_code, 200)
-        self.assertEqual(self.response.status_code, 200)
+        #User has perm to device list and device details
+        device_list = self.client.get('/ui/search/info/', follow=True)
+        self.assertEqual(device_list.status_code, 200)
+        url = '/ui/search/info/%s' % self.device.id
+        device_details = self.client.get(url, follow=True)
+        self.assertEqual(device_details.status_code, 200)
 
-    def test_name_field(self):
-        self.assertEqual(self.db_device.name, DEVICE_NAME)
-        self.assertEqual(self.object.name, self.db_device.name)
-        self.assertEqual(self.object.barcode, self.db_device.barcode)
+    def test_name_field_old(self):
+        #Search objects in response.context
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(context.name, self.device.name)
 
     def test_address_or_network_field(self):
-        #datapase
-        self.assertEqual(self.db_ip.address, DEVICE_IP)
-        self.assertEqual(self.db_ip.device.name, DEVICE_NAME)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
         #test ip
-        self.assertEqual(self.object.name, self.db_device.name)
-        self.assertEqual(self.object.ipaddress_set.get().address, DEVICE_IP)
+        self.assertEqual(context.name, self.device.name)
+        ip = context.ipaddress_set.filter(address=DEVICE['ip']).count()
+        self.assertTrue(ip>0)
         #test network
         """
         FIXME - i can`t tests network, i need more info !
         """
 
     def test_remarks_field(self):
-        self.assertEqual(self.db_device.remarks, DEVICE_REMARKS)
-        self.assertEqual(self.object.remarks, DEVICE_REMARKS)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(context.remarks, DEVICE['remarks'])
 
     def test_venture_or_role_field(self):
-        self.assertEqual(self.db_device.venture.name, DEVICE_VENTURE)
-        self.assertEqual(self.object.venture.name, DEVICE_VENTURE)
-        self.assertEqual(self.object.venture_role_id, self.venture_role.id)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(self.device.venture.name, DEVICE['venture'])
+        self.assertEqual(context.venture.name, DEVICE['venture'])
+        self.assertEqual(context.venture_role_id, self.venture_role.id)
+        """
+        FIXME: i can`t see venture_symbol
+        """
 
     def test_model_field(self):
-        self.assertEqual(self.db_device.model.type, DeviceType.unknown)
-        self.assertEqual(self.object.model.type, DeviceType.unknown)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(self.device.model.type, DeviceType.unknown)
+        self.assertEqual(context.model.type, DeviceType.unknown)
+        self.assertEqual(context.model.name, DEVICE['model_name'])
 
     def test_component_or_software_field(self):
-        """
-        FIXME - component id amd name
-        """
-        self.assertEqual('todo', 'done')
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        processor = context.processor_set.filter(device=self.device.id).count()
+        self.assertTrue(processor>0)
 
     def test_serial_number_mac_or_wwn_field(self):
-        self.assertEqual(self.device.sn, DEVICE_SN)
-        self.assertEqual(self.object.sn, DEVICE_SN)
-        mac = self.device.ethernet_set.get(device=self.device).mac
-        self.assertEqual(mac, DEVICE_MAC.replace(':', ''))
-        object_query = self.object.ethernet_set.get(device=self.device)
-        self.assertEqual(object_query.mac, DEVICE_MAC.replace(':', ''))
-        device_url = ('/ui/search/info/%s') % (self.diskshare.share_id.id)
-        response = self.client.get(device_url, follow=True)
-        object = response.context['object'].diskshare_set.instance.id
-#
-#        import pdb
-#        pdb.set_trace()
-#        self.assertEqual(self.device.diskshare, WWN)
-#        self.assertEqual(object, DEVICE_SN)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(self.device.sn, context.sn)
+        mac = context.ethernet_set.filter(mac=DEVICE['mac']).count()
+        self.assertTrue(mac>0)
+        """
+        FIXME: I can`t see any wwn (diskshare, disksharemount), generatic_sn
+        in template
+        """
 
     def test_barcode_field(self):
-        self.assertEqual(self.device.barcode, DEVICE_BARCODE)
-        self.assertEqual(self.object.barcode, DEVICE_BARCODE)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(self.device.barcode, DEVICE['barcode'])
+        self.assertEqual(context.barcode, DEVICE['barcode'])
 
     def test_datacenter_rack_or_position_field(self):
-        self.assertEqual(self.device.position, DEVICE_POSITION)
-        self.assertEqual(self.object.position, DEVICE_POSITION)
-        self.assertEqual(self.device.rack, DEVICE_RACK)
-        self.assertEqual(self.object.rack, DEVICE_RACK)
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
+        self.assertEqual(self.device.position, DEVICE['position'])
+        self.assertEqual(context.position, DEVICE['position'])
+        self.assertEqual(self.device.rack, DEVICE['rack'])
+        self.assertEqual(context.rack, DEVICE['rack'])
         self.assertEqual(self.device.dc, DATACENTER)
-        self.assertEqual(self.object.dc, DATACENTER)
+        self.assertEqual(context.dc, DATACENTER)
 
     def test_history_field(self):
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
         db_query = HistoryChange.objects.get(
-            device=self.device, new_value=DEVICE_NAME,
+            device=self.device, new_value=DEVICE['name'],
         )
-        object_query = self.object.historychange_set.get(
-            device=self.device, new_value=DEVICE_NAME,
+        object_query = context.historychange_set.get(
+            device=self.device, new_value=DEVICE['name'],
         )
-        self.assertEqual(db_query.new_value, DEVICE_NAME)
-        self.assertEqual(object_query.new_value, DEVICE_NAME)
+        self.assertEqual(db_query.old_value, '')
+        self.assertEqual(db_query.new_value, DEVICE['name'])
+        self.assertEqual(object_query.old_value, '')
+        self.assertEqual(object_query.new_value, DEVICE['name'])
 
     def test_device_type_field(self):
+        url = '/ui/search/info/%s' % self.device.id
+        device_search = self.client.get(url,follow=True)
+        context = device_search.context['object']
         self.assertEqual(self.device.model.type, DeviceType.unknown.id)
-        self.assertEqual(self.object.model.type, DeviceType.unknown)
+        self.assertEqual(context.model.type, DeviceType.unknown)
