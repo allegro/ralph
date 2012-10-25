1366d1a04d1688f99c44de7b77fbb05ec9517f6e
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-10-25 13:34:42 +0200

    fixed Unassigned device count

--- src/ralph/ui/views/catalog.py
+++ src/ralph/ui/views/catalog.py
@@ -224,11 +224,15 @@ class CatalogDevice(Catalog):
         else:
             self.query = DeviceModel.objects.filter(
                     type=self.model_type_id).filter(group=None)
-        self.unassigned_count = DeviceModel.objects.filter(
-                type=self.model_type_id
-            ).filter(
-                group=None
-            ).count()
+        unassigned_device = DeviceModel.objects.filter(
+            type=self.model_type_id
+        ).filter(
+            group=None
+        )
+        unassigned_count = 0
+        for u in unassigned_device:
+            unassigned_count = unassigned_count + u.get_count()
+        self.unassigned_count = unassigned_count
         self.groups = list(DeviceModelGroup.objects.filter(
             type=self.model_type_id))
         for g in self.groups:
