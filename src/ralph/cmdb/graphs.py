77e41cce8039ac9833deaec701004a3d6e7cf22e
Marcin Kliks <marcin.kliks@gmail.com>
2012-10-26 18:42:31 +0200

    Graphing introduced

--- src/ralph/cmdb/graphs.py
+++ src/ralph/cmdb/graphs.py
@@ -6,7 +6,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from django.utils import simplejson
+
 from ralph.cmdb.views import BaseCMDBView
+from ralph.cmdb.models import CI, CIRelation
+from django.http import HttpResponse
 
 class Graphs(BaseCMDBView):
     template_name = 'cmdb/graphs.html'
@@ -17,3 +21,17 @@ class Graphs(BaseCMDBView):
         })
         return ret
 
+    @staticmethod
+    def get_ajax(self):
+        relations = [ dict(parent=x.parent.id, child=x.child.id, parent_name=x.parent.name, child_name=x.child.name)
+                for x in CIRelation.objects.filter(parent__type=2)]
+        nodes = dict()
+        for x in relations:
+            nodes[x.get('parent')] = x.get('parent_name')
+            nodes[x.get('child')] = x.get('child_name')
+
+        response_dict = dict(nodes=nodes.items(), relations=relations)
+        return HttpResponse(
+            simplejson.dumps(response_dict),
+            mimetype='application/json',
+        )
