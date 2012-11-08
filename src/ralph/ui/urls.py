47e0df1521b43687d78834bc1cc603482e9a6e41
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-11-08 14:20:55 +0100

    Report - services (with/without Venture - CMDB)

--- src/ralph/ui/urls.py
+++ src/ralph/ui/urls.py
@@ -37,7 +37,9 @@ from ralph.ui.views.catalog import (Catalog, CatalogDevice,
 from ralph.ui.views.deploy import Deployment
 from ralph.ui.views.ventures import VenturesDeviceList, VenturesCMDB
 from ralph.ui.views.racks import RacksDeviceList
-from ralph.ui.views.reports import ReportVentures, ReportMargins
+from ralph.ui.views.reports import (ReportVentures, ReportMargins,
+        ReportServices
+        )
 
 
 urlpatterns = patterns('',
@@ -184,6 +186,7 @@ urlpatterns = patterns('',
     url(r'^catalog/(?P<kind>component)/(?P<type>\d*)/(?P<group>\d*)/$', login_required(CatalogComponent.as_view()), {}, 'catalog'),
 
     url(r'^reports/$', login_required(ReportVentures.as_view()), {}, 'reports'),
+    url(r'^reports/services/$', login_required(ReportServices.as_view()), {}, 'reports_services'),
     url(r'^reports/ventures/$', login_required(ReportVentures.as_view()), {}, 'reports_ventures'),
     url(r'^reports/margins/$', login_required(ReportMargins.as_view()), {}, 'reports_margins'),
 )
