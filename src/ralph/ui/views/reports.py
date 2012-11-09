94a0e353782a225f5cd0a0b976bb20355e62b21a
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-11-09 13:00:31 +0100

    pep8

--- src/ralph/ui/views/reports.py
+++ src/ralph/ui/views/reports.py
@@ -18,12 +18,16 @@ from dj.choices import Choices
 
 from ralph.account.models import Perm
 from ralph.business.models import Venture
-from ralph.cmdb.models_ci import CI, CIRelation, CI_STATE_TYPES, CI_RELATION_TYPES
+from ralph.cmdb.models_ci import (
+    CI, CIRelation, CI_STATE_TYPES, CI_RELATION_TYPES
+)
 from ralph.deployment.models import DeploymentStatus
 from ralph.discovery.models_device import MarginKind, DeviceType
 from ralph.discovery.models_history import HistoryCost
 from ralph.ui.forms import DateRangeForm, MarginsReportForm
-from ralph.ui.reports import get_total_cost, get_total_count, get_total_cores, get_total_virtual_cores
+from ralph.ui.reports import (
+    get_total_cost, get_total_count, get_total_cores, get_total_virtual_cores
+)
 from ralph.ui.views.common import Base, DeviceDetailView
 from ralph.ui.views.devices import DEVICE_SORT_COLUMNS
 from ralph.util import csvutil
@@ -37,102 +41,126 @@ class ReportType(Choices):
     _ = Choices.Choice
 
     no_ping1 = _('No ping since 1 day').extra(
-            filter=lambda device_list: device_list.filter(
-                ipaddress__last_seen__lte=threshold(-1)),
-            columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
-            )
+        filter=lambda device_list: device_list.filter(
+            ipaddress__last_seen__lte=threshold(-1)),
+        columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
+    )
     no_ping3 = _('No ping since 3 days').extra(
-            filter=lambda device_list: device_list.filter(
-                ipaddress__last_seen__lte=threshold(-3)),
-            columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
-            )
+        filter=lambda device_list: device_list.filter(
+            ipaddress__last_seen__lte=threshold(-3)),
+        columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
+    )
     no_ping7 = _('No ping since 7 days').extra(
-            filter=lambda device_list: device_list.filter(
-                ipaddress__last_seen__lte=threshold(-7)),
-            columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
-            )
+        filter=lambda device_list: device_list.filter(
+            ipaddress__last_seen__lte=threshold(-7)),
+        columns=['venture', 'position', 'lastseen', 'remarks', 'lastping'],
+    )
     no_purchase_date = _('No purchase date').extra(
-            filter=lambda device_list: device_list.filter(
-                purchase_date=None),
-            columns=['venture', 'position', 'barcode', 'cost', 'price',
-                'remarks'],
-            )
+        filter=lambda device_list: device_list.filter(
+            purchase_date=None),
+        columns=['venture', 'position', 'barcode', 'cost', 'price', 'remarks'],
+    )
     no_venture_role = _('No venture and role').extra(
-            filter=lambda device_list: device_list.filter(
-                venture_role=None),
-            columns=['venture', 'position', 'barcode', 'cost', 'lastseen',
-                'remarks'],
-            )
+        filter=lambda device_list: device_list.filter(
+            venture_role=None),
+        columns=[
+            'venture', 'position', 'barcode', 'cost', 'lastseen', 'remarks'
+        ],
+    )
     deactivated_support = _('Deactivated support').extra(
         filter=lambda device_list: device_list.filter(
-            support_expiration_date__lte= datetime.date.today()),
+            support_expiration_date__lte=datetime.date.today()
+        ),
         columns=['venture', 'model', 'position', 'barcode',
                  'serial_number', 'remarks', 'support'],
     )
     support_expires30 = _('Support expires in 30 days').extra(
-            filter=lambda device_list: device_list.filter(
-                support_expiration_date__lte=threshold(30)),
-            columns=['venture', 'model', 'position', 'barcode',
-                     'serial_number', 'remarks', 'support'],
-            )
+        filter=lambda device_list: device_list.filter(
+            support_expiration_date__lte=threshold(30)),
+        columns=[
+            'venture', 'model', 'position', 'barcode', 'serial_number',
+            'remarks', 'support'
+        ],
+    )
     support_expires60 = _('Support expires in 60 days').extra(
-            filter=lambda device_list: device_list.filter(
-                support_expiration_date__lte=threshold(60)),
-            columns=['venture', 'model', 'position', 'barcode',
-                     'serial_number', 'remarks', 'support'],
-            )
+        filter=lambda device_list: device_list.filter(
+            support_expiration_date__lte=threshold(60)),
+        columns=[
+            'venture', 'model', 'position', 'barcode', 'serial_number',
+            'remarks', 'support'
+        ],
+    )
     support_expires90 = _('Support expires in 90 days').extra(
-            filter=lambda device_list: device_list.filter(
-                support_expiration_date__lte=threshold(90)),
-            columns=['venture', 'model', 'position', 'barcode',
-                     'serial_number', 'remarks', 'support'],
-            )
+        filter=lambda device_list: device_list.filter(
+            support_expiration_date__lte=threshold(90)),
+        columns=['venture', 'model', 'position', 'barcode',
+                 'serial_number', 'remarks', 'support'],
+    )
     verified = _('Verified venture and role').extra(
-            filter=lambda device_list: device_list.filter(verified=True),
-            columns=['venture', 'remarks', 'barcode', 'serial_number']
-            )
+        filter=lambda device_list: device_list.filter(verified=True),
+        columns=['venture', 'remarks', 'barcode', 'serial_number']
+    )
     deployment_open = _('Deployment open').extra(
-            filter=lambda device_list: device_list.filter(
-                deployment__status=DeploymentStatus.open),
-                columns=['venture', 'remarks', 'position', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deployment__status=DeploymentStatus.open
+        ),
+        columns=['venture', 'remarks', 'position', 'barcode'],
+    )
     deployment_in_progress = _('Deployment in progress').extra(
-            filter=lambda device_list: device_list.filter(
-                deployment__status=DeploymentStatus.in_progress),
-                columns=['venture', 'remarks', 'position', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deployment__status=DeploymentStatus.in_progress
+        ),
+        columns=['venture', 'remarks', 'position', 'barcode']
+    )
     deployment_running = _('Deployment running').extra(
-            filter=lambda device_list: device_list.filter(
-                deployment__status=DeploymentStatus.in_deployment),
-                columns=['venture', 'remarks', 'position', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deployment__status=DeploymentStatus.in_deployment
+        ),
+        columns=['venture', 'remarks', 'position', 'barcode']
+    )
     deprecation_devices = _('Deprecation devices').extra(
-            filter=lambda device_list: device_list.filter(
-                deprecation_date__lte = datetime.date.today()),
-                columns=['venture', 'purchase', 'deprecation',
-                         'deprecation_date', 'remarks', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deprecation_date__lte=datetime.date.today()
+        ),
+        columns=[
+            'venture', 'purchase', 'deprecation', 'deprecation_date',
+            'remarks', 'barcode'
+        ]
+    )
     deprecation_devices30 = _('Deprecation devices in 30').extra(
-            filter=lambda device_list: device_list.filter(
-                deprecation_date__lte = threshold(30)).filter(
-                    deprecation_date__gte=datetime.date.today()),
-                columns=['venture', 'purchase', 'deprecation',
-                         'deprecation_date','remarks', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deprecation_date__lte=threshold(30)
+        ).filter(
+            deprecation_date__gte=datetime.date.today()
+        ),
+        columns=[
+            'venture', 'purchase', 'deprecation', 'deprecation_date',
+            'remarks', 'barcode'
+        ]
+    )
     deprecation_devices60 = _('Deprecation devices in 60').extra(
-            filter=lambda device_list: device_list.filter(
-                deprecation_date__lte = threshold(60)).filter(
-                    deprecation_date__gte=datetime.date.today()),
-                columns=['venture', 'purchase', 'deprecation',
-                         'deprecation_date','remarks', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deprecation_date__lte=threshold(60)
+        ).filter(
+            deprecation_date__gte=datetime.date.today()
+        ),
+        columns=[
+            'venture', 'purchase', 'deprecation', 'deprecation_date',
+            'remarks', 'barcode'
+        ]
+    )
     deprecation_devices90 = _('Deprecation devices in 90').extra(
-            filter=lambda device_list: device_list.filter(
-                    deprecation_date__lte = threshold(90)).filter(
-                deprecation_date__gte=datetime.date.today()),
-                columns=['venture', 'purchase', 'deprecation',
-                         'deprecation_date', 'remarks', 'barcode']
-            )
+        filter=lambda device_list: device_list.filter(
+            deprecation_date__lte=threshold(90)
+        ).filter(
+            deprecation_date__gte=datetime.date.today()
+        ),
+        columns=[
+            'venture', 'purchase', 'deprecation', 'deprecation_date',
+            'remarks', 'barcode'
+        ]
+    )
+
 
 class Reports(DeviceDetailView):
     template_name = 'ui/device_reports.html'
@@ -141,6 +169,7 @@ class Reports(DeviceDetailView):
     def get_context_data(self, **kwargs):
         result = super(Reports, self).get_context_data(**kwargs)
         return result
+
 
 class SidebarReports(object):
     section = 'reports'
@@ -183,7 +212,8 @@ class ReportMargins(SidebarReports, Base):
         has_perm = profile.has_perm
         if not has_perm(Perm.read_device_info_reports):
             return HttpResponseForbidden(
-                    "You don't have permission to see reports.")
+                "You don't have permission to see reports."
+            )
         self.margin_kinds = MarginKind.objects.all()
         if 'start' in self.request.GET:
             self.form = MarginsReportForm(self.margin_kinds, self.request.GET)
@@ -201,7 +231,8 @@ class ReportMargins(SidebarReports, Base):
         context = super(ReportMargins, self).get_context_data(**kwargs)
         if self.form.is_valid():
             venture = Venture.objects.get(
-                    id=self.form.cleaned_data['margin_venture'])
+                id=self.form.cleaned_data['margin_venture']
+            )
             query = HistoryCost.objects.filter(
                 db.Q(venture=venture) |
                 db.Q(venture__parent=venture) |
@@ -215,8 +246,9 @@ class ReportMargins(SidebarReports, Base):
             start = self.form.cleaned_data['start']
             end = self.form.cleaned_data['end']
             for mk in self.margin_kinds:
-                q = query.filter(db.Q(device__margin_kind=mk) |
-                     db.Q(
+                q = query.filter(
+                    db.Q(device__margin_kind=mk) |
+                    db.Q(
                         db.Q(device__margin_kind=None) &
                         db.Q(
                             db.Q(device__venture__margin_kind=mk) |
@@ -225,19 +257,24 @@ class ReportMargins(SidebarReports, Base):
                             db.Q(device__venture__margin_kind=None,
                                  device__venture__parent__margin_kind=None,
                                  device__venture__parent__parent__margin_kind=mk) |
-                            db.Q(device__venture__margin_kind=None,
-                                 device__venture__parent__margin_kind=None,
-                                 device__venture__parent__parent__margin_kind=None,
-                        device__venture__parent__parent__parent__margin_kind=mk)
+                            db.Q(
+                                device__venture__margin_kind=None,
+                                device__venture__parent__margin_kind=None,
+                                device__venture__parent__parent__margin_kind=None,
+                                device__venture__parent__parent__parent__margin_kind=mk
+                            )
                         )
                     )
                 )
                 mk.total = get_total_cost(q, start, end)
-                mk.count, mk.count_now, devices = get_total_count(q, start, end)
+                mk.count, mk.count_now, devices = get_total_count(
+                    q, start, end
+                )
                 mk.sim_margin = self.form.get('m_%d' % mk.id, 0) or 0
-                mk.sim_cost = ((mk.total or 0) /
-                               (1 + mk.margin/100) *
-                               (1 + mk.sim_margin/100))
+                mk.sim_cost = (
+                    (mk.total or 0) / (1 + mk.margin / 100)
+                    * (1 + mk.sim_margin / 100)
+                )
                 total_sim += mk.sim_cost
                 total_cost += mk.total or 0
                 total_count += mk.count or 0
@@ -250,9 +287,9 @@ class ReportMargins(SidebarReports, Base):
         context.update({
             'form': self.form,
             'margin_kinds': self.margin_kinds,
-            'zip_margin_kinds_form': zip([f for f in self.form if
-                                          not f.label],
-                                          self.margin_kinds),
+            'zip_margin_kinds_form': zip(
+                [f for f in self.form if not f.label], self.margin_kinds
+            ),
         })
         return context
 
@@ -301,7 +338,8 @@ class ReportVentures(SidebarReports, Base):
         has_perm = profile.has_perm
         if not has_perm(Perm.read_device_info_reports):
             return HttpResponseForbidden(
-                    "You don't have permission to see reports.")
+                "You don't have permission to see reports."
+            )
         if 'start' in self.request.GET:
             self.form = DateRangeForm(self.request.GET)
         else:
@@ -311,17 +349,19 @@ class ReportVentures(SidebarReports, Base):
             })
         if self.form.is_valid():
             self.ventures = profile.perm_ventures(
-                    Perm.read_device_info_reports
-                ).filter(
-                    db.Q(parent=None) |
-                    db.Q(parent__parent=None),
-                    show_in_ralph=True
-                ).order_by('path')
+                Perm.read_device_info_reports
+            ).filter(
+                db.Q(parent=None) |
+                db.Q(parent__parent=None),
+                show_in_ralph=True
+            ).order_by('path')
             start = self.form.cleaned_data['start']
             end = self.form.cleaned_data['end']
-            total_cloud_cost = get_total_cost(HistoryCost.objects.filter(
+            total_cloud_cost = get_total_cost(
+                HistoryCost.objects.filter(
                     device__model__type=DeviceType.cloud_server.id
-                ), start, end)
+                ), start, end
+            )
             for venture in self.ventures:
                 query = HistoryCost.objects.filter(
                     db.Q(venture=venture) |
@@ -334,10 +374,14 @@ class ReportVentures(SidebarReports, Base):
                 (venture.count, venture.count_now,
                  devices) = get_total_count(query, start, end)
                 venture.core_count = get_total_cores(devices, start, end)
-                venture.virtual_core_count = get_total_virtual_cores(devices, start, end)
-                cloud_cost = get_total_cost(query.filter(
+                venture.virtual_core_count = get_total_virtual_cores(
+                    devices, start, end
+                )
+                cloud_cost = get_total_cost(
+                    query.filter(
                         device__model__type=DeviceType.cloud_server.id
-                    ), start, end)
+                    ), start, end
+                )
                 venture.cloud_use = (cloud_cost or 0) / total_cloud_cost * 100
         else:
             self.ventures = Venture.objects.none()
@@ -385,12 +429,15 @@ class ReportServices(SidebarReports, Base):
 
     def get_context_data(self, **kwargs):
         context = super(ReportServices, self).get_context_data(**kwargs)
-        context.update({
-            'serv_with_ven': self.serv_with_ven,
-            'serv_without_ven': self.serv_without_ven,
-            'profile': self.request.user.get_profile(),
-            })
+        context.update(
+            {
+                'serv_with_ven': self.serv_with_ven,
+                'serv_without_ven': self.serv_without_ven,
+                'profile': self.request.user.get_profile(),
+            }
+        )
         return context
+
 
 class ReportDeviceList(object):
     template_name = 'ui/device_report_list.html'
@@ -403,8 +450,10 @@ class ReportDeviceList(object):
 
     def get_context_data(self, **kwargs):
         result = super(ReportDeviceList, self).get_context_data(**kwargs)
-        report_menu_items = (MenuItem(desc, href=name) for name, desc in
-            ReportType(item=lambda v: (v.name, v.desc)))
+        report_menu_items = (
+            MenuItem(desc, href=name) for name, desc in
+            ReportType(item=lambda v: (v.name, v.desc))
+        )
         report_type = self.get_report_type()
         result.update({
             'report_menu_items': report_menu_items,
