bb43785a1cc4719d84963c290d3808fd890a829d
Kamil Warguła <kwargula@gmail.com>
2012-11-14 12:23:19 +0100

    Modification CMDB API resources

--- src/ralph/cmdb/api.py
+++ src/ralph/cmdb/api.py
@@ -16,8 +16,10 @@ from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource as MResource
 from tastypie.throttle import CacheThrottle
 
-from ralph.cmdb.models import CI, CIRelation
+from ralph.cmdb.models import CI, CILayer, CIRelation, CIType
 from ralph.cmdb import models as db
+from ralph.cmdb.models_ci import CIOwner, CIOwnershipType
+from ralph.deployment.models import get_login_from_owner_name
 
 THROTTLE_AT = settings.API_THROTTLING['throttle_at']
 TIMEFREME = settings.API_THROTTLING['timeframe']
@@ -70,6 +72,64 @@ class CIResource(MResource):
         authentication = ApiKeyAuthentication()
         authorization = DjangoAuthorization()
         resource_name = 'ci'
+        filtering = {
+            'name': ('startswith', 'exact',),
+            'barcode': ('startswith', 'exact',),
+            'layers': ('exact',),
+            'pci_scope': ('exact',),
+            'type': ('exact',),
+            'technical_owners': ('exact',),
+            'busines_owners': ('exact',),
+        }
         throttle = CacheThrottle(throttle_at=THROTTLE_AT, timeframe=TIMEFREME,
-                                expiration=EXPIRATION)
+                                 expiration=EXPIRATION)
 
+    def dehydrate(self, bundle):
+        ci = CI.objects.get(uid=bundle.data.get('uid'))
+        technical_owners = CIOwner.objects.filter(
+            ciownership__type=CIOwnershipType.technical.id,
+            ci=ci
+        )
+        business_owners = CIOwner.objects.filter(
+            ciownership__type=CIOwnershipType.business.id,
+            ci=ci
+        )
+        bundle.data['type'] = {'name': ci.type.name, 'id': ci.type_id}
+        bundle.data['technical_owner'] = []
+        for technical_owner in technical_owners:
+            bundle.data['technical_owner'].append(
+                {'username': get_login_from_owner_name(technical_owner)}
+            )
+        bundle.data['bussiness_owner'] = []
+        for business_owner in business_owners:
+            bundle.data['technical_owner'].append(
+                {'username': get_login_from_owner_name(business_owner)}
+            )
+        bundle.data['layers'] = []
+        for layer in ci.layers.all():
+            bundle.data['layers'].append({'name': layer.name, 'id': layer.id})
+        return bundle
+
+
+class CILayersResource(MResource):
+    class Meta:
+        queryset = CILayer.objects.all()
+        authentication = ApiKeyAuthentication()
+        authorization = DjangoAuthorization()
+        resourse_name = 'cilayers'
+        list_allowed_methods = ['get']
+        excludes = ['cache_version', 'created', 'modified']
+        throttle = CacheThrottle(throttle_at=THROTTLE_AT, timeframe=TIMEFREME,
+                                 expiration=EXPIRATION)
+
+
+class CITypesResource(MResource):
+    class Meta:
+        queryset = CIType.objects.all()
+        authentication = ApiKeyAuthentication()
+        authorization = DjangoAuthorization()
+        resourse_name = 'citypes'
+        list_allowed_methods = ['get']
+        excludes = ['cache_version', 'created', 'modified']
+        throttle = CacheThrottle(throttle_at=THROTTLE_AT, timeframe=TIMEFREME,
+                                 expiration=EXPIRATION)
