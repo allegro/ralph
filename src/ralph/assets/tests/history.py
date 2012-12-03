ddbdec5ccc7027063eb1ab0d07dcbf41e684d18f
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-12-03 08:47:39 +0100

    Assets tests

--- src/ralph/assets/tests/history.py
+++ src/ralph/assets/tests/history.py
@@ -44,7 +44,7 @@ class HistoryAssetsView(TestCase):
             'warehouse': self.warehouse.id,
             'sn': '666-666-666',
             'barcode': '666666',
-        }
+            }
         self.asset_change_params = {
             'barcode': '777777',
             'status': AssetStatus.damaged.id,
@@ -55,7 +55,7 @@ class HistoryAssetsView(TestCase):
             'license_type': LicenseTypes.oem.id,
             'date_of_last_inventory': '2012-11-08',
             'last_logged_user': 'ralph',
-        }
+            }
         self.asset = None
         self.add_bo_device_asset()
         self.edit_bo_device_asset()
@@ -138,7 +138,7 @@ class ConnectAssetWithDevice(TestCase):
             'size': 1,
             'warehouse': self.warehouse.id,
             'barcode': '7777',
-        }
+            }
         self.asset = None
 
     def create_device(self):
