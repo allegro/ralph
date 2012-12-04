a2ab74adba77445e435917458e0294b4126fead6
Marcin Kliks <marcin.kliks@gmail.com>
2012-12-04 18:27:36 +0100

    idrac plugin initial.

--- src/ralph/discovery/plugins/idrac.py
+++ src/ralph/discovery/plugins/idrac.py
@@ -7,9 +7,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import subprocess
+import os
+import ssl
+from sys import stdout
+
 
 from django.conf import settings
 from ralph.util import plugin
+from tempfile import mkstemp
 
 
 IDRAC_USER = settings.IDRAC_USER
@@ -24,15 +29,21 @@ class IDRAC(object):
         self.password = password
 
     def _get_cert(self):
-        pass
+        return 'cert'
+        (handle, path) = mkstemp()
+        cert = ssl.get_server_certificate((self.host, 443))
+        os.write(handle, cert)
+        os.close(handle)
+        return path
 
     def _run_command(self, class_name, namespace):
         command = [
-            "wsman", "enumerate", "%s/%s" % (SCHEMA, class_name),
+            "wsman-emu", "enumerate", "%s/%s" % (SCHEMA, class_name),
             "-N", namespace, "-u", self.user, "-p", self.password,
-            "-h", self.host, "-P", 443, "-v", "-j", "urf-8",
-            "-y", "basic", "-o" "-m", 256, "-V", "-c", self._get_cert(),
+            "-h", self.host, "-P", '443', "-v", "-j", "urf-8",
+            "-y", "basic", "-o" "-m", '256', "-V", "-c", self._get_cert(),
         ]
+        print(' '.join(command))
         proc = subprocess.Popen(command, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
         out, err = proc.communicate()
@@ -49,7 +60,9 @@ class IDRAC(object):
 
 
 def _run_idrac(ip):
-    pass
+    x = IDRAC(host=ip)
+    cert_filename = x._get_cert()
+    output = x._run_command('DCIM_NICView', 'root/dcim')
 
 
 @plugin.register(chain='discovery', requires=['ping', 'http'])
@@ -58,7 +71,7 @@ def idrac(**kwargs):
         return False, "not configured", kwargs
     ip = str(kwargs['ip'])
     http_family = kwargs.get('http_family')
-    if http_family not in ('Mbedthis-Appweb', ):
+    if http_family not in ('Dell', ):
         return False, 'no match', kwargs
     name = _run_idrac(ip)
     return True, name, kwargs
