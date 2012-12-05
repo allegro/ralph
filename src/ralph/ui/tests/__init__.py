1c18b043f43c5a0d6466de5bccab55194117fa57
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-12-05 13:49:18 +0100

    remove selenium

--- src/ralph/ui/tests/__init__.py
+++ src/ralph/ui/tests/__init__.py
@@ -5,4 +5,3 @@ import ralph.ui.tests.selenium
 from ralph.ui.tests.reports import TestReportsServices
 from ralph.ui.tests.search import TestSearch
 from ralph.ui.tests.device import TestDeviceView
-from ralph.ui.tests.selenium import MySeleniumTests
