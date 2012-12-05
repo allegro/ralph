ee7bbee91894d7829963c730626ab2d0e49c2ffd
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-12-05 08:23:02 +0100

    add simple test in selenium.

--- src/ralph/ui/tests/__init__.py
+++ src/ralph/ui/tests/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import ralph.ui.tests.selenium
+
 from ralph.ui.tests.reports import TestReportsServices
 from ralph.ui.tests.search import TestSearch
 from ralph.ui.tests.device import TestDeviceView
+from ralph.ui.tests.selenium import MySeleniumTests
