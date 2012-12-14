50d1f6c27824015305cfa34667607ec23722a307
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2012-12-14 09:44:31 +0100

    get first free ip

--- src/ralph/deployment/management/commands/nextip.py
+++ src/ralph/deployment/management/commands/nextip.py
@@ -8,14 +8,18 @@ from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand, CommandError
 
-#from ralph.deployment.util import get_nexthostname
+from ralph.deployment.util import get_nextip
 
 
 class Command(BaseCommand):
     args = '<dc name>'
-    help = 'Return next ip name for specified network.'
+    help = 'Return next IP for specified network.'
 
     def handle(self, *args, **options):
         if not args:
             raise CommandError('Please specify the network name.')
-        pass
+        status, ip, error_msg = get_nextip(args[0])
+        if not status:
+            raise CommandError(error_msg)
+        self.stdout.write("First free IP: %s\n" % ip)
+
