6cfa6d8a3cac682953ad419baac7b8f5f812f59f
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-12-14 14:22:52 +0100

    sort imports

--- src/ralph/assets/tests/__init__.py
+++ src/ralph/assets/tests/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from ralph.assets.tests.functional.view import *
-from ralph.assets.tests.functional.search import *
-from ralph.assets.tests.functional.form_validation import *
+from ralph.assets.tests.functional.bulkedit import *
 from ralph.assets.tests.functional.crud import *
+from ralph.assets.tests.functional.form_validation import *
+from ralph.assets.tests.functional.multivalue_fields import *
+from ralph.assets.tests.functional.search import *
+from ralph.assets.tests.functional.view import *
