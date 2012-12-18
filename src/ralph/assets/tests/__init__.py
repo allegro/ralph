e1100c38a9130a3c43feaf1c6d5372ab19618f18
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2012-12-18 10:30:17 +0100

    delete temp fix (import tests)

--- src/ralph/assets/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from ralph.assets.tests.functional.bulkedit import *
-from ralph.assets.tests.functional.crud import *
-from ralph.assets.tests.functional.form_validation import *
-from ralph.assets.tests.functional.multivalue_fields import *
-from ralph.assets.tests.functional.search import *
-from ralph.assets.tests.functional.view import *
