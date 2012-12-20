017f668dc57532959fd4298f636cc849616d2ce3
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-12-20 14:21:05 +0100

    Show DHCPServer entries in Admin, don't create them automatically

--- src/ralph/dnsedit/admin.py
+++ src/ralph/dnsedit/admin.py
@@ -9,7 +9,7 @@ from django.contrib import admin
 
 from lck.django.common.admin import ModelAdmin
 
-from ralph.dnsedit.models import DHCPEntry, DNSHistory
+from ralph.dnsedit.models import DHCPEntry, DNSHistory, DHCPServer
 
 
 class DHCPEntryAdmin(ModelAdmin):
@@ -18,6 +18,14 @@ class DHCPEntryAdmin(ModelAdmin):
     save_on_top = True
 
 admin.site.register(DHCPEntry, DHCPEntryAdmin)
+
+
+class DHCPServerAdmin(ModelAdmin):
+    list_display = ('ip', 'last_synchronized')
+    search_fields = ('ip',)
+    save_on_top = True
+
+admin.site.register(DHCPServer, DHCPServerAdmin)
 
 
 class DNSHistoryAdmin(ModelAdmin):
