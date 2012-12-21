036982ccaf3be8031d2adaadf7f4db096c479089
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2012-12-21 10:42:59 +0100

    DOn't create the IPAddress in dns plugin after all -- it's created earlier

--- src/ralph/deployment/plugins/dns.py
+++ src/ralph/deployment/plugins/dns.py
@@ -9,18 +9,10 @@ from __future__ import unicode_literals
 from ralph.util import plugin
 from ralph.dnsedit.util import reset_dns
 from ralph.deployment.models import Deployment
-from ralph.discovery.models import IPAddress
 
 
 @plugin.register(chain='deployment', requires=[], priority=0)
 def dns(deployment_id):
     deployment = Deployment.objects.get(id=deployment_id)
     reset_dns(deployment.hostname, deployment.ip)
-    ipaddr, created = IPAddress.objects.concurrent_get_or_create(
-        address=deployment.ip
-    )
-    if created:
-        ipaddr.hostname = deployment.hostname
-    ipaddr.device = deployment.device
-    ipaddr.save()
     return True
