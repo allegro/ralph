659fad8794e968b8821242f5f6c53311f5a81c57
Łukasz Langa <lukasz@langa.pl>
2012-12-27 12:10:54 +0100

    unnecessary hashlib imports removed

--- src/ralph/discovery/plugins/hp_oa.py
+++ src/ralph/discovery/plugins/hp_oa.py
@@ -6,7 +6,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import hashlib
 from urllib2 import urlopen, URLError
 import httplib
 
--- src/ralph/discovery/plugins/idrac.py
+++ src/ralph/discovery/plugins/idrac.py
@@ -6,7 +6,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import hashlib
 import re
 import uuid
 
