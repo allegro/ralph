38d30157da45913ccb1901f51a6c32d3f600c44c
Kamil Warguła <kwargula@gmail.com>
2013-01-21 14:52:18 +0100

    Add error handling

--- src/ralph/dnsedit/management/commands/dnsappendrecords.py
+++ src/ralph/dnsedit/management/commands/dnsappendrecords.py
@@ -26,8 +26,19 @@ class DisallowedRecordType(Error):
     """Trying to create a record which is not a of allowed types."""
 
 
+class InadequateLengthRow(Error):
+    """Trying to unpack row."""
+
+
+class EmptyRecordValue(Error):
+    """Trying to create record from empty values."""
+
+
 class Command(BaseCommand):
-    """Import DNS records form csv file"""
+    """Append DNS records form csv file to existing Domain
+    record should be in this format:
+    name;type;content;domain name
+    """
 
     help = textwrap.dedent(__doc__).strip()
     requires_model_validation = True
@@ -39,7 +50,17 @@ class Command(BaseCommand):
     def handle_single(self, filename):
         print('Importing DNS records from {}...'.format(filename))
         with open(filename, 'rb') as f:
-            for name, type, content, domain_name in UnicodeReader(f):
+            for i, value in enumerate(UnicodeReader(f), 1):
+                if len(value) != 4:
+                    raise InadequateLengthRow(
+                        'CSV row {} have {} elements, should be 4'.format(
+                            i,
+                            len(value),
+                        )
+                    )
+                name, type, content, domain_name = value
+                if not all((name, type, content, domain_name)):
+                    raise EmptyRecordValue('Record fields can not be empty')
                 if type in RECORD_TYPES:
                     domain = self.get_domain(domain_name)
                     self.create_record(type, domain, name, content)
