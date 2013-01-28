2eed5a6a5ee9cf13c3c04c65977d61e797c1193f
Kamil Warguła <kwargula@gmail.com>
2013-01-28 11:59:04 +0100

    Review fixes for PR #407

--- src/ralph/dnsedit/management/commands/dnsappendrecords.py
+++ src/ralph/dnsedit/management/commands/dnsappendrecords.py
@@ -18,19 +18,15 @@ class Error(Exception):
     pass
 
 
-class DomainNotExists(Error):
-    """Trying to get Domain from name."""
-
-
-class DisallowedRecordType(Error):
+class DisallowedRecordTypeError(Error):
     """Trying to create a record which is not a of allowed types."""
 
 
-class InadequateLengthRow(Error):
+class IncorrectLengthRowError(Error):
     """Trying to unpack row."""
 
 
-class EmptyRecordValue(Error):
+class EmptyRecordValueError(Error):
     """Trying to create record from empty values."""
 
 
@@ -52,7 +48,7 @@ class Command(BaseCommand):
         with open(filename, 'rb') as f:
             for i, value in enumerate(UnicodeReader(f), 1):
                 if len(value) != 4:
-                    raise InadequateLengthRow(
+                    raise IncorrectLengthRowError(
                         'CSV row {} have {} elements, should be 4'.format(
                             i,
                             len(value),
@@ -60,24 +56,17 @@ class Command(BaseCommand):
                     )
                 name, type, content, domain_name = value
                 if not all((name, type, content, domain_name)):
-                    raise EmptyRecordValue('Record fields can not be empty')
+                    raise EmptyRecordValueError('Record fields can not be empty')
                 if type in RECORD_TYPES:
-                    domain = self.get_domain(domain_name)
+                    domain = Domain.objects.get(name=domain_name)
                     self.create_record(type, domain, name, content)
                 else:
-                    raise DisallowedRecordType(
+                    raise DisallowedRecordTypeError(
                         'Record type {} is not allowed. Use:\n {}'.format(
                             type,
                             RECORD_TYPES,
                         )
                     )
-
-    def get_domain(self, domain_name):
-        try:
-            domain = Domain.objects.get(name=domain_name)
-        except Domain.DoesNotExist:
-            raise DomainNotExists('Domain %s not found.' % domain_name)
-        return domain
 
     def create_record(self, type, domain, name, content):
         record, created = Record.objects.get_or_create(
@@ -86,20 +75,11 @@ class Command(BaseCommand):
             type=type,
             content=content,
         )
-        if created:
-            print('Record created: {} IN {} {} {}'.format(
-                record.name,
-                record.type,
-                record.content,
-                record.domain,
-            )
-            )
-        else:
-            print('Record exist: {} IN {} {} {}'.format(
-                record.name,
-                record.type,
-                record.content,
-                record.domain
-            )
-            )
+        print('Record {}: {} IN {} {} {}'.format(
+            'created' if created else 'exists',
+            record.name,
+            record.type,
+            record.content,
+            record.domain,
+        ))
         return created
