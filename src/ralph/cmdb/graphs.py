880ff1879a22a6a2217dfb8f4490138203143865
Marcin Kliks <marcin.kliks@gmail.com>
2013-01-30 15:16:45 +0100

    PR#418 fixes and improvements. Changed recursion to stack structure. Better naming

--- src/ralph/cmdb/graphs.py
+++ src/ralph/cmdb/graphs.py
@@ -9,7 +9,6 @@ from __future__ import unicode_literals
 from collections import defaultdict
 from itertools import chain
 
-from django.db.models import Q
 import pygraph
 from pygraph.algorithms.searching import breadth_first_search
 from pygraph.classes.exceptions import AdditionError
@@ -18,7 +17,6 @@ from ralph.cmdb.models import CI, CIRelation, CI_TYPES, CI_RELATION_TYPES
 
 
 class ImpactCalculator(object):
-    visited = []
 
     def __init__(self, root_ci, relation_types=[]):
         self.root_ci = root_ci
@@ -47,74 +45,87 @@ class ImpactCalculator(object):
                 'id': relation.get('id'),
             })
 
-    def get_all_childs(self):
-        """Returns all down-level childs that matches CONTAINS relation"""
+    def get_all_children(self):
+        """Returns all down-level children that matches CONTAINS relation"""
         self.cache_relations()
         self.visited_nodes = set()
-        return self.get_all_childs_rec(self.root_ci.id)
+        return self.get_all_children_rec(self.root_ci.id)
 
-    def get_all_childs_rec(self, ci_id):
-        """Get all contains children of node ``ci_id``"""
-        # guard
-        if ci_id in self.visited_nodes:
-            return []
-        self.visited_nodes.add(ci_id)
-        affected = []
-        for rel in self.cached_relations[ci_id]:
-            affected.append(rel)
-            affected.extend(self.get_all_childs_rec(rel['child']))
-        return affected
+    def get_all_children_rec(self, node):
+        stack = [node]
+        visited = {node}
+        while stack:
+            node = stack.pop()
+            for child in self.cached_relations[node]:
+                if child['id'] in visited:
+                    continue
+                yield child
+                visited.add(node)
+                stack.append(child['id'])
 
     def find_affected_nodes(self, ci_id):
         try:
-            st, pre = breadth_first_search(self.graph, ci_id)
+            search_tree, pre = breadth_first_search(self.graph, ci_id)
         except KeyError:
             return []
-        return (st, pre)
+        return (search_tree, pre)
+
+    def add_edge(self, type_, parent_id, child_id):
+        if type_ == CI_RELATION_TYPES.CONTAINS.id:
+            # the only relation which we can traverse going straight
+            from_ = parent_id
+            to = child_id
+        else:
+            # opposite direction for graph traversal.
+            from_ = child_id
+            to = parent_id
+        try:
+            # the only relation which we can traverse going straight
+            self.graph.add_edge((from_, to), attrs=(type_,))
+        except AdditionError:
+            # ignore duplicated relations(types) in graph
+            pass
 
     def build_graph(self):
-        # get all down-level childs
-        ci_contains = self.get_all_childs()
+        # get all down-level children
+        ci_contains = self.get_all_children()
         # get one level up parents
         ci_is_part_of = CIRelation.objects.filter(child__id=self.root_ci.id)
         ci_is_required = CIRelation.objects.filter(
             # get children and parent one level up/down.
-            Q(type=CI_RELATION_TYPES.REQUIRES.id) &
-            Q(child=self.root_ci)
+            type=CI_RELATION_TYPES.REQUIRES.id,
+            child=self.root_ci,
         )
         ci_requires = CIRelation.objects.filter(
             # get children and parent one level up/down.
-            Q(type=CI_RELATION_TYPES.REQUIRES.id) &
-            Q(parent=self.root_ci)
+            type=CI_RELATION_TYPES.REQUIRES.id,
+            parent=self.root_ci,
         )
         ci_is_role = CIRelation.objects.filter(
-            Q(
-                # get roles
-                Q(type=CI_RELATION_TYPES.HASROLE.id) &
-                Q(child=self.root_ci)
-            )
+            type=CI_RELATION_TYPES.HASROLE.id,
+            child=self.root_ci,
         )
         ci_has_role = CIRelation.objects.filter(
-            Q(
-                Q(type=CI_RELATION_TYPES.HASROLE.id) &
-                Q(parent=self.root_ci)
-            )
+            type=CI_RELATION_TYPES.HASROLE.id,
+            parent=self.root_ci,
         )
         ci_service = CIRelation.objects.filter(
-            Q(
-                # get venture node belongs to, and service venture belongs to.
-                Q(type=CI_RELATION_TYPES.CONTAINS.id) &
-                Q(
-                    child__child=self.root_ci,
-                    parent__type=CI_TYPES.SERVICE.id,
-                    child__type=CI_TYPES.VENTURE.id,
-                )
-            )
+            # get venture node belongs to, and service venture belongs to.
+            type=CI_RELATION_TYPES.CONTAINS.id,
+            child__child=self.root_ci,
+            parent__type=CI_TYPES.SERVICE.id,
+            child__type=CI_TYPES.VENTURE.id,
         )
         self.graph = pygraph.classes.graph.graph()
-        self.graph.add_nodes((c['pk'] for c in CI.objects.values('pk')))
+        self.graph.add_nodes(CI.objects.values_list('pk', flat=True))
+
+        for relation in ci_contains:
+            parent = relation['parent']
+            child = relation['child']
+            type_ = relation['type']
+            self.add_edge(type_, parent, child)
+
         for relation in chain(
-            ci_contains,
             ci_is_part_of,
             ci_is_required,
             ci_requires,
@@ -122,23 +133,5 @@ class ImpactCalculator(object):
             ci_has_role,
             ci_service,
         ):
-            if isinstance(relation, dict):
-                parent = relation['parent']
-                child = relation['child']
-                type_ = relation['type']
-            else:
-                type_ = relation.type
-                if relation.type == CI_RELATION_TYPES.CONTAINS.id:
-                    # the only relation which we can traverse going straight
-                    parent = relation.parent_id
-                    child = relation.child_id
-                else:
-                    # opposite direction for graph traversal.
-                    parent = relation.child_id
-                    child = relation.parent_id
-            try:
-                self.graph.add_edge((parent, child), attrs=(type_,))
-            except AdditionError:
-                # ignore duplicated relations(types) in graph
-                pass
+            self.add_edge(relation.type, relation.parent_id, relation.child_id)
 
