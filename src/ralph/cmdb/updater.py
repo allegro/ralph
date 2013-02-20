573d7c2980526518174c9aca9e94ec06360908e9
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2013-02-20 10:56:55 +0100

    CMDB - another changes in CILayer

--- src/ralph/cmdb/updater.py
+++ src/ralph/cmdb/updater.py
@@ -9,18 +9,17 @@ from __future__ import unicode_literals
 from ralph.cmdb.models import CI
 
 
-def update_cis_layers(touched_content_types, layer_content_types, layer):
+def update_cis_layers(touched_types, layer_types, layer):
     """
-    Update all CIs whose "content type" is connected with changed layers.
+    Update all CIs whose "type" is connected with changed layers.
     """
-    ci_objects = CI.objects.filter(content_type__in=touched_content_types)
+    ci_objects = CI.objects.filter(type__in=touched_types)
     for ci in ci_objects:
         ci_layers = set()
         for ci_layer in ci.layers.all():
             if ci_layer.id != layer.pk:
                 ci_layers.add(ci_layer)
-        if ci.content_type.id in layer_content_types:
+        if ci.type.id in layer_types:
             ci_layers.add(layer)
         ci.layers = ci_layers
         ci.save()
-
