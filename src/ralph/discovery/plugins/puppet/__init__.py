dfe0737e20367a1ba6c14b90faca9a2dbfc0e700
Marcin Kliks <marcin.kliks@gmail.com>
2013-03-26 17:16:54 +0100

    Added new Puppet Facter provider - API

--- src/ralph/discovery/plugins/puppet/__init__.py
+++ src/ralph/discovery/plugins/puppet/__init__.py
@@ -15,6 +15,7 @@ import time
 
 from lck.django.common import nested_commit_on_success
 import MySQLdb
+import requests
 from django.conf import settings
 
 from ralph.util import network, plugin
@@ -24,24 +25,99 @@ from ralph.discovery import hardware
 from .facts import parse_facts, handle_facts_os, handle_facts_packages
 from .lshw import parse_lshw
 from .util import connect_db, get_ip_hostname_sets
+from ralph.discovery.plugins.puppet.yaml import load
 
 
 SAVE_PRIORITY = 51
 
 
+class PuppetProvider(object):
+    """Base class for puppet facts providers (api/db)"""
+    def get_facts(self, ip_set, hostname_set):
+        raise NotImplementedError()
+
+
+class PuppetAPIProvider(PuppetProvider):
+    def get_facts(self, ip_set, hostname_set):
+        return self.get_all_facts_by_hostname_set(hostname_set)
+
+    def get_all_facts_by_hostname_set(self, hostname_set):
+        for hostname in hostname_set:
+            facts = load(self.get_data_for_hostname(hostname))
+            if facts:
+                return facts
+
+    def get_data_for_hostname(self, hostname):
+        r = requests.get(
+            "%(base_url)s/%(hostname)s" % dict(
+            base_url=settings.PUPPET_API_URL, hostname=hostname),
+            headers={'Accept': 'yaml'},
+            verify=False,
+        )
+        if r.status_code == requests.codes['\o/']:
+            return r.text
+
+
+class PuppetDBProvider(PuppetProvider):
+    def get_facts(self, ip_set, hostname_set):
+        db = connect_db()
+        facts = self.get_all_facts_by_ip_set(db, ip_set)
+        if not facts and hostname_set:
+            facts = self.get_all_facts_by_hostname_set(db, hostname_set)
+        return facts
+
+    def get_all_facts_by_ip_set(self, db, ip_set):
+        facts = None
+        for ip in ip_set:
+            rows = db.execute("""SELECT fn.name, fv.value
+                                FROM hosts h, fact_values fv, fact_names fn
+                                WHERE h.ip=%s AND fv.host_id=h.id
+                                AND fv.fact_name_id=fn.id""", ip)
+            _facts = dict(rows.fetchall())
+            if _facts:
+                if _facts['virtual'] == 'zone':
+                    continue
+                if not facts:
+                    facts = _facts
+                else:
+                    raise ValueError(
+                        "more than 1 machine reported by Puppet for "
+                        "this IP set: {}".format(ip_set))
+        return facts
+
+    def get_all_facts_by_hostname_set(self, db, hostname_set):
+        facts = None
+        for hostname in hostname_set:
+            rows = db.execute("""SELECT fn.name, fv.value
+                                FROM hosts h, fact_values fv, fact_names fn
+                                WHERE h.name=%s AND fv.host_id=h.id
+                                AND fv.fact_name_id=fn.id""", hostname)
+            _facts = dict(rows.fetchall())
+            if _facts:
+                if not facts:
+                    facts = _facts
+                else:
+                    raise ValueError(
+                        "more than 1 machine reported by Puppet for "
+                        "this hostname set: {}".format(hostname_set))
+        return facts
+
+
 @plugin.register(chain='discovery', requires=['ping'], priority=200)
 def puppet(**kwargs):
-    if not settings.PUPPET_DB_URL:
+    if not settings.PUPPET_API_URL and not settings.PUPPET_DB_URL:
         return False, "not configured", kwargs
     ip = str(kwargs['ip'])
     ip_set, hostname_set = get_ip_hostname_sets(ip)
-    db = connect_db()
-    facts = get_all_facts_by_ip_set(db, ip_set)
-    if not facts and hostname_set:
-        facts = get_all_facts_by_hostname_set(db, hostname_set)
+    if settings.PUPPET_DB_URL:
+        puppet = PuppetAPIProvider()
+    else:
+        puppet = PuppetDBProvider()
+
+    facts = puppet.get_facts(ip_set, hostname_set)
+
     if not facts:
         return False, "host config not found.", kwargs
-
     try:
         is_virtual = is_host_virtual(facts)
         try:
@@ -51,7 +127,8 @@ def puppet(**kwargs):
         else:
             dev, dev_name = parse_lshw(lshw, facts, is_virtual)
     except MySQLdb.OperationalError as e:
-        if e.args[0] in (1205, 1213) and 'try restarting transaction' in e.args[1]:
+        if (e.args[0] in (1205, 1213)
+                and 'try restarting transaction' in e.args[1]):
             time.sleep(random.choice(range(10)) + 1)
             raise plugin.Restart(unicode(e), kwargs)
         raise
@@ -69,53 +146,21 @@ def puppet(**kwargs):
     if created:
         ip_address.hostname = network.hostname(ip_address.address)
     ip_address.last_puppet = datetime.datetime.now()
-    ip_address.save(update_last_seen=True) # no priorities for IP addresses
+    ip_address.save(update_last_seen=True)  # no priorities for IP addresses
 
     handle_facts_os(dev, facts, is_virtual)
     handle_facts_packages(dev, facts.get('packages'))
     return True, message, kwargs
 
-def get_all_facts_by_ip_set(db, ip_set):
-    facts = None
-    for ip in ip_set:
-        rows = db.execute("""SELECT fn.name, fv.value
-                            FROM hosts h, fact_values fv, fact_names fn
-                            WHERE h.ip=%s AND fv.host_id=h.id
-                            AND fv.fact_name_id=fn.id""", ip)
-        _facts = dict(rows.fetchall())
-        if _facts:
-            if _facts['virtual'] == 'zone':
-                continue
-            if not facts:
-                facts = _facts
-            else:
-                raise ValueError("more than 1 machine reported by Puppet for "
-                    "this IP set: {}".format(ip_set))
-    return facts
-
-def get_all_facts_by_hostname_set(db, hostname_set):
-    facts = None
-    for hostname in hostname_set:
-        rows = db.execute("""SELECT fn.name, fv.value
-                            FROM hosts h, fact_values fv, fact_names fn
-                            WHERE h.name=%s AND fv.host_id=h.id
-                            AND fv.fact_name_id=fn.id""", hostname)
-        _facts = dict(rows.fetchall())
-        if _facts:
-            if not facts:
-                facts = _facts
-            else:
-                raise ValueError("more than 1 machine reported by Puppet for "
-                    "this hostname set: {}".format(hostname_set))
-    return facts
 
 def is_host_virtual(facts):
-    is_virtual = facts.get('virtual', 'physical') not in ('physical',
-        'openvz', 'openvzhn')
+    is_virtual = facts.get('virtual', 'physical') not in (
+        'physical', 'openvz', 'openvzhn')
     if facts.get('manufacturer') == 'Bochs' and not is_virtual:
         facts['virtual'] = 'virtual'
         is_virtual = True
     return is_virtual
+
 
 @nested_commit_on_success
 def parse_wwn(facts, dev):
@@ -125,7 +170,7 @@ def parse_wwn(facts, dev):
         except DiskShare.DoesNotExist:
             return None
         mount, created = DiskShareMount.concurrent_get_or_create(
-                share=share, device=dev)
+            share=share, device=dev)
         return mount
     wwns = []
     for key, wwn in facts.iteritems():
@@ -147,6 +192,7 @@ def parse_wwn(facts, dev):
 HPACU_GENERAL_REGEX = re.compile(r'hpacu_([^_]+)__(.+)')
 HPACU_LOGICAL_PHYSICAL_REGEX = re.compile(r'([^_]+)__(.+)')
 
+
 @nested_commit_on_success
 def parse_hpacu(facts, dev):
     disks = {}
@@ -162,7 +208,9 @@ def parse_hpacu(facts, dev):
         disks.setdefault(physical_disk, {})[property] = value.strip()
     hardware.handle_hpacu(dev, disks, SAVE_PRIORITY)
 
+
 SMARTCTL_REGEX = re.compile(r'smartctl_([^_]+)__(.+)')
+
 
 @nested_commit_on_success
 def parse_smartctl(facts, dev):
@@ -176,7 +224,9 @@ def parse_smartctl(facts, dev):
         disks.setdefault(disk, {})[property] = value.strip()
     hardware.handle_smartctl(dev, disks, SAVE_PRIORITY)
 
+
 MEGARAID_REGEX = re.compile(r'megacli_([^_]+)_([^_]+)__(.+)')
+
 
 @nested_commit_on_success
 def parse_megaraid(facts, dev):
@@ -191,6 +241,7 @@ def parse_megaraid(facts, dev):
         property = m.group(3)
         disks.setdefault((controller, disk), {})[property] = value.strip()
     hardware.handle_megaraid(dev, disks, SAVE_PRIORITY)
+
 
 @nested_commit_on_success
 def parse_uptime(facts, dev):
