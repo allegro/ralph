6ca67b29b87625a0922ca153c63cefc7d15fa9e5
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2013-03-27 09:01:10 +0100

    discovery - better HITACHI disks detection

--- src/ralph/discovery/hardware.py
+++ src/ralph/discovery/hardware.py
@@ -26,6 +26,7 @@ INQUIRY_REGEXES = (
     re.compile(r'^(?P<vendor>IBM)-(?P<product>[a-zA-Z0-9]+)\s+(?P<sn>[a-zA-Z0-9]+)$'),
     re.compile(r'^(?P<vendor>HP)\s+(?P<product>[a-zA-Z0-9]{11})\s+(?P<sn>[a-zA-Z0-9]{12})$'),
     re.compile(r'^(?P<vendor>HITACHI)\s+(?P<product>[a-zA-Z0-9]{15})(?P<sn>[a-zA-Z0-9]{15})$'),
+    re.compile(r'^(?P<vendor>HITACHI)\s+(?P<product>[a-zA-Z0-9]{15})\s+(?P<sn>[a-zA-Z0-9]{12})$'),
 )
 
 
