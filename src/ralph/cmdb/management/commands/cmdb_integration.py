2c7c823f420c188e450b30c14710ff169ed7ea1e
Łukasz Langa <lukasz@langa.pl>
2013-04-04 12:44:21 +0200

    RQ migration complete

--- src/ralph/cmdb/management/commands/cmdb_integration.py
+++ src/ralph/cmdb/management/commands/cmdb_integration.py
@@ -5,38 +5,40 @@ from __future__ import unicode_literals
 
 import textwrap
 import logging
-import re
 
 from django.core.management.base import BaseCommand
 from ralph.util import plugin
-from functools import partial
 from optparse import make_option
 from ralph.discovery.tasks import run_chain
-# hook plugins
-import ralph.cmdb.integration.sync
+
 
 logger = logging.getLogger(__name__)
+
+
+def get_cmdb_plugins():
+    for key in plugin.BY_NAME:
+        if key.startswith('cmdb'):
+            yield key
+
 
 class Command(BaseCommand):
     """Integration with 3rd party services."""
     help = textwrap.dedent(__doc__).strip()
     requires_model_validation = True
 
-    def get_cmdb_plugins(self):
-        return dict([(x, plugin.BY_NAME[x]) for x in plugin.BY_NAME.keys() if x.startswith('cmdb')])
-
     def __init__(self, *args, **kwargs):
         self.option_list = []
         self.option_list.extend(BaseCommand.option_list)
-        for p in self.get_cmdb_plugins().iteritems():
-            plugin_name = p[0]
-            self.option_list.append(make_option('--%s' % plugin_name,
+        for plugin_name in get_cmdb_plugins():
+            self.option_list.append(make_option(
+                '--%s' % plugin_name,
                 dest="%s" % plugin_name,
                 action="store_true",
                 help="Trigger %s plugin" % plugin_name,
-                default=False
+                default=False,
             ))
-        self.option_list.append(make_option('--remote',
+        self.option_list.append(make_option(
+            '--remote',
             dest="remote",
             action="store_true",
             help="Runs on remote worker",
@@ -45,18 +47,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         specified_option = False
-        if options.get('remote'):
-            run = run_chain.delay
-        else:
-            run = partial(run_chain, interactive=True, clear_down=False)
-        context = {'context' : ''}
-        for p in self.get_cmdb_plugins().iteritems():
-            if options.get(p[0]):
-                chain_name=p[0]
-                logger.debug('Executing %s chain .' % p[0])
-                run(context, chain_name)
-                logger.debug('Finished executing %s chain.' % p[0])
-                specified_option = True
+        interactive = not bool(options['remote'])
+        for chain_name in get_cmdb_plugins():
+            if not options.get(chain_name):
+                continue
+            logger.debug('Executing %s chain .' % chain_name)
+            run_chain(
+                {'queue': chain_name}, chain_name, interactive=interactive,
+            )
+            logger.debug('Finished executing %s chain.' % chain_name)
+            specified_option = True
         if not specified_option:
             print('Please specify option. ')
 
+
+# hook plugins
+import ralph.cmdb.integration.sync   # noqa
