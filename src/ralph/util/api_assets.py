113279317c1aa5c77e895210cc2c8ca1a8c222c4
Kamil Warguła <kwargula@gmail.com>
2013-05-31 14:05:55 +0200

    Method get_device_components accept sn or barcode

--- src/ralph/util/api_assets.py
+++ src/ralph/util/api_assets.py
@@ -5,17 +5,22 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
 from ralph.discovery.models import Device
 
 
-def get_device_components(sn):
+def get_device_components(sn=None, barcode=None):
     """Yields dicts describing all device components to be taken in assets"""
-    try:
-        ralph_device = Device.objects.get(sn=sn)
-    except Device.DoesNotExist:
-        raise LookupError('Device not found')
-    else:
+    if sn:
+        try:
+            ralph_device = Device.objects.get(sn=sn)
+        except Device.DoesNotExist:
+            raise LookupError('Device not found')
+    elif barcode:
+        try:
+            ralph_device = Device.objects.get(barcode=barcode)
+        except Device.DoesNotExist:
+            raise LookupError('Device not found')
+    if ralph_device:
         components = ralph_device.get_components()
         for processor in components.get('processors', []):
             yield {
