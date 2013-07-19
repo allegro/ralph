65b04c5e07ad19100af38b61fe677e18a3b9f4e9
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2013-07-19 11:02:39 +0200

    typo

--- src/ralph/scan/autoscan.py
+++ src/ralph/scan/autoscan.py
@@ -64,7 +64,7 @@ def autoscan_network(network):
             func=_autoscan_group,
             args=[group],
             timeout=60,
-            result_ttil=0,
+            result_ttl=0,
         )
 
 
