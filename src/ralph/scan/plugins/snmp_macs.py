8303323ab7a1fa77ad92ec56cf4927ad19b8840e
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2013-07-26 07:45:41 +0200

    clean up the snmp_macs plugin

--- src/ralph/scan/plugins/snmp_macs.py
+++ src/ralph/scan/plugins/snmp_macs.py
@@ -11,11 +11,13 @@ from django.conf import settings
 
 from ralph.discovery.models import DeviceType, MAC_PREFIX_BLACKLIST
 from ralph.discovery.snmp import snmp_macs
-from ralph.scan.errors import NoMatchError
 from ralph.scan.plugins import get_base_result_template
 
+SETTINGS = settings.SCAN_PLUGINS.get(__name__, {})
 
-SETTINGS = settings.SCAN_PLUGINS.get('ralph.scan.plugins.snmp_macs', {})
+
+class Error(Exception):
+    pass
 
 
 def _get_model_info(snmp_name):
@@ -65,7 +67,9 @@ def _get_model_info(snmp_name):
         model_name = 'Linux'
         model_type = DeviceType.unknown
     else:
-        raise NoMatchError("No match.")
+        raise Error(
+            "The SNMP name %r doesn't match any supported device."
+        )
     return model_name, model_type, is_management
 
 
@@ -73,11 +77,13 @@ def _snmp_mac(ip_address, snmp_name, snmp_community, snmp_version,
               messages=[]):
     oid = (1, 3, 6, 1, 2, 1, 2, 2, 1, 6)
     sn = None
-    try:
-        model_name, model_type, is_management = _get_model_info(snmp_name)
-    except NoMatchError as e:
-        messages.append(str(e))
+    if not snmp_name or not snmp_community:
+        raise Error(
+            "Empty SNMP name or community. "
+            "Please perform an autoscan of this address first."
+        )
         return
+    model_name, model_type, is_management = _get_model_info(snmp_name)
     if snmp_name.startswith('IronPort'):
         parts = snmp_name.split(',')
         pairs = dict(
@@ -88,10 +94,17 @@ def _snmp_mac(ip_address, snmp_name, snmp_community, snmp_version,
         sn = pairs.get('Serial #')
     if snmp_name.startswith('APC'):
         m = re.search(r'\sSN:\s*(\S+)', snmp_name)
-        sn = m.group(1) if m else None
+        if m:
+            sn = m.group(1)
     mac_addresses = set()
-    for mac in snmp_macs(ip_address, snmp_community, oid, attempts=2,
-                         timeout=3, snmp_version=snmp_version):
+    for mac in snmp_macs(
+        ip_address,
+        snmp_community,
+        oid,
+        attempts=2,
+        timeout=3,
+        snmp_version=snmp_version,
+    ):
         # Skip virtual devices
         if mac[0:6] in MAC_PREFIX_BLACKLIST:
             continue
@@ -103,57 +116,50 @@ def _snmp_mac(ip_address, snmp_name, snmp_community, snmp_version,
             # Skip VMWare interfaces on Windows
             continue
         if mac.startswith('0001D7') and model_type != DeviceType.load_balancer:
-            messages.append('This is an F5.')
-            return
+            raise Error("This is an F5.")
         mac_addresses.add(mac)
     if not mac_addresses:
-        messages.append('No MAC address.')
-        return
+        raise Error("No valid MAC addresses in the SNMP response.")
+
     result = {
         'type': str(model_type),
         'model_name': model_name,
         'mac_addresses': list(mac_addresses),
     }
     if sn:
-        result.update({
-            'sn': sn,
-        })
+        result.update(sn=sn)
     if is_management:
-        result.update({
-            'management_ip_addresses': [ip_address],
-        })
-    if model_name.lower() in ('linux', 'sunos'):
-        family = 'Linux' if model_name.lower() == 'linux' else 'Sun'
-        result.update({
-            'system': {
-                'family': family,
-            },
-        })
+        result.update(management_ip_addresses=[ip_address])
+        if model_name.lower() == 'linux':
+            result.get('system', {}).update(family="Linux")
+        elif model_name.lower() == 'sunos':
+            result.get('system', {}).update(family="Sun")
     return result
 
 
 def scan_address(ip_address, **kwargs):
     snmp_name = kwargs.get('snmp_name', '')
-    snmp_version = kwargs.get('snmp_version', '2c')
+    snmp_version = kwargs.get('snmp_version', '2c') or '2c'
     if snmp_version == '3':
         snmp_community = SETTINGS['snmp_v3_auth']
     else:
-        snmp_community = str(kwargs.get(
-            'snmp_community', SETTINGS['snmp_v3_auth'],
-        ))
+        snmp_community = str(kwargs['snmp_community'])
     messages = []
-    device_info = _snmp_mac(
-        ip_address, snmp_name, snmp_community, snmp_version, messages,
-    )
     result = get_base_result_template('snmp_macs', messages)
-    if not device_info:
-        result.update({
-            'status': 'failed',
-        })
+    try:
+        device_info = _snmp_mac(
+            ip_address,
+            snmp_name,
+            snmp_community,
+            snmp_version,
+            messages,
+        )
+    except Error as e:
+        messages.append(unicode(e))
+        result.update(status="error")
     else:
         result.update({
-            'status': 'ok',
-            'devices': [device_info],
+            'status': 'success',
+            'device': device_info,
         })
     return result
-
