a2fb1155246b6d421076bcc2cf068a23cea5181a
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2013-08-21 11:48:26 +0200

    Add logging to manual scan

--- src/ralph/scan/manual.py
+++ src/ralph/scan/manual.py
@@ -5,13 +5,19 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
-import rq
-import django_rq
+import datetime
+from django.conf import settings
 from django.utils.importlib import import_module
+import django_rq
+import json
+import os.path
+import rq
 
 from ralph.discovery.models import IPAddress, Network
 from ralph.scan.errors import NoQueueError
+
+
+SCAN_LOG_DIRECTORY = getattr(settings, 'SCAN_LOG_DIRECTORY', None)
 
 
 def scan_address(address, plugins):
@@ -39,7 +45,7 @@ def scan_address(address, plugins):
         ),
         kwargs={
             'snmp_community': ipaddress.snmp_community,
-#            'snmp_version': ipaddress.snmp_version,
+            'snmp_version': ipaddress.snmp_version,
             'snmp_version': '2c',
             'http_family': ipaddress.http_family,
             'snmp_name': ipaddress.snmp_name,
@@ -76,4 +82,19 @@ def _scan_address(address, plugins, **kwargs):
             job.meta['status'][plugin_name] = result.get('status', 'success')
         job.meta['finished'].append(plugin_name)
         job.save()
+    _log_results(address, results)
     return results
+
+
+def _log_results(address, results):
+    """If logging is configured, logs the results of the scan."""
+
+    if SCAN_LOG_DIRECTORY is None:
+        return
+    filename = '%s_%s.json' % (
+        address,
+        datetime.datetime.now().strftime("%Y-%m-%d_%H:%M"),
+    )
+    filepath = os.path.join(SCAN_LOG_DIRECTORY, filename)
+    with open(filepath, 'w') as f:
+        json.dump(results, f)
