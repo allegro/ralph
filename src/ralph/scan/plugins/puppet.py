a944619231b14b65b459f3ea8ebb99f3ecc71f89
Andrzej Jankowski <andrew.jankowski@gmail.com>
2013-09-11 14:21:42 +0200

    new puppet plugin - in progress

--- src/ralph/scan/plugins/puppet.py
+++ src/ralph/scan/plugins/puppet.py
@@ -5,14 +5,26 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import random
+import thread
+import time
+
+import MySQLdb
+import requests
+import sqlalchemy as sqla
+import yaml
+
 from django.conf import settings
 
-from ralph.discovery.models import IPAddress
+from ralph.discovery.models import IPAddress, SERIAL_BLACKLIST
+from ralph.scan.lshw import Error as LshwError
+from ralph.scan.lshw import handle_lshw
 from ralph.scan.plugins import get_base_result_template
-from ralph.util import network
+from ralph.util import network, uncompress_base64_data
 
 
 SETTINGS = settings.SCAN_PLUGINS.get(__name__, {})
+ENGINES = {}
 
 
 class Error(Exception):
@@ -20,7 +32,7 @@ class Error(Exception):
 
 
 class PuppetBaseProvider(object):
-    def get_facts(self, ip_addresses, hostnames):
+    def get_facts(self, ip_addresses, hostnames, messages=[]):
         raise NotImplementedError()
 
 
@@ -28,16 +40,127 @@ class PuppetAPIProvider(PuppetBaseProvider):
     def __init__(self, api_url):
         self.api_url = api_url
 
-    def get_facts(self, ip_addresses, hostnames):
-        raise NotImplementedError()
+    def get_facts(self, ip_addresses, hostnames, messages=[]):
+        return self._get_all_facts_by_hostnames(hostnames)
+
+    def _get_all_facts_by_hostnames(self, hostnames):
+        def construct_ruby_object(loader, suffix, node):
+            return loader.construct_yaml_map(node)
+
+        def construct_ruby_sym(loader, node):
+            return loader.construct_yaml_str(node)
+
+        yaml.add_multi_constructor(u"!ruby/object:", construct_ruby_object)
+        yaml.add_constructor(u"!ruby/sym", construct_ruby_sym)
+        for hostname in hostnames:
+            data = self._get_data_for_hostname(hostname)
+            if data:
+                return yaml.load(data)['values']
+
+    def _get_data_for_hostname(self, hostname):
+        response = requests.get(
+            "%(base_url)s/%(hostname)s" % dict(
+                base_url=self.api_url,
+                hostname=hostname,
+            ),
+            headers={'Accept': 'yaml'},
+            verify=False,
+        )
+        if response.status_code == requests.codes.ok:
+            return response.text
 
 
 class PuppetDBProvider(PuppetBaseProvider):
     def __init__(self, db_url):
         self.db_url = db_url
+        self.conn = self._connect_db()
 
-    def get_facts(self, ip_addresses, hostnames):
-        raise NotImplementedError()
+    def _connect_db(self, force_reconnect=False):
+        thread_id = thread.get_ident()
+        if force_reconnect and thread_id in ENGINES:
+            del ENGINES[thread_id]
+        conn = None
+        try:
+            engine = ENGINES[thread_id]
+            conn = engine.connect()
+            _test = conn.execute("SELECT 1")
+            _test.fetchall()
+        except (KeyError, MySQLdb.OperationalError):
+            if conn:
+                conn.close()
+            engine = sqla.create_engine(self.db_url, pool_recycle=3600)
+            ENGINES[thread_id] = engine
+            conn = engine.connect()
+        return conn
+
+    def get_facts(self, ip_addresses, hostnames, messages=[]):
+        return self._get_facts(ip_addresses, hostnames, messages)
+
+    def _get_facts(self, ip_addresses, hostnames, messages=[]):
+        try:
+            facts = self._get_facts_by_ip_addresses(ip_addresses)
+            if not facts and hostnames:
+                facts = self._get_facts_by_hostnames(hostnames)
+        except MySQLdb.OperationalError as e:
+            messages.append(unicode(e))
+            if all((
+                e.args[0] in (1205, 1213),
+                'try restarting transaction' in e.args[1],
+            )):
+                time.sleep(random.choice(range(10)) + 1)
+                self.conn = self._connect_db(True)
+                return self._get_facts(ip_addresses, hostnames, messages)
+            raise
+        return facts
+
+    def _get_facts_by_ip_addresses(self, ip_addresses):
+        facts = None
+        for ip_address in ip_addresses:
+            rows = self.conn.execute(
+                """
+                    SELECT fn.name, fv.value
+                    FROM hosts h, fact_values fv, fact_names fn
+                    WHERE h.ip=%s AND fv.host_id=h.id
+                    AND fv.fact_name_id=fn.id
+                """,
+                ip_address,
+            )
+            _facts = dict(rows.fetchall())
+            if _facts:
+                if _facts['virtual'] == 'zone':
+                    continue
+                if not facts:
+                    facts = _facts
+                else:
+                    raise Error(
+                        "More than 1 machine reported by Puppet for "
+                        "this IP set: {}".format(', '.join(ip_addresses)),
+                    )
+        return facts
+
+
+    def _get_facts_by_hostnames(self, hostnames):
+        facts = None
+        for hostname in hostnames:
+            rows = self.conn.execute(
+                """
+                    SELECT fn.name, fv.value
+                    FROM hosts h, fact_values fv, fact_names fn
+                    WHERE h.name=%s AND fv.host_id=h.id
+                    AND fv.fact_name_id=fn.id
+                """,
+                hostname,
+            )
+            _facts = dict(rows.fetchall())
+            if _facts:
+                if not facts:
+                    facts = _facts
+                else:
+                    raise Error(
+                        "More than 1 machine reported by Puppet for "
+                        "this hostname set: {}".format(', '.join(hostnames)),
+                    )
+        return facts
 
 
 def _get_ip_addresses_hostnames_sets(ip):
@@ -58,15 +181,49 @@ def _get_ip_addresses_hostnames_sets(ip):
     return ip_addresses_set, hostnames_set
 
 
-def _puppet(provider, ip_address):
+def _is_host_virtual(facts):
+    is_virtual = facts.get('virtual', 'physical') not in (
+        'physical', 'openvz', 'openvzhn',
+    )
+    if facts.get('manufacturer') == 'Bochs' and not is_virtual:
+        facts['virtual'] = 'virtual'
+        is_virtual = True
+    return is_virtual
+
+
+def _parse_lshw(lshw, is_virtual, sn=None):
+    data = uncompress_base64_data(lshw)
+    if sn in SERIAL_BLACKLIST:
+        sn = None
+    device_info = handle_lshw(data, is_virtual)
+    if sn:
+        device_info['serial_number'] = sn
+    return device_info
+
+
+def _parse_facts(facts, is_virtual):
+    # todo
+    return {}
+
+
+def _puppet(provider, ip_address, messages=[]):
     ip_addresses_set, hostnames_set = _get_ip_addresses_hostnames_sets(
         ip_address,
     )
-    facts = provider.get_facts(ip_addresses_set, hostnames_set)
+    facts = provider.get_facts(ip_addresses_set, hostnames_set, messages)
     if not facts:
         raise Error('Host config not found.')
+    is_virtual = _is_host_virtual(facts)
+    if 'lshw' in facts:
+        device_info = _parse_lshw(facts['lshw'], is_virtual)
+    else:
+        device_info = _parse_facts(facts, is_virtual)
+
+    # todo: ethernets for lshw?
+
     import pprint
-    pprint.pprint(facts)
+    pprint.pprint(device_info)
+
     return {}
 
 
@@ -84,8 +241,8 @@ def scan_address(ip_address, **kwargs):
     else:
         puppet_provider = PuppetAPIProvider(puppet_api_url)
     try:
-        device_info = _puppet(puppet_provider)
-    except Error as e:
+        device_info = _puppet(puppet_provider, ip_address, messages)
+    except (LshwError, Error) as e:
         messages.append(unicode(e))
         result['status'] = 'error'
     else:
