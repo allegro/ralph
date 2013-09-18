abd98f6d59dc8f7203d7fdc809898f1d46c02a28
Andrzej Jankowski <andrew.jankowski@gmail.com>
2013-09-18 09:06:53 +0200

    puppet classifier - fix

--- src/ralph/deployment/views.py
+++ src/ralph/deployment/views.py
@@ -179,7 +179,13 @@ def puppet_classifier(request):
         name = node.name or '?'
         location = name + '__' + location if location else name
         node = node.parent
-    department = device.venture.get_department()
+    department = None
+    owners = []
+    if device.venture:
+        department = device.venture.get_department()
+        ownerships = device.venture.all_ownerships()
+        if hasattr(ownerships, 'select_related'):
+            owners = ownerships.select_related()
     response = {
         'name': device.name,
         'device_id': device.id,
@@ -195,9 +201,8 @@ def puppet_classifier(request):
                 'last_name': o.owner.last_name,
                 'email': o.owner.email,
                 'type': o.type,
-            } for o in
-            device.venture.all_ownerships().select_related('owner')
-        ] if device.venture else [],
+            } for o in owners
+        ],
         'verified': device.verified,
         'last_seen': device.last_seen.strftime('%Y-%m-%dT%H:%M:%S'),
         'model': device.model.name if device.model else None,
