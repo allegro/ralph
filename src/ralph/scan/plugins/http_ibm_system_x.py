8f26d78388d1cf775c387ad6dce79f45e4ce6ce7
Andrzej Jankowski <andrew.jankowski@gmail.com>
2013-10-02 14:52:35 +0200

    http_ibm_system_x - done

--- src/ralph/scan/plugins/http_ibm_system_x.py
+++ src/ralph/scan/plugins/http_ibm_system_x.py
@@ -8,16 +8,47 @@ from __future__ import unicode_literals
 import urllib2
 
 from django.conf import settings
+from xml.etree import cElementTree as ET
 
 from ralph.discovery.http import guess_family, get_http_info
+from ralph.discovery.models import DeviceType
 from ralph.scan.errors import Error, NotConfiguredError, NoMatchError
 from ralph.scan.plugins import get_base_result_template
 
 
 SETTINGS = settings.SCAN_PLUGINS.get(__name__, {})
+GENERIC_SOAP_TEMPLATE = '''\n
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope"
+                   xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing"
+                   xmlns:wsman="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd">
+    <SOAP-ENV:Header>
+        <wsa:To>http://%(management_url)s</wsa:To>
+        <wsa:ReplyTo>
+            <wsa:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:Address>
+        </wsa:ReplyTo>
+        <wsman:ResourceURI>http://www.ibm.com/iBMC/sp/%(resource)s</wsman:ResourceURI>
+        <wsa:Action>%(action)s</wsa:Action>
+        <wsa:MessageID>dt:1348650519402</wsa:MessageID>
+    </SOAP-ENV:Header>
+    <SOAP-ENV:Body>
+        %(body)s
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+'''
 
 
-def get_session_id(ip_address, user, password):
+def _send_soap(post_url, session_id, message):
+    opener = urllib2.build_opener()
+    request = urllib2.Request(
+        post_url, message,
+        headers={'session_id': session_id},
+    )
+    response = opener.open(request, timeout=10)
+    response_data = response.read()
+    return response_data
+
+
+def _get_session_id(ip_address, user, password):
     login_url = "http://%s/session/create" % ip_address
     login_data = "%s,%s" % (user, password)
     opener = urllib2.build_opener()
@@ -29,11 +60,159 @@ def get_session_id(ip_address, user, password):
     raise Error('Session error.')
 
 
-def _http_ibm_system_x(ip_address, user, password):
-    session_id = get_session_id(ip_address)
-    management_url = "http://%s/wsman" % ip_address
-    model_name = get_model_name(management_url, session_id)
+def _get_model_name(management_url, session_id):
+    message = GENERIC_SOAP_TEMPLATE % dict(
+        management_url=management_url,
+        action='http://www.ibm.com/iBMC/sp/Monitors/GetVitalProductData',
+        resource='Monitors',
+        body='''
+            <GetVitalProductData xmlns="http://www.ibm.com/iBMC/sp/Monitors"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            </GetVitalProductData>
+        ''',
+    )
+    soap_result = _send_soap(management_url, session_id, message)
+    tree = ET.XML(soap_result)
+    product_name = tree.findall(
+        '{0}Body/GetVitalProductDataResponse/'
+        'GetVitalProductDataResponse/MachineLevelVPD/'
+        'ProductName'.format('{http://www.w3.org/2003/05/soap-envelope}'),
+    )
+    return product_name[0].text
 
+
+def _get_sn(management_url, session_id):
+    message = GENERIC_SOAP_TEMPLATE % dict(
+        management_url=management_url,
+        action='http://www.ibm.com/iBMC/sp/iBMCControl/GetSPNameSettings',
+        resource='iBMCControl',
+        body='''
+            <GetSPNameSettings xmlns="http://www.ibm.com/iBMC/sp/iBMCControl"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            </GetSPNameSettings>
+        ''',
+    )
+    soap_result = _send_soap(management_url, session_id, message)
+    tree = ET.XML(soap_result)
+    sn = tree.findall('{0}Body/GetSPNameSettingsResponse/SPName'.format(
+        '{http://www.w3.org/2003/05/soap-envelope}',
+    ))
+    sn = sn[0].text
+    return sn
+
+
+def _get_mac_addresses(management_url, session_id):
+    message = GENERIC_SOAP_TEMPLATE % dict(
+        management_url=management_url,
+        action='http://www.ibm.com/iBMC/sp/Monitors/GetHostMacAddresses',
+        resource='Monitors',
+        body='''
+            <GetHostMacAddresses xmlns="http://www.ibm.com/iBMC/sp/Monitors"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            </GetHostMacAddresses>
+        ''',
+    )
+    soap_result = _send_soap(management_url, session_id, message)
+    tree = ET.XML(soap_result)
+    mac_addresses = tree.findall(
+        '{0}Body/GetHostMacAddressesResponse/**'.format(
+            '{http://www.w3.org/2003/05/soap-envelope}',
+        ),
+    )
+    return [mac.find('Address').text for mac in mac_addresses]
+
+
+def _get_memory(management_url, session_id):
+    message = GENERIC_SOAP_TEMPLATE % dict(
+        management_url=management_url,
+        action='http://www.ibm.com/iBMC/sp/Monitors/GetMemoryInfo',
+        resource='Monitors',
+        body='''
+            <GetMemoryInfo xmlns="http://www.ibm.com/iBMC/sp/Monitors"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            </GetMemoryInfo>
+        ''',
+    )
+    soap_result = _send_soap(management_url, session_id, message)
+    tree = ET.XML(soap_result)
+    memory = tree.findall('{0}Body/GetMemoryInfoResponse/Memory/*'.format(
+        '{http://www.w3.org/2003/05/soap-envelope}',
+    ))
+    return [
+        dict(
+            label=chip.find('Description').text,
+            index=int(chip.find('Description').text.split()[1]),
+            size=int(chip.find('Size').text) * 1024,
+        ) for chip in memory
+    ]
+
+
+def _get_processors(management_url, session_id):
+    message = GENERIC_SOAP_TEMPLATE % dict(
+        management_url=management_url,
+        resource='Monitors',
+        action='http://www.ibm.com/iBMC/sp/Monitors/GetProcessorInfo',
+        body='''
+            <GetProcessorInfo xmlns="http://www.ibm.com/iBMC/sp/Monitors"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            </GetProcessorInfo>
+        ''',
+    )
+    soap_result = _send_soap(management_url, session_id, message)
+    tree = ET.XML(soap_result)
+    data = tree.findall('{0}Body/GetProcessorInfoResponse/Processor/*'.format(
+        '{http://www.w3.org/2003/05/soap-envelope}',
+    ))
+    processors = []
+    for d in data:
+        dsc = d.find('Description').text
+        speed = d.find('Speed').text
+        family = d.find('Family').text
+        cores = d.find('Cores').text
+        threads = d.find('Threads').text
+        index = dsc.split()[1]
+        label = '%s CPU %s MHz, %s cores %s threads' % (
+            family,
+            speed,
+            cores,
+            threads,
+        )
+        processors.append(dict(
+            index=index,
+            label=label,
+            speed=speed,
+            cores=cores,
+            family=family,
+        ))
+    return processors
+
+
+def _http_ibm_system_x(ip_address, user, password):
+    session_id = _get_session_id(ip_address, user, password)
+    management_url = "http://%s/wsman" % ip_address
+    model_name = _get_model_name(management_url, session_id)
+    sn = _get_sn(management_url, session_id)
+    device = {
+        'type': DeviceType.rack_server.raw,
+        'model_name': model_name,
+        'serial_number': sn,
+        'management_ip_address': [ip_address],
+    }
+    macs = _get_mac_addresses(management_url, session_id)
+    if macs:
+        device['mac_addresses'] = macs
+    memory = _get_memory(management_url, session_id)
+    if memory:
+        device['memory'] = memory
+    processors = _get_processors(management_url, session_id)
+    if processors:
+        device['processors'] = processors
+    return device
 
 def scan_address(ip_address, **kwargs):
     user = SETTINGS.get('user')
---
+++ src/ralph/scan/tests/plugins/test_http_ibm_system_x.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from mock import patch
+
+from ralph.scan.tests.plugins.samples import http_ibm_system_x as SAMPLES
+from ralph.scan.plugins.http_ibm_system_x import _http_ibm_system_x
+
+
+def _patched_send_soap(post_url, session_id, message):
+    if 'http://www.ibm.com/iBMC/sp/Monitors/GetVitalProductData' in message:
+        return SAMPLES.GENERIC_DATA_RESPONSE
+    elif 'http://www.ibm.com/iBMC/sp/iBMCControl/GetSPNameSettings' in message:
+        return SAMPLES.SN_RESPONSE
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetMemoryInfo' in message:
+        return SAMPLES.MEMORY_RESPONSE
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetHostMacAddresses' in message:
+        return SAMPLES.MACS_RESPONSE
+    elif 'http://www.ibm.com/iBMC/sp/Monitors/GetProcessorInfo' in message:
+        return SAMPLES.PROCESSORS_RESPONSE
+    else:
+        raise Exception('Unknown message type')
+
+
+def _patched_get_session_id(ip_address, user, password):
+    return '65c77c6b-af39-4901-8fef-2dec8c6dd24d'
+
+
+class HttpIbmSystemXTest(TestCase):
+    @patch(
+        'ralph.scan.plugins.http_ibm_system_x._get_session_id',
+        _patched_get_session_id,
+    )
+    @patch(
+        'ralph.scan.plugins.http_ibm_system_x._send_soap',
+        _patched_send_soap,
+    )
+    def test_http_ibm_system_x(self):
+        self.assertEqual(
+            _http_ibm_system_x('127.0.0.1', '-', '-'),
+            {
+                'mac_addresses': ['6E:F3:DD:E5:96:40', '6E:F3:DD:E5:96:42'],
+                'management_ip_address': ['127.0.0.1'],
+                'memory': [
+                    {'index': 2, 'label': 'DIMM 2', 'size': 4096},
+                    {'index': 3, 'label': 'DIMM 3', 'size': 8192},
+                    {'index': 6, 'label': 'DIMM 6', 'size': 8192},
+                    {'index': 9, 'label': 'DIMM 9', 'size': 4096},
+                    {'index': 11, 'label': 'DIMM 11', 'size': 4096},
+                    {'index': 12, 'label': 'DIMM 12', 'size': 8192},
+                    {'index': 15, 'label': 'DIMM 15', 'size': 8192},
+                    {'index': 18, 'label': 'DIMM 18', 'size': 4096},
+                ],
+                'model_name': 'System x3550 M3',
+                'processors': [
+                    {
+                        'cores': '8',
+                        'family': 'Intel Xeon',
+                        'index': '1',
+                        'label': 'Intel Xeon CPU 2666 MHz, 8 cores 1 threads',
+                        'speed': '2666',
+                    },
+                    {
+                        'cores': '8',
+                        'family': 'Intel Xeon',
+                        'index': '2',
+                        'label': 'Intel Xeon CPU 2666 MHz, 8 cores 1 threads',
+                        'speed': '2666',
+                    },
+                ],
+                'serial_number': 'SN# KD55ARA',
+                'type': 'rack server',
+            },
+        )
+
