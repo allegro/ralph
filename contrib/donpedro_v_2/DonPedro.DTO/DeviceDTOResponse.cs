a463c0ee4c8505171629b36582247fa60c0a2695
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2013-11-18 14:20:50 +0100

    new donpedro for Scan app - start

---
+++ contrib/donpedro_v_2/DonPedro.DTO/DeviceDTOResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DonPedro.DTO
+{
+	public class DeviceDTOResponse : BaseDTOResponse
+	{
+		public string Sn { get; set; }
+		public string Caption { get; set; }
+		public string Version { get; set; }
+		public string Vendor { get; set; }
+	}
+}
---
+++ contrib/donpedro_v_2/DonPedro.DTO/DiskShareMountDTOResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DonPedro.DTO
+{
+	public class DiskShareMountDTOResponse : BaseDTOResponse
+	{
+		public string Volume { 
+			get {
+				return this.Label;
+			}
+			set {
+				this.Label = value;
+			}
+		}
+		public string Sn { get; set; }
+	}
+}
