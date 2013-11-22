bff10f65a30995e1e848d1dc1119ddf3a8dc54ca
Andrzej Jankowski <andrew.jankowski@gmail.com>
2013-11-22 11:37:33 +0100

    fix

--- src/ralph/scan/data.py
+++ src/ralph/scan/data.py
@@ -407,7 +407,6 @@ def set_device_data(device, data):
             )
             model.save()
             device.model = model
-            device.save(priority=SCAN_SAVE_PRIORITY)
         else:
             if all((
                 device.model.type != model_type,
@@ -415,7 +414,6 @@ def set_device_data(device, data):
             )):
                 device.model.type = model_type
                 device.model.save()
-                device.save(priority=SCAN_SAVE_PRIORITY)
     if 'disks' in data:
         _update_component_data(
             device,
