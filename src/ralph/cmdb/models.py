44396aa0c716ef342678a4a77405ecc6c2b65193
Marcin Kliks <marcin.kliks@gmail.com>
2014-03-28 09:14:48 +0100

    JiraChanges should be here.

--- src/ralph/cmdb/models.py
+++ src/ralph/cmdb/models.py
@@ -55,6 +55,7 @@ from ralph.cmdb.models_changes import (
     CIEvent,
     CIProblem,
     CIIncident,
+    JiraChanges,
 
     ArchivedCIChangeZabbixTrigger,
     ArchivedCIChangeCMDBHistory,
@@ -111,6 +112,7 @@ __all__ = [
     'CIEvent',
     'CIProblem',
     'CIIncident',
+    'JiraChanges',
 
     # audits
     'Auditable',
