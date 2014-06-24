de1ad63ede94ee3496954606ec500bc4576c276e
Marcin Kliks <marcin.kliks@gmail.com>
2014-06-24 13:54:27 +0200

    1. Docker. 2. Fixed Ralph build on Ubuntu 14.04

--- contrib/docker/createsuperuser.py
+++ contrib/docker/createsuperuser.py
@@ -2,11 +2,6 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ralph.settings")
 
-#from django.core.management import setup_environ
-#from django.conf import settings
-#import settings
-#setup_environ(settings)
-
 from django.contrib.auth.models import User
 
 u = User.objects.get(username='ralph')
