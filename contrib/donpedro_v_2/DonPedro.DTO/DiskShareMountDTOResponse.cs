6abe8cf06aa26dd8d523e899ac83169a0d884839
Andrzej Jankowski <andrzej.jankowski@allegro.pl>
2014-07-15 14:26:18 +0200

    donpedro - additional output options and WWNs normalization

--- contrib/donpedro_v_2/DonPedro.DTO/DiskShareMountDTOResponse.cs
+++ contrib/donpedro_v_2/DonPedro.DTO/DiskShareMountDTOResponse.cs
@@ -4,8 +4,76 @@ namespace DonPedro.DTO
 {
 	public class DiskShareMountDTOResponse : BaseDTOResponse
 	{
+		private string wwn;
+		
 		public string Volume { get; set; }
-		public string SerialNumber { get; set; }
+		
 		public string Size { get; set; }
+		
+		public string SerialNumber { 
+			get {
+				return wwn;
+			}
+			
+			set {
+				wwn = NormalizeWWN(value);
+			}
+		}
+		
+		private string NormalizeWWN(string wwn)
+		{
+			wwn = wwn.Replace(
+				":", ""
+			).Replace(
+				" ", ""
+			).Replace(
+				".", ""
+			).Trim().ToUpper();
+			
+			if (wwn.Length == 16)
+			{
+				// 3PAR
+			}
+			else if (wwn.Length == 17)
+			{
+				// 3PAR - multipath
+				wwn = wwn.Substring(1);
+			}
+			else if (wwn.Length == 33 && wwn.Substring(27).Equals("000000") && wwn.Substring(8, 3).Equals("000"))
+			{
+				// MSA - multipath
+				wwn = wwn.Substring(11, 16);
+			}
+			else if (wwn.Length == 32 && wwn.Substring(27).Equals("000000") && wwn.Substring(12, 4).Equals("0000"))
+			{
+				// MSA
+				wwn = wwn.Substring(6, 6) + wwn.Substring(16, 10);
+			}
+			else if (wwn.Length == 33 && wwn.Substring(25).Equals("00000000") && wwn.StartsWith("36000402"))
+			{
+				// NEXSAN
+				wwn = wwn.Substring(0, 25);
+			}
+			else if (wwn.Length == 32 && wwn.StartsWith("600A0B80"))
+			{
+				// IBM
+			}
+			else if (
+				wwn.Length == 33 && 
+				(
+					wwn.StartsWith("3600A0B80") ||
+					wwn.StartsWith("3600508B1") ||
+					wwn.StartsWith("3600144F0")
+				)
+			)
+			{
+				// IBM - multipath
+            	// HP logical volume - multipath
+            	// SUN - multipath
+            	wwn = wwn.Substring(1);
+			}
+
+			return wwn;
+		}
 	}
 }
---
+++ contrib/donpedro_v_2/DonPedroOutput/TextPrinter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using DonPedro.Detectors;
+using DonPedro.DTO;
+
+namespace DonPedroOutput
+{
+	public class TextPrinter
+	{
+		public void Print(Detector d, bool jsonOnly)
+		{
+			if (jsonOnly)
+			{
+				Console.WriteLine(d.GetAllComponentsJSON());
+			}
+			else
+			{
+				Console.WriteLine("Detected CPUs:");
+				foreach(ProcessorDTOResponse item in d.GetProcessorsInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected memory:");
+				foreach(MemoryDTOResponse item in d.GetMemoryInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected OS:");
+				OperatingSystemDTOResponse os = d.GetOperatingSystemInfo();
+				PropertiesPrinter.Print(os);
+				
+				Console.WriteLine("\nDetected storage:");
+				foreach(StorageDTOResponse item in d.GetStorageInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected IP addresses:");
+				foreach(IPAddressDTOResponse item in d.GetIPAddressInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected MAC addresses:");
+				foreach(MacAddressDTOResponse item in d.GetMacAddressInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected fc cards:");
+				foreach(FibreChannelDTOResponse item in d.GetFibreChannelInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected shares:");
+				foreach(DiskShareMountDTOResponse item in d.GetDiskShareMountInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected software:");
+				foreach(SoftwareDTOResponse item in d.GetSoftwareInfo())
+				{
+					PropertiesPrinter.Print(item);
+				}
+				
+				Console.WriteLine("\nDetected device:");
+				DeviceDTOResponse dev = d.GetDeviceInfo();
+				PropertiesPrinter.Print(dev);
+			}
+		}
+	}
+}
