0662d86ba41c81512f3938eae2784bea936ee04d
Tomasz Mieszkowski <tomasz.mieszkowski@ext.allegro.pl>
2014-07-18 11:11:13 +0200

    Added 'ACLGateway' class which enforces permissions checks (via 'ralph_permission' decorator on 'dispatch' metod) and which should be present in mro path of every class-based view.
    Fixed 'ralph_permission' decorator and added 'decorated_with' property to it
    (for faciliating unit tests).
    
    Applied 'ralph_permission' decorator to the function-based views.
    
    Added test checking if every view inherits from 'ACLGateway' class (for
    class-based views) or is decorated with 'ralph_permission' decorator
    (for function-based views) - both of them are for enforcing permissions
    checks. This test includes a list of excluded callbacks and callbacks
    decorated manually (i.e. not inheriting from 'ACLGateway' class) - for
    the explaination see the comments in the source code.

--- src/ralph/cmdb/rest/rest.py
+++ src/ralph/cmdb/rest/rest.py
@@ -6,13 +6,23 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 
+from ralph.account.models import Perm, ralph_permission
 from ralph.cmdb.integration.puppet import PuppetAgentsImporter
 from ralph.discovery.tasks import run_chain
 from ralph.util.views import jsonify
 
+perms = [
+    {
+        'perm': Perm.has_core_access,
+        'msg': _("You don't have permissions for this resource."),
+    },
+]
 
+
+@ralph_permission(perms)
 @csrf_exempt
 @jsonify
 def notify_puppet_agent(request):
@@ -22,6 +32,7 @@ def notify_puppet_agent(request):
     return {'ok': True}
 
 
+@ralph_permission(perms)
 @csrf_exempt
 @jsonify
 def commit_hook(request):
