c2b8f7714b084a4e13c10132a6036318d1d075b1
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-07-24 09:42:34 +0200

    added utils files with object factories (for tests) & device lookups

---
+++ src/ralph/cmdb/tests/utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from uuid import uuid1
+
+import factory
+from factory.django import DjangoModelFactory
+
+from ralph.cmdb import models_ci
+from ralph.discovery import models_device
+
+
+class CITypeFactory(DjangoModelFactory):
+    FACTORY_FOR = models_ci.CIType
+    name = factory.Sequence(lambda n: 'Name #{}'.format(n))
+
+
+class CIFactory(DjangoModelFactory):
+    FACTORY_FOR = models_ci.CI
+
+    @factory.lazy_attribute
+    def uid(self):
+        return str(uuid1())
+    name = factory.Sequence(lambda n: 'Name #{}'.format(n))
+    business_service = False
+    technical_service = True
+    pci_scope = False
+    # layers, m2m
+    barcode = None
+    # content_type
+    object_id = True
+    # content_object
+    state = models_ci.CI_STATE_TYPES.INACTIVE.id
+    status = models_ci.CI_STATUS_TYPES.REFERENCE.id
+    type = factory.SubFactory(CITypeFactory)
+    zabbix_id = None
+    # relations, m2m
+    added_manually = False
+    # owners, m2m
+
+
+class ServiceCatalogFactory(CIFactory):
+    FACTORY_FOR = models_device.ServiceCatalog
+
+    @factory.lazy_attribute
+    def type(self):
+        return CITypeFactory(name=models_ci.CI_TYPES.SERVICE)
+
+
+class DeviceEnvironmentFactory(DjangoModelFactory):
+    FACTORY_FOR = models_device.DeviceEnvironment
+
+    name = factory.Sequence(lambda n: 'Device Environment #%s' % n)
