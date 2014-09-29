e0d44571ff077afc8ef53251db815bef548a26f0
Kamil Warguła <kwargula@gmail.com>
2014-09-29 16:37:03 +0200

    Bumped django-bob version to 1.9.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     zip_safe=False,  # because templates are loaded from file path
     install_requires=[
         'bob-ajax-selects==1.4.1',
-        'django-bob==1.9.0',
+        'django-bob==1.9.1',
         'django-powerdns-dnssec==0.9.3',
         'django-tastypie==0.9.16',
         'django-rq==0.4.5',
