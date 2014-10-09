c2f90dc869798c2778fecfc498b0f8d81e477a79
Kamil Warguła <kwargula@gmail.com>
2014-10-09 15:14:00 +0200

    Add default region

--- src/ralph/settings.py
+++ src/ralph/settings.py
@@ -755,3 +755,4 @@ LOGIN_REDIRECT_URL = reverse_lazy('find_user_home')
 
 # url to page where user requests permission to module (eg. assets)
 # REQUEST_PERM_URL = 'http://tickets.office/request/ralph_module/permission'
+DEFAULT_REGION_NAME = 'Default Region'
