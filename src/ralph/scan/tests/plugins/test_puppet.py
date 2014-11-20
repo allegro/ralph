a6f42b82d52c214275646ef5d43645721b16ea94
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-11-20 10:21:48 +0100

    puppet plugin can get data from puppetdb-api

--- src/ralph/scan/tests/plugins/test_puppet.py
+++ src/ralph/scan/tests/plugins/test_puppet.py
@@ -18,6 +18,21 @@ from ralph.scan.plugins.puppet import (
 )
 
 
+from django.test.utils import override_settings
+from ralph.scan.errors import Error, NotConfiguredError
+from ralph.scan.plugins.puppet import (
+    get_puppet_providers,
+    PuppetAPIJsonProvider,
+    PuppetDBProvider,
+)
+
+
+existing_file = __file__  # test requires existing file
+fake_cert_config = {
+    v: existing_file for v in ['ca_cert', 'local_cert', 'local_cert_key']
+}
+
+
 class PuppetPluginTest(TestCase):
 
     def setUp(self):
@@ -129,4 +144,56 @@ class PuppetPluginTest(TestCase):
         self.assertEqual(
             _get_ip_addresses_hostnames_sets('127.0.0.1'),
             ({'127.0.0.1', '127.0.0.2'}, {'localhost'}),
+        )
+
+    def test_getting_puppet_providers_raise_exception(self):
+        self.assertRaises(NotConfiguredError, get_puppet_providers)
+
+    @override_settings(PUPPET_API_JSON_URL='https://server:port')
+    @mock.patch.object(PuppetDBProvider, '__init__')
+    @mock.patch.object(PuppetAPIJsonProvider, '__init__')
+    @override_settings(
+        PUPPET_DB_URL='mysql://user:password@server:port/db_name'
+    )
+    def test_api_proivider_is_first(self, db_provider, api_provider):
+        db_provider.return_value, api_provider.return_value = None, None
+        providers = get_puppet_providers()
+        self.assertEqual(len(providers), 2)
+        self.assertTrue(isinstance(providers[0], PuppetAPIJsonProvider))
+
+    @mock.patch.object(PuppetDBProvider, '__init__')
+    @override_settings(
+        PUPPET_DB_URL='mysql://user:password@server:port/db_name'
+    )
+    def test_db_provider_returned_when_no_api_provider(self, db_provider):
+        db_provider.return_value = None
+        providers = get_puppet_providers()
+        self.assertEqual(len(providers), 1)
+        self.assertTrue(isinstance(providers[0], PuppetDBProvider))
+
+
+class TestPuppetAPIJsonProvider(TestCase):
+
+    @mock.patch.object(PuppetAPIJsonProvider, '_get_data_for_hostname')
+    @override_settings(PUPPET_API_JSON_URL='https://server:port')
+    @override_settings(PUPPET_API_JSON_CERTS=fake_cert_config)
+    def test_get_facts_returns_facts(self, mocked):
+        mocked_data = [{'some_key': 'some_value'}, {}]
+        mocked.side_effect = mocked_data
+        provider = PuppetAPIJsonProvider('api_url')
+        hostnames = 'a' * len(mocked_data)
+        facts = provider.get_facts([], hostnames, [])
+        self.assertEqual(facts, mocked_data[0])
+
+    @mock.patch.object(PuppetAPIJsonProvider, '_get_data_for_hostname')
+    @override_settings(PUPPET_API_JSON_URL='https://server:port')
+    @override_settings(PUPPET_API_JSON_CERTS=fake_cert_config)
+    def test_get_facts_raises_multifounds(self, mocked):
+        data = {'some_key': 'some_value'}
+        mocked_data = [data, data]
+        mocked.side_effect = mocked_data
+        provider = PuppetAPIJsonProvider('api_url')
+        hostnames = 'a' * len(mocked_data)
+        self.assertRaises(
+            Error, provider.get_facts, [], hostnames, [],
         )
