ccf619ec534e2977682d3c1c1073b15a1e880e87
Szymon Pyżalski <szymon.pyzalski@allegro.pl>
2015-02-04 10:59:21 +0100

    Improved Xen detection
    The Xen servers > 6.5 don't contain 'xen' string in their snmp_name. We
    need to detect them using their HTTP response (looking for 'XenServer'
    string in the returned HTML document).
    
    Also - renamed the existing Dell detection test, so new tests can be
    added.

--- src/ralph/discovery/tests/test_http.py
+++ src/ralph/discovery/tests/test_http.py
@@ -13,7 +13,23 @@ from ralph.discovery.http import guess_family
 
 class DiscoveryHttpTest(TestCase):
 
-    def test_guess_family(self):
+    def test_guess_family_dell_when_data_from_dell_server(self):
         headers = {}
         family = guess_family(headers, DATA)
         self.assertEqual(family, 'Dell')
+
+    def test_guess_family_xen_when_data_from_xen(self):
+        headers = {}
+        family = guess_family(headers, """
+            <html>
+                <title>XenServer 6.5.0</title>
+                <head>
+                </head>
+                <body>
+                  <p/>Citrix Systems, Inc. XenServer 6.5.0
+                  <p/><a href="XenCenter.iso">XenCenter CD image</a>
+                  <p/><a href="XenCenterSetup.exe">XenCenter installer</a>
+                </body>
+            </html>"""
+        )
+        self.assertEqual(family, 'Xen')
