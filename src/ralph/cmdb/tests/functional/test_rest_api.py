3e9e8e41c880f176510bd079c866bb07eddb1ab6
Arkadiusz Bienert <arkadiusz.bienert@allegrogroup.com>
2015-05-06 12:14:04 +0200

    Do not trigger an error when changing service state to the same value

--- src/ralph/cmdb/tests/functional/test_rest_api.py
+++ src/ralph/cmdb/tests/functional/test_rest_api.py
@@ -41,3 +41,18 @@ class CMDBApiTest(UserTestCase):
         )
         chenged_service = CI.objects.get(id=service.id)
         self.assertEqual(chenged_service.state, CI_STATE_TYPES.INACTIVE.id)
+
+    def test_accept_fake_state_change(self):
+        """Don't trigger an error when a service with linked devices is being
+        patched into the same CI state it's already in
+        """
+        data = json.dumps({'state': CI_STATE_TYPES.ACTIVE.id})
+        service = ServiceCatalogFactory(state=CI_STATE_TYPES.ACTIVE.id)
+        device = DeviceFactory(service=service)
+
+        response = self.patch(
+            '/api/v0.9/ci/{}/'.format(service.id),
+            data,
+            CONTENT_TYPE='application/json',
+        )
+        self.assertTrue(response.status_code in [200, 201, 202, 204])
