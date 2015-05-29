2c3cb9f51f214b563c7a8a877d06e17475a6da56
Mateusz Kurek <master.mateusz@gmail.com>
2015-05-29 11:02:11 +0200

    added git to docker provisioning; added sitetree initialization

--- docker/init.sh
+++ docker/init.sh
@@ -8,3 +8,5 @@ sleep 15
 $RALPH_EXEC migrate --noinput
 $RALPH_EXEC createsuperuser --noinput --username ralph --email ralph@allegrogroup.com
 python $RALPH_DIR/docker/createsuperuser.py
+$RALPH_EXEC sitetreeload
+$RALPH_EXEC sitetree_resync_apps
