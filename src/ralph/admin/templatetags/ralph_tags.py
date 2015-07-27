7e23f5a1c05d4462ca072dca8e289f5260ee4095
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-07-27 12:03:32 +0200

    Added search to `edit-view`.
    Search now is added to edit views of objects (e.g: supports).

--- src/ralph/admin/templatetags/ralph_tags.py
+++ src/ralph/admin/templatetags/ralph_tags.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.contrib.admin.views.main import SEARCH_VAR
 from django.template import Library
 
 
@@ -11,3 +12,12 @@ def views_tabs(views, name=None, obj=None):
     Render extra views as tabs.
     """
     return {'views': views, 'name': name, 'object': obj}
+
+
+@register.inclusion_tag('admin/search_form.html')
+def contextual_search_form(search_url, search_fields):
+    return {
+        'search_url': search_url,
+        'search_fields': search_fields,
+        'search_var': SEARCH_VAR,
+    }
