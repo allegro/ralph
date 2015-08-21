b85f31f2e6bbcabb24d784275619b719ebe46105
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-08-21 11:34:53 +0200

    generated en translation files

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -94,17 +94,13 @@ DATABASES = {
 AUTH_USER_MODEL = 'accounts.RalphUser'
 
 LANGUAGE_CODE = 'en-us'
-
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 TIME_ZONE = 'Europe/Warsaw'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = False
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
