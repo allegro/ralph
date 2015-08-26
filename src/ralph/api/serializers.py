592e62b13acb1ed873cdac9a6485203e86965be2
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-08-26 18:42:14 +0200

    API improvements
    * improved serialization for save actions (POST, PUT, PATCH) - from now, there will be default serializer created for save actions, in which every relation will be serialized using `PrimaryKeyRelatedField` (so user should specify object PK in relation field). This serializer could be overwritten using `save_serializer_class` attribute in `RalphAPIViewSet`
    * serializer now returns both object PK and its url (see `get_default_field_names` for details)
    * improved passing context to subserializers - from now every serializer (even instantiated directly as a field in other serializer) will have current context set.
    * introduced `QuerysetRelatedMixin` which can easily handle `select_related` and `prefetch_related` on queryset. By default `select_related` is fill with related admin site `list_select_related` value
    * new `AdminSearchFieldsMixin` thanks to which search fields will be by default taken from related admin site.

--- src/ralph/api/serializers.py
+++ src/ralph/api/serializers.py
@@ -2,8 +2,9 @@
 import logging
 from collections import OrderedDict
 
-from rest_framework import serializers
+from rest_framework import permissions, serializers
 
+from ralph.api.relations import RalphHyperlinkedRelatedField, RalphRelatedField
 from ralph.lib.permissions.api import (
     PermissionsPerFieldSerializerMixin,
     RelatedObjectsPermissionsSerializerMixin
@@ -31,7 +32,10 @@ class ReversedChoiceField(serializers.ChoiceField):
         """
         Return choice name (value) instead of default key.
         """
-        return self.choices[obj]
+        try:
+            return self.choices[obj]
+        except KeyError:
+            return super().to_representation(obj)
 
     def to_internal_value(self, data):
         """
@@ -56,17 +60,77 @@ class RalphAPISerializerMixin(
         * handling field-level permissions (through
           `PermissionsPerFieldSerializerMixin`)
         * use `ReversedChoiceField` as default serializer for choice field
-        * request and user object easily accessible in serializer
+        * request and user object easily accessible in each related serializer
     """
     serializer_choice_field = ReversedChoiceField
-    # turn if on when all models are linked
-    # serializer_related_field = serializers.HyperlinkedRelatedField
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._request = self.context['request']
-        self._user = self._request.user
+    @property
+    def serializer_related_field(self):
+        """
+        When it's safe request (ex. GET), related serailizer is hyperlinked
+        (contains url to related object). When it's not safe request (ex. POST),
+        serializer expect to pass only PK for related object.
+        """
+        if self.context['request'].method in permissions.SAFE_METHODS:
+            return RalphHyperlinkedRelatedField
+        return RalphRelatedField
+
+    def get_default_field_names(self, declared_fields, model_info):
+        """
+        Return the default list of field names that will be used if the
+        `Meta.fields` option is not specified.
+
+        Add additional pk field at the beginning of fields list (it's not
+        included in `rest_framework.serializers.HyperlinkedModelSerializer`
+        by default).
+        """
+        return (
+            [model_info.pk.name] +
+            super().get_default_field_names(declared_fields, model_info)
+        )
+
+    def get_fields(self, *args, **kwargs):
+        """
+        Bind every returned field to self (as a parent)
+        """
+        fields = super().get_fields(*args, **kwargs)
+        # assign parent to every field as self
+        for field_name, field in fields.items():
+            if not field.parent:
+                field.parent = self
+        return fields
+
+    def build_field(self, field_name, info, model_class, nested_depth):
+        """
+        Attach context with request to every nested field which is another
+        serializer.
+        """
+        field_class, field_kwargs = super().build_field(
+            field_name, info, model_class, nested_depth
+        )
+        if issubclass(field_class, serializers.BaseSerializer):
+            field_kwargs.setdefault('context', {})['request'] = (
+                self.context.get('request')
+            )
+        return field_class, field_kwargs
+
+    def build_nested_field(self, field_name, relation_info, nested_depth):
+        """
+        Nested serializer is inheriting from `RalphAPISerializer`.
+        """
+        class NestedSerializer(RalphAPISerializer):
+            class Meta:
+                model = relation_info.related_model
+                depth = nested_depth - 1
+        field_class, field_kwargs = super().build_nested_field(
+            field_name, relation_info, nested_depth
+        )
+        field_class = NestedSerializer
+        return field_class, field_kwargs
 
 
-class RalphAPISerializer(RalphAPISerializerMixin, serializers.ModelSerializer):
+class RalphAPISerializer(
+    RalphAPISerializerMixin,
+    serializers.HyperlinkedModelSerializer
+):
     pass
--- src/ralph/api/tests/_base.py
+++ src/ralph/api/tests/_base.py
@@ -2,6 +2,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from rest_framework.test import APITestCase
 
 from ralph.tests.models import Foo
 
@@ -35,3 +36,36 @@ class APIPermissionsTestMixin(object):
         add_perm(cls.user1, 'delete_foo')
         add_perm(cls.user2, 'add_foo')
         add_perm(cls.user_not_staff, 'change_foo')
+
+
+class RalphAPITestCase(APITestCase):
+    """
+    Base test for Ralph API Test Case.
+
+    By default there are some users created.
+    """
+    @classmethod
+    def _create_users(cls):
+        def create_user(name, **kwargs):
+            params = dict(
+                username=name,
+                is_staff=True,
+                is_active=True,
+            )
+            params.update(kwargs)
+            return get_user_model().objects.create(**params)
+
+        cls.user1 = create_user('user1')
+        cls.user2 = create_user('user2')
+        cls.superuser = create_user(
+            'superuser', is_staff=True, is_superuser=True
+        )
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._create_users()
+
+    def setUp(self):
+        super().setUp()
+        self.client.force_authenticate(self.superuser)
