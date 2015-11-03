24b1fc4a70dbf87a711a56101aae7bc3b5bfdad2
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2015-11-03 12:39:13 +0000

    finished api

--- src/ralph/security/api.py
+++ src/ralph/security/api.py
@@ -1,47 +1,23 @@
 # -*- coding: utf-8 -*-
-#from ralph.api import RalphAPISerializer, RalphAPIViewSet, router
-#from ralph.supports.models import Support, SupportType
-#
-#
-#class SupportTypeSerializer(RalphAPISerializer):
-#    class Meta:
-#        model = SupportType
-#
-#
-#class SupportTypeViewSet(RalphAPIViewSet):
-#    queryset = SupportType.objects.all()
-#    serializer_class = SupportTypeSerializer
-#
-#
-#class SupportSerializer(RalphAPISerializer):
-#    class Meta:
-#        model = Support
-#        depth = 1
-#        # temporary - waiting for Polymorphic
-#        # (https://github.com/allegro/ralph/pull/1725)
-#        # we should create serializer for this field which will call
-#        # proper serializer for each type returned by Polymorphic or try to
-#        # use generic nested serializer for concrete type
-#        exclude = ('base_objects',)
-#
-#
-#class SupportViewSet(RalphAPIViewSet):
-#    queryset = Support.objects.all()
-#    serializer_class = SupportSerializer
-#
-#
-#router.register(r'supports', SupportViewSet)
-#router.register(r'support-types', SupportTypeViewSet)
-#urlpatterns = []
-
-
-
-
 from ralph.api import RalphAPISerializer, RalphAPIViewSet, router
 from ralph.security.models import SecurityScan, Vulnerability
 
 
+class VulnerabilitySerializer(RalphAPISerializer):
+
+    class Meta:
+        model = Vulnerability
+        depth = 1
+
+
+class VulnerabilityViewSet(RalphAPIViewSet):
+    queryset = Vulnerability.objects.all()
+    serializer_class = VulnerabilitySerializer
+
+
 class SecurityScanSerializer(RalphAPISerializer):
+    vulnerabilities = VulnerabilitySerializer(many=True)
+
     class Meta:
         model = SecurityScan
 
@@ -49,21 +25,6 @@ class SecurityScanSerializer(RalphAPISerializer):
 class SecurityScanViewSet(RalphAPIViewSet):
     queryset = SecurityScan.objects.all()
     serializer_class = SecurityScanSerializer
-
-
-class VulnerabilitySerializer(RalphAPISerializer):
-
-    class Meta:
-        model = Vulnerability
-        #TODO:: rm it
-        #exclude = ('security_scans', )
-        depth = 1
-
-
-
-class VulnerabilityViewSet(RalphAPIViewSet):
-    queryset = Vulnerability.objects.all()
-    serializer_class = VulnerabilitySerializer
 
 
 router.register(r'vulnerabilities', VulnerabilityViewSet)
--- src/ralph/security/models.py
+++ src/ralph/security/models.py
@@ -1,101 +1,39 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
-
-from dj.choices import Choices
-from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from ralph.accounts.models import Regionalizable
 from ralph.assets.models.base import BaseObject
-from ralph.lib.mixins.models import NamedMixin, TaggableMixin, TimeStampMixin
+from ralph.lib.mixins.models import TaggableMixin, TimeStampMixin
 from ralph.lib.permissions import PermByFieldMixin
 
 
 from ralph.security.choices import Risk, ScanStatus
 
-#class SupportType(NamedMixin, models.Model):
-#    """The type of a support"""
-#
-#
-#class SupportStatus(Choices):
-#    _ = Choices.Choice
-#
-#    SUPPORT = Choices.Group(0)
-#    new = _("new")
-#
-#
-#class Support(
-#    Regionalizable,
-#    PermByFieldMixin,
-#    NamedMixin.NonUnique,
-#    TimeStampMixin,
-#    TaggableMixin,
-#    models.Model,
-#):
-#    contract_id = models.CharField(max_length=50, blank=False)
-#    description = models.CharField(max_length=100, blank=True)
-#    # TODO: new attachment mechanism
-#    # attachments = models.ManyToManyField(
-#    #     models_assets.Attachment, null=True, blank=True
-#    # )
-#    price = models.DecimalField(
-#        max_digits=10, decimal_places=2, default=0, null=True, blank=True,
-#    )
-#    date_from = models.DateField(null=True, blank=True)
-#    date_to = models.DateField(null=False, blank=False)
-#    escalation_path = models.CharField(max_length=200, blank=True)
-#    contract_terms = models.CharField(max_length=200, blank=True)
-#    remarks = models.TextField(blank=True)
-#    sla_type = models.CharField(max_length=200, blank=True)
-#    # asset_type = models.PositiveSmallIntegerField(
-#    #     choices=AssetType()
-#    # )
-#    status = models.PositiveSmallIntegerField(
-#        default=SupportStatus.new.id,
-#        verbose_name=_("status"),
-#        choices=SupportStatus(),
-#        null=False,
-#        blank=False,
-#    )
-#    producer = models.CharField(max_length=100, blank=True)
-#    supplier = models.CharField(max_length=100, blank=True)
-#    serial_no = models.CharField(max_length=100, blank=True)
-#    invoice_no = models.CharField(max_length=100, blank=True, db_index=True)
-#    invoice_date = models.DateField(
-#        null=True, blank=True, verbose_name=_('Invoice date'),
-#    )
-#    period_in_months = models.IntegerField(null=True, blank=True)
-#    # property_of = models.ForeignKey(
-#    #     AssetOwner,
-#    #     on_delete=models.PROTECT,
-#    #     null=True,
-#    #     blank=True,
-#    # )
-#    support_type = models.ForeignKey(
-#        SupportType,
-#        on_delete=models.PROTECT,
-#        blank=True,
-#        default=None,
-#        null=True,
-#    )
-#    base_objects = models.ManyToManyField(BaseObject, related_name='supports')
-#
-#    def __init__(self, *args, **kwargs):
-#        self.saving_user = None
-#        super(Support, self).__init__(*args, **kwargs)
-#
-#    def get_natural_end_support(self):
-#        return naturaltime(datetime(*(self.date_to.timetuple()[:6])))
 
+class Vulnerability(
+    # Regionalizable,
+    PermByFieldMixin,
+    TimeStampMixin,
+    TaggableMixin,
+    models.Model,
+):
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=255,
+        unique=True)  # mysql unique 255
+    patch_deadline = models.DateTimeField(null=True, blank=True)
+    risk = models.PositiveIntegerField(choices=Risk())
+    external_vulnerability_id = models.IntegerField(
+        unique=True,  # id means id
+        null=True,
+        blank=True,
+        help_text=_('Id of vulnerability from external system'),
+    )
 
-
-from django.db import models
 
 class SecurityScan(
-    #Regionalizable,
+    # Regionalizable,
     PermByFieldMixin,
-    #NamedMixin.NonUnique,
     TimeStampMixin,
     TaggableMixin,
     models.Model,
@@ -106,22 +44,4 @@ class SecurityScan(
     details_url = models.URLField(max_length=255, blank=True)
     rescan_url = models.URLField(blank=True, verbose_name='Rescan url')
     asset = models.ForeignKey(BaseObject, null=True)
-
-class Vulnerability(
-    #Regionalizable,
-    PermByFieldMixin,
-    #NamedMixin.Unique,
-    TimeStampMixin,
-    TaggableMixin,
-    models.Model,
-):
-    name = models.CharField(verbose_name=_("name"), max_length=255, unique=True)  # mysql unique 255
-    patch_deadline = models.DateTimeField(null=True, blank=True)
-    risk = models.PositiveIntegerField(choices=Risk())
-    external_vulnerability_id = models.IntegerField(
-        unique=True,  # id means id
-        null=True,
-        blank=True,
-        help_text=_('Id of vulnerability from external system'),
-    )
-    security_scans = models.ManyToManyField(SecurityScan)
+    vulnerabilities = models.ManyToManyField(Vulnerability)
