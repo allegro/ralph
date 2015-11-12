b81b16c66de4210d2a11a322e6bda197c9710b63
Piotr Jarolewski <jarolewski.piotr@gmail.com>
2015-11-12 08:43:02 +0100

    Session in Redis

--- src/ralph/settings/prod.py
+++ src/ralph/settings/prod.py
@@ -13,3 +13,9 @@ REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
     # visualisation)
     'rest_framework.authentication.SessionAuthentication',
 )
+
+SESSION_ENGINE = 'redis_sessions.session'
+SESSION_REDIS_HOST = REDIS_CONNECTION['HOST']
+SESSION_REDIS_PORT = REDIS_CONNECTION['PORT']
+SESSION_REDIS_DB = REDIS_CONNECTION['DB']
+SESSION_REDIS_PREFIX = 'session'
