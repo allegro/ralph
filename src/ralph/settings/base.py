f5302e0da96d9dba62cb57ac6edcff37b42d6d76
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-11-13 13:53:52 +0100

    XML in API
    Added XML parser and renderer to REST API (using djangorestframework_xml)

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -170,6 +170,17 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'ralph.lib.permissions.api.PermissionsForObjectFilter',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework_xml.parsers.XMLParser',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',  # noqa
     'PAGE_SIZE': 10,
     'DEFAULT_METADATA_CLASS': 'ralph.lib.api.utils.RalphApiMetadata',
