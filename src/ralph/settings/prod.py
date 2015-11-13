6e7859dfeff83522610c7dd72c555652fae6f942
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-11-13 09:47:36 +0100

    Settings as environment variables
    * renamed db and redis settings names to be more generic
    * all (important) settings could be now set by environment variables
    * improved logging config

--- src/ralph/settings/prod.py
+++ src/ralph/settings/prod.py
@@ -2,12 +2,11 @@ import os
 
 from ralph.settings import *  # noqa
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'  # noqa
-STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
-
 # FIXME: when going for full production, change it to False
-
 DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'  # noqa
 
 REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
     'rest_framework.authentication.TokenAuthentication',
