0254b1f859e140fcc086d6f19b689f12bc81184f
Marcin Kliks <marcin.kliks@allegro.pl>
2015-11-17 17:33:49 +0000

    Added domain management system.
    This module manages ownership, purpose and price aspects of a domains.
    For more information read the docs/domains/overview.md documentation.

---
+++ src/ralph/domains/admin.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+from ralph.admin import RalphAdmin, RalphTabularInline, register
+from ralph.attachments.admin import AttachmentsMixin
+from ralph.domains.models.domains import (
+    Domain,
+    DomainContract,
+    DomainRegistrant
+)
+
+
+class DomainContractInline(RalphTabularInline):
+    model = DomainContract
+    extra = 0
+
+
+@register(Domain)
+class DomainAdmin(AttachmentsMixin, RalphAdmin):
+    list_select_related = ['technical_owner', 'business_owner', 'domain_holder']
+    list_filter = [
+        'name', 'service_env',
+    ]
+    list_display = [
+        'name', 'parent', 'business_owner',
+        'technical_owner', 'domain_holder'
+    ]
+    fieldsets = (
+        (_('Basic info'), {
+            'fields': (
+                'name', 'parent', 'remarks', 'domain_status',
+            )
+        }),
+        (_('Ownership info'), {
+            'fields': (
+                'service_env',
+                'business_segment', 'business_owner',
+                'technical_owner', 'domain_holder'
+            )
+        })
+    )
+    search_fields = ['name', ]
+    inlines = (DomainContractInline, )
+
+
+@register(DomainContract)
+class DomainContractAdmin(AttachmentsMixin, RalphAdmin):
+    list_select_related = ['domain', 'domain__service_env']
+    list_filter = [
+        'domain__name', 'domain__service_env__service__name'
+    ]
+    list_display = [
+        'domain', 'expiration_date', 'price'
+    ]
+    fieldsets = (
+        (_('Basic info'), {
+            'fields': (
+                'domain', 'expiration_date', 'registrant'
+
+            )
+        }),
+        (_('Financial info'), {
+            'fields': (
+                'price',
+            )
+        }),
+    )
+    search_fields = ['domain__name', ]
+
+
+@register(DomainRegistrant)
+class DomainRegistrantAdmin(RalphAdmin):
+    pass
---
+++ src/ralph/domains/models/domains.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+from dj.choices import Choices
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from ralph.assets.models import BaseObject
+from ralph.assets.models.assets import BusinessSegment
+from ralph.back_office.models import AssetHolder
+from ralph.lib.mixins.models import (
+    AdminAbsoluteUrlMixin,
+    NamedMixin,
+    TimeStampMixin
+)
+from ralph.lib.permissions import PermByFieldMixin
+
+
+class DomainRegistrant(
+    PermByFieldMixin,
+    NamedMixin,
+    TimeStampMixin,
+    models.Model,
+):
+    pass
+
+
+class DomainStatus(Choices):
+    _ = Choices.Choice
+
+    active = _('Active')
+    pending_lapse = _('Pending lapse')
+    pending_transfer = _('Pending transfer away')
+    lapsed = _('Lapsed (inactive)')
+    transfered_away = _('Transfered away')
+
+
+class Domain(BaseObject, AdminAbsoluteUrlMixin):
+    name = models.CharField(
+        verbose_name=_('Domain name'),
+        help_text=_('Full domain name'),
+        unique=True,
+        max_length=255
+    )
+    domain_status = models.PositiveIntegerField(
+        choices=DomainStatus(),
+        default=DomainStatus.active.id,
+    )
+    business_segment = models.ForeignKey(
+        BusinessSegment, blank=True, null=True,
+        help_text=_("Business segment for a domain")
+    )
+    business_owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='domaincontract_business_owner',
+        blank=True,
+        null=True,
+        help_text=_("Business contact person for a domain")
+    )
+    technical_owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='domaincontract_technical_owner',
+        blank=True,
+        null=True,
+        help_text=_("Technical contact person for a domain")
+    )
+    domain_holder = models.ForeignKey(
+        AssetHolder,
+        blank=True,
+        null=True,
+        help_text=_("Company which receives invoice for the domain")
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class DomainContract(
+    AdminAbsoluteUrlMixin,
+    PermByFieldMixin,
+    TimeStampMixin,
+    models.Model,
+):
+    domain = models.ForeignKey(Domain)
+    expiration_date = models.DateField(null=True, blank=True)
+    registrant = models.ForeignKey('DomainRegistrant', null=True, blank=True)
+    price = models.DecimalField(
+        null=True, blank=True, decimal_places=2, max_digits=15,
+        help_text=_("Price for domain renewal for given period"),
+        verbose_name=_("Price"))
+
+    def __str__(self):
+        return "{domain_name} - {expiration_date}".format(
+            domain_name=self.domain.name,
+            expiration_date=self.expiration_date
+        )
+        return self.hostname
