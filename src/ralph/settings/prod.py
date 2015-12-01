ff7724f065f75180595e7e40417b87914caa236d
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-12-01 09:57:27 +0100

    redis cache

--- src/ralph/settings/prod.py
+++ src/ralph/settings/prod.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from ralph.settings import *  # noqa
@@ -22,3 +23,35 @@ if os.environ.get('STORE_SESSIONS_IN_REDIS'):
     SESSION_REDIS_DB = REDIS_CONNECTION['DB']
     SESSION_REDIS_PASSWORD = REDIS_CONNECTION['PASSWORD']
     SESSION_REDIS_PREFIX = 'session'
+
+if os.environ.get('USE_REDIS_CACHE'):
+    DEFAULT_CACHE_OPTIONS = {
+        'DB': os.environ.get('REDIS_CACHE_DB', REDIS_CONNECTION['DB']),
+        'PASSWORD': os.environ.get(
+            'REDIS_CACHE_PASSWORD', REDIS_CONNECTION['PASSWORD']
+        ),
+        'PARSER_CLASS': os.environ.get(
+            'REDIS_CACHE_PARSER', 'redis.connection.HiredisParser'
+        ),
+        'CONNECTION_POOL_CLASS': os.environ.get(
+            'REDIS_CACHE_CONNECTION_POOL_CLASS',
+            'redis.BlockingConnectionPool'
+        ),
+        'PICKLE_VERSION': -1,
+    }
+    CACHES = {
+        'default': {
+            'BACKEND': os.environ.get(
+                'REDIS_CACHE_BACKEND', 'redis_cache.RedisCache'
+            ),
+            'LOCATION': json.loads(
+                os.environ.get('REDIS_CACHE_LOCATION', '"{}:{}"'.format(
+                    REDIS_CONNECTION['HOST'], REDIS_CONNECTION['PORT']
+                ))
+            ),
+            'OPTIONS': (
+                json.loads(os.environ.get('REDIS_CACHE_OPTIONS', "{}")) or
+                DEFAULT_CACHE_OPTIONS
+            ),
+        },
+    }
