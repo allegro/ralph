fcd44d1cd28f6579f1b46966739bb8194533bcc1
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-01-18 14:20:39 +0000

    working popup flow (broken), though dirty

--- src/ralph/static/src/js/ralph.init.js
+++ src/ralph/static/src/js/ralph.init.js
@@ -1,2 +1,21 @@
 var ralph = ralph || {};
 ralph.jQuery = jQuery.noConflict();
+
+function dismissRelatedLookupPopup(win, chosenId) {
+    var name = windowname_to_id(win.name);
+/*
+    var elem = document.getElementById(name);
+    if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
+        elem.value += ',' + chosenId;
+    } else {
+        document.getElementById(name).value = chosenId;
+    }
+*/
+
+    console.log('name', name);
+    var elem = document.querySelector('#' + name);
+    elem.xxx(chosenId);
+    win.close();
+}
+
+
