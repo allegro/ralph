allegro/ralph

commit ed8f859e1679ba68b0db4b0e2b374913136f54f5

Buyout date in my equipment page and add depreciation rate to model category

src/ralph/accounts/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse
 from django.db.models import Q
+from django.forms.models import model_to_dict
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
@@ -17,7 +18,7 @@ from ralph.back_office.models import BackOfficeAsset
 from ralph.lib.permissions.admin import PermissionAdminMixin
 from ralph.lib.table import Table
 from ralph.lib.transitions.models import TransitionsHistory
-from ralph.licences.models import BaseObjectLicence, Licence
+from ralph.licences.models import Licence
 
 # use string for whole app (app_label) or tuple (app_label, model_name) to
 # exclude particular model
@@ -79,16 +80,18 @@ class AssetList(Table):
         return '<a href="{}">{}</a>'.format(
             reverse(
                 'admin:back_office_backofficeasset_change',
-                args=(item['id'],)
+                args=(item.id,)
             ),
             _('go to asset')
         )
     url.title = _('Link')
 
+    def buyout_date(self, item):
+        return item.buyout_date
+    buyout_date.title = _('Buyout date')
+
     def user_licence(self, item):
-        licences = BaseObjectLicence.objects.filter(
-            base_object=item['id']
-        ).select_related('licence')
+        licences = item.licences.select_related('licence')
         if licences:
             result = [
                 '<a href="{}">{}</a><br />'.format(
@@ -104,6 +107,7 @@ class AssetList(Table):
             return []
 
     def report_failure(self, item):
+        item = model_to_dict(item)
         url = settings.MY_EQUIPMENT_REPORT_FAILURE_URL
         if url:
             if self.request and 'username' not in item:
@@ -128,7 +132,7 @@ class AssignedLicenceList(Table):
         return '<a href="{}">{}</a>'.format(
             reverse(
                 'admin:licences_licence_change',
-                args=(item['id'],)
+                args=(item.id,)
             ),
             _('go to licence')
         )
@@ -160,7 +164,8 @@ class UserInfoMixin(object):
             self.get_asset_queryset(),
             [
                 'id', 'model__category__name', 'model__manufacturer__name',
-                'model__name', 'sn', 'barcode', 'remarks', 'status', 'url'
+                'model__name', 'sn', 'barcode', 'remarks', 'status',
+                'buyout_date', 'url'
             ],
             ['user_licence']
         )

src/ralph/back_office/admin.py
@@ -78,6 +78,7 @@ class BackOfficeAssetAdmin(
 ):
 
     """Back Office Asset admin class."""
+    add_form_template = 'backofficeasset/add_form.html'
     form = BackOfficeAssetAdminForm
     actions = ['bulk_edit_action']
     show_transition_history = True

.travis.yml
@@ -5,9 +5,9 @@ env:
         - DATABASE_USER=travis
         - DATABASE_PASSWORD=travis
     matrix:
-        - DJANGO_VERSION=1.8.7 TEST_DB_ENGINE=sqlite
-        - DJANGO_VERSION=1.8.7 TEST_DB_ENGINE=mysql
-        - DJANGO_VERSION=1.8.7 TEST_DB_ENGINE=psql
+        - DJANGO_VERSION=1.8.8 TEST_DB_ENGINE=sqlite
+        - DJANGO_VERSION=1.8.8 TEST_DB_ENGINE=mysql
+        - DJANGO_VERSION=1.8.8 TEST_DB_ENGINE=psql
         - DJANGO_VERSION=1.9 TEST_DB_ENGINE=sqlite
         - DJANGO_VERSION=1.9 TEST_DB_ENGINE=mysql
         - DJANGO_VERSION=1.9 TEST_DB_ENGINE=psql

src/ralph/accounts/views.py
@@ -14,7 +14,7 @@ class UserProfileView(RalphTemplateView):
 class MyEquipmentAssetList(AssetList):
     def user_licence(self, item):
         licences = BaseObjectLicence.objects.filter(
-            base_object=item['id']
+            base_object=item.id
         ).select_related('licence', 'licence__software')
         if licences:
             result = [
@@ -37,9 +37,11 @@ class CurrentUserInfoView(UserInfoMixin, RalphBaseTemplateView):
         asset_fields = [
             ('barcode', _('Barcode / Inventory Number')),
             'model__category__name', 'model__manufacturer__name',
-            'model__name', ('sn', _('Serial Number')), 'invoice_date',
-            'status'
+            'model__name', ('sn', _('Serial Number')), 'invoice_date', 'status'
         ]
+        if settings.MY_EQUIPMENT_SHOW_BUYOUT_DATE:
+            asset_fields += ['buyout_date']
+
         if settings.MY_EQUIPMENT_REPORT_FAILURE_URL:
             asset_fields += ['report_failure']
         context['asset_list'] = MyEquipmentAssetList(

src/ralph/assets/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
+from rest_framework import serializers
 
 from ralph.accounts.api_simple import SimpleRalphUserSerializer
 from ralph.accounts.models import Team
@@ -136,11 +137,19 @@ class ManufacturerSerializer(RalphAPISerializer):
 
 
 class CategorySerializer(RalphAPISerializer):
+
+    depreciation_rate = serializers.FloatField(
+        source='get_default_depreciation_rate'
+    )
+
     class Meta:
         model = Category
 
 
 class AssetModelSerializer(RalphAPISerializer):
+
+    category = CategorySerializer()
+
     class Meta:
         model = AssetModel
 

src/ralph/assets/migrations/0005_category_depreciation_rate.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assets', '0004_auto_20151204_0758'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='default_depreciation_rate',
+            field=models.DecimalField(help_text='This value is in percentage. For example value: "100" means it depreciates during a year. Value: "25" means it depreciates during 4 years, and so on... .', decimal_places=2, max_digits=5, blank=True, default=25),
+        ),
+    ]

src/ralph/assets/models/assets.py
@@ -186,6 +186,17 @@ class Category(MPTTModel, NamedMixin.NonUnique, TimeStampMixin, models.Model):
         db_index=True
     )
     imei_required = models.BooleanField(default=False)
+    default_depreciation_rate = models.DecimalField(
+        blank=True,
+        decimal_places=2,
+        default=settings.DEFAULT_DEPRECIATION_RATE,
+        help_text=_(
+            'This value is in percentage.'
+            ' For example value: "100" means it depreciates during a year.'
+            ' Value: "25" means it depreciates during 4 years, and so on... .'
+        ),
+        max_digits=5,
+    )
 
     class Meta:
         verbose_name = _('category')
@@ -196,6 +207,16 @@ class Category(MPTTModel, NamedMixin.NonUnique, TimeStampMixin, models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_default_depreciation_rate(self, category=None):
+        if category is None:
+            category = self
+
+        if category.default_depreciation_rate:
+            return category.default_depreciation_rate
+        elif category.parent:
+            return self.get_default_depreciation_rate(category.parent)
+        return 0
 
 
 class AssetLastHostname(models.Model):
@@ -341,7 +362,26 @@ class Asset(AdminAbsoluteUrlMixin, BaseObject):
     def __str__(self):
         return self.hostname
 
-    def get_deprecation_months(self):
+    @property
+    def buyout_date(self):
+        """
+        Get buyout date.
+
+        Calculate buyout date invoice_date + depreciation_rate months
+
+        Returns:
+            Deprecation date
+        """
+        if self.depreciation_end_date:
+            return self.depreciation_end_date
+        elif self.invoice_date:
+            return self.invoice_date + relativedelta(
+                months=self.get_depreciation_months() + 1
+            )
+        else:
+            return None
+
+    def get_depreciation_months(self):
         return int(
             (1 / (self.depreciation_rate / 100) * 12)
             if self.depreciation_rate else 0
@@ -355,14 +395,14 @@ class Asset(AdminAbsoluteUrlMixin, BaseObject):
             deprecation_date = self.deprecation_end_date
         else:
             deprecation_date = self.invoice_date + relativedelta(
-                months=self.get_deprecation_months(),
+                months=self.get_depreciation_months(),
             )
         return deprecation_date < date
 
     def get_depreciated_months(self):
         # DEPRECATED
         # BACKWARD_COMPATIBILITY
-        return self.get_deprecation_months()
+        return self.get_depreciation_months()
 
     def is_deprecated(self, date=None):
         # DEPRECATED

src/ralph/back_office/templates/backofficeasset/add_form.html
@@ -1,0 +1,22 @@
+{% extends "admin/change_form.html" %}
+{% block admin_change_form_document_ready %}
+  <script type="text/javascript">
+  (function($) {
+    $(function () {
+      "use strict";
+      $('#id_model').change(function() {
+        var url = '{% url 'assetmodel-list' %}' + $(this).val();
+        $.ajax({
+          url: url,
+        }).done(function(data) {
+          if (data.category.depreciation_rate) {
+            $('#id_depreciation_rate').val(
+              data.category.depreciation_rate
+            );
+          }
+        });
+      });
+    });
+  })(ralph.jQuery);
+  </script>
+{% endblock %}

commit 03979d368f885d566822ca101fb23ad17e2b573d

Fixed report failure url formatting on my equipment tab
ed8f859 introduced bug with formatting report failure url on my equipment tab - only first-level fields could be used. This commit fixes this bug - nested fields (using __ convention) could be used in report failure url as well.

src/ralph/accounts/admin.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from string import Formatter
 from urllib.parse import quote
 
 from django.conf import settings
@@ -12,6 +13,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from ralph.accounts.models import RalphUser, Region, Team
 from ralph.admin import RalphAdmin, register
+from ralph.admin.helpers import getattr_dunder
 from ralph.admin.mixins import RalphAdminFormMixin
 from ralph.admin.views.extra import RalphDetailView
 from ralph.back_office.models import BackOfficeAsset
@@ -107,11 +109,17 @@ class AssetList(Table):
             return []
 
     def report_failure(self, item):
-        item = model_to_dict(item)
+        item_dict = model_to_dict(item)
         url = settings.MY_EQUIPMENT_REPORT_FAILURE_URL
         if url:
-            if self.request and 'username' not in item:
-                item['username'] = self.request.user.username
+            placeholders = [
+                k[1] for k in Formatter().parse(url) if k[1] is not None
+            ]
+            item_dict.update({
+                k: getattr_dunder(item, k) for k in placeholders
+            })
+            if self.request and 'username' not in item_dict:
+                item_dict['username'] = self.request.user.username
 
             def escape_param(p):
                 """
@@ -119,7 +127,9 @@ class AssetList(Table):
                 """
                 return quote(str(p).replace('"', '\u2033'))
             return '<a href="{}" target="_blank">{}</a><br />'.format(
-                url.format(**{k: escape_param(v) for (k, v) in item.items()}),
+                url.format(
+                    **{k: escape_param(v) for (k, v) in item_dict.items()}
+                ),
                 _('Report failure')
             )
         return ''