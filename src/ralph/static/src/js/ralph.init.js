481ce69c4895f0f0dea600e5f41cd3ca4faf1343
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-01-20 12:47:44 +0000

    working editing with update

--- src/ralph/static/src/js/ralph.init.js
+++ src/ralph/static/src/js/ralph.init.js
@@ -1,9 +1,15 @@
 var ralph = ralph || {};
 ralph.jQuery = jQuery.noConflict();
 
-function dismissRelatedLookupPopup(win, chosenId) {
+function getCallerNode(win) {
+    console.log('getCallerNode', win);
     var name = windowname_to_id(win.name);
+    var elem = document.querySelector('#' + name);
+    return elem;
+}
+function dismissRelatedLookupPopup(win, chosenId) {
 /*
+    var name = windowname_to_id(win.name);
     var elem = document.getElementById(name);
     if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
         elem.value += ',' + chosenId;
@@ -11,8 +17,29 @@ function dismissRelatedLookupPopup(win, chosenId) {
         document.getElementById(name).value = chosenId;
     }
 */
-    console.log('name', name);
-    var elem = document.querySelector('#' + name);
+    elem = getCallerNode(win);
     elem.updateById(chosenId);
     win.close();
 }
+
+
+function dismissChangeRelatedObjectPopup(win, objId, newRepr, newId) {
+/*
+    objId = html_unescape(objId);
+    xnewRepr = html_unescape(newRepr);
+    var id = windowname_to_id(win.name).replace(/^edit_/, '');
+    var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
+    var selects = django.jQuery(selectsSelector);
+    selects.find('option').each(function() {
+        if (this.value === objId) {
+            this.innerHTML = xnewRepr;
+            this.value = newId;
+        }
+    });
+    debugger;
+*/
+    elem = getCallerNode(win);
+    elem.reloadBadges();
+    win.close();
+}
+
