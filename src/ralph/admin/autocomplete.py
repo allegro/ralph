8e819b51384ac8d6d8e48873b0a9f44beb6b92a0
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-01-21 08:41:03 +0000

    replace static change_id_rack with dynamic value

--- src/ralph/admin/autocomplete.py
+++ src/ralph/admin/autocomplete.py
@@ -70,9 +70,11 @@ class SuggestView(JsonViewMixin, View):
                 'edit_url': '{}?_popup=1'.format(
                     get_admin_url(obj, 'change')
                 ) if can_edit else None,
-                'tooltip': getattr(obj, 'autocomplete_tooltip', None)
+                'tooltip': getattr(obj, 'autocomplete_tooltip', None),
+                'edit_icon_id': 'change_id_' + self.model._meta.model_name,
             } for obj in self.get_queryset(request.user)
         ]
+        print(results)
         return self.render_to_json_response({'results': results})
 
 
