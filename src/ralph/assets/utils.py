c691366bb7d4ebf07638f6a446be8eecdaa3f36c
Piotr Jarolewski <piotr.jarolewski@allegrogroup.com>
2016-02-16 13:08:50 +0100

    Add exclude fields to move parents model

--- src/ralph/assets/utils.py
+++ src/ralph/assets/utils.py
@@ -3,7 +3,7 @@ from django.db import transaction
 
 
 @transaction.atomic
-def move_parents_models(from_obj, to_obj):
+def move_parents_models(from_obj, to_obj, exclude_copy_fields=None):
     """
     Move parents Django model to diffrent model inheriting from
     the same parent.
@@ -14,13 +14,21 @@ def move_parents_models(from_obj, to_obj):
     Args:
         from_obj: From Django model object
         to_obj: To Django modelo object
+        exclude_copy_fields: Remove field from object dict
 
     Example:
         >> instance = DataCenterAsset.objects.get(pk=1)
         >> back_office_asset = BackOfficeAsset()
         >> move_parents_models(instance, back_office_asset)
     """
-    to_obj.__dict__.update(from_obj.__dict__)
+    from_dict = from_obj.__dict__
+    if exclude_copy_fields:
+        for field_name in exclude_copy_fields:
+            field = to_obj._meta.get_field(field_name)
+            from_dict.pop(field.name, None)
+            from_dict.pop(field.attname, None)
+
+    to_obj.__dict__.update(from_dict)
     to_obj.content_type = None
     to_obj.save()
     parents = from_obj._meta.parents
