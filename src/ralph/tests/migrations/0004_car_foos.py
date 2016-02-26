allegro/ralph

commit 53c8dd382ecde1ed48e6b901c981ef9dd8c6428f

Change get_queryset in django popup view.

src/ralph/admin/views/main.py
@@ -55,7 +55,13 @@ class RalphChangeList(ChangeList):
         return ordering
 
     def get_queryset(self, request):
-        queryset = None
+        queryset = super().get_queryset(request)
         if self.is_popup:
-            queryset = getattr(self.model, 'get_autocomplete_queryset', None)
-        return queryset() if queryset else super().get_queryset(request)
+            # For popup window we limit display of records to the same as
+            # we have in the autocomplete widget.
+            autocomplete_queryset = getattr(
+                self.model, 'get_autocomplete_queryset', None
+            )
+            if autocomplete_queryset:
+                queryset = queryset & autocomplete_queryset()
+        return queryset

src/ralph/api/tests/test_viewsets.py
@@ -191,5 +191,5 @@ class TestAdminSearchFieldsMixin(RalphTestCase):
     def test_get_filter_fields_from_admin(self):
         cvs = CarViewSet()
         self.assertEqual(
-            cvs.filter_fields, ['manufacturer__name', 'year']
+            cvs.filter_fields, ['manufacturer__name', 'name', 'year']
         )

src/ralph/tests/admin.py
@@ -9,6 +9,7 @@ from ralph.tests.models import Bar, Car, Car2, Manufacturer, Order
 class CarAdmin(RalphAdmin):
     ordering = ['name']
     list_filter = ['year']
+    search_fields = ['name']
 
 
 @register(Bar)

src/ralph/tests/models.py
@@ -39,6 +39,10 @@ class Car(models.Model):
     manufacturer._autocomplete = False
     manufacturer._filter_title = 'test'
 
+    @classmethod
+    def get_autocomplete_queryset(cls):
+        return cls.objects.filter(year=2015)
+
 
 class Car2(models.Model):
     manufacturer = models.ForeignKey(Manufacturer)

src/ralph/admin/tests/tests_functional.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from ddt import data, ddt, unpack
+from django.contrib.admin.options import IS_POPUP_VAR
+from django.contrib.admin.views.main import SEARCH_VAR
 from django.core.urlresolvers import reverse
 from django.test import RequestFactory, TestCase
 from django.views.generic import View
@@ -9,9 +11,9 @@ from ralph.admin.decorators import register_extra_view
 from ralph.admin.sites import ralph_site
 from ralph.admin.views.extra import RalphDetailView, RalphListView
 from ralph.admin.views.main import RalphChangeList
-from ralph.tests.admin import CarAdmin
+from ralph.tests.admin import CarAdmin, ManufacturerAdmin
 from ralph.tests.mixins import ClientMixin, ReloadUrlsMixin
-from ralph.tests.models import Car, Foo
+from ralph.tests.models import Car, Foo, Manufacturer
 
 
 class ExtraListView(RalphListView, View):
@@ -211,6 +213,31 @@ class ExtraViewsTest(ReloadUrlsMixin, ClientMixin, TestCase):
 
 @ddt
 class ChangeListTest(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        manufacturer = Manufacturer.objects.create(
+            name='test', country='pl'
+        )
+        Manufacturer.objects.create(
+            name='test2', country='pl2'
+        )
+        Car.objects.create(
+            year=2015,
+            name='test',
+            manufacturer=manufacturer
+        )
+        Car.objects.create(
+            year=2014,
+            name='AutotompleteTest 2',
+            manufacturer=manufacturer
+        )
+        Car.objects.create(
+            year=2015,
+            name='AutotompleteTest',
+            manufacturer=manufacturer
+        )
+
     def _change_list_factory(
         self, model, model_admin, request, list_display=None
     ):
@@ -225,7 +252,7 @@ class ChangeListTest(TestCase):
             list_select_related=model_admin.list_select_related,
             search_fields=model_admin.search_fields,
             model=model,
-            model_admin=model_admin(Car, ralph_site),
+            model_admin=model_admin(model, ralph_site),
             request=request,
         )
 
@@ -307,3 +334,38 @@ class ChangeListTest(TestCase):
             Car, CarAdmin, ordering_params, list_display,
         )
         self.assertEqual(expected_ordering, ordering)
+
+    def test_get_queryset_autocomplete(self):
+        request = RequestFactory().get('/car', data={IS_POPUP_VAR: 1})
+        change_list = self._change_list_factory(
+            model=Car,
+            model_admin=CarAdmin,
+            request=request,
+            list_display=['id']
+        )
+        resutlt = change_list.get_queryset(request)
+        self.assertEqual(len(resutlt), 2)
+
+    def test_get_queryset_aucotomplete_search(self):
+        request = RequestFactory().get('/car', data={
+            IS_POPUP_VAR: 1, SEARCH_VAR: 'autotompletetest'}
+        )
+        change_list = self._change_list_factory(
+            model=Car,
+            model_admin=CarAdmin,
+            request=request,
+            list_display=['id']
+        )
+        resutlt = change_list.get_queryset(request)
+        self.assertEqual(len(resutlt), 1)
+
+    def test_get_queryset(self):
+        request = RequestFactory().get('/manufacturer', data={IS_POPUP_VAR: 1})
+        change_list = self._change_list_factory(
+            model=Manufacturer,
+            model_admin=ManufacturerAdmin,
+            request=request,
+            list_display=['id']
+        )
+        resutlt = change_list.get_queryset(request)
+        self.assertEqual(len(resutlt), 2)

commit 30669cbc1c72ca96e1506e2fd2a81f0d862bf5e7

Fixed autocomplete unique queryset bug
Fixed #2248 bug introduced in 53c8dd3 - distinct (unique) queryset cannot be combined with non-distinct (non-unique) queryset.
Solution: if one of the querysets is disctinct, make all of them distinct.

src/ralph/admin/views/main.py
@@ -63,5 +63,18 @@ class RalphChangeList(ChangeList):
                 self.model, 'get_autocomplete_queryset', None
             )
             if autocomplete_queryset:
-                queryset = queryset & autocomplete_queryset()
+                autocomplete_queryset = autocomplete_queryset()
+                # #2248 - cannot combine unique (distinct) and non-unique query
+                # if one of the queries is distinct, make sure all are distinct
+                if (
+                    queryset.query.distinct and
+                    not autocomplete_queryset.query.distinct
+                ):
+                    autocomplete_queryset = autocomplete_queryset.distinct()
+                if (
+                    not queryset.query.distinct and
+                    autocomplete_queryset.query.distinct
+                ):
+                    queryset = queryset.distinct()
+                queryset = queryset & autocomplete_queryset
         return queryset

src/ralph/api/tests/test_viewsets.py
@@ -191,5 +191,6 @@ class TestAdminSearchFieldsMixin(RalphTestCase):
     def test_get_filter_fields_from_admin(self):
         cvs = CarViewSet()
         self.assertEqual(
-            cvs.filter_fields, ['manufacturer__name', 'name', 'year']
+            cvs.filter_fields,
+            ['manufacturer__name', 'name', 'foos__bar', 'year']
         )

src/ralph/tests/admin.py
@@ -9,7 +9,7 @@ from ralph.tests.models import Bar, Car, Car2, Manufacturer, Order
 class CarAdmin(RalphAdmin):
     ordering = ['name']
     list_filter = ['year']
-    search_fields = ['name']
+    search_fields = ['name', 'foos__bar']
 
 
 @register(Bar)

src/ralph/tests/migrations/0004_car_foos.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tests', '0003_bar'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='car',
+            name='foos',
+            field=models.ManyToManyField(to='tests.Foo'),
+        ),
+    ]

src/ralph/tests/models.py
@@ -38,6 +38,7 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer)
     manufacturer._autocomplete = False
     manufacturer._filter_title = 'test'
+    foos = models.ManyToManyField(Foo)
 
     @classmethod
     def get_autocomplete_queryset(cls):