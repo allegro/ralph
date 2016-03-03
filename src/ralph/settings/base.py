b6480b57849fe47e8ee7d0a4626c64a0cecbd143
Yongzhi Pan <panyongzhi@gmail.com>
2016-03-03 14:14:01 +0800

    USE_TZ = True.

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -124,7 +124,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
+USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
