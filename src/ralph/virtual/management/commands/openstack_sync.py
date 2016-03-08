8c721b6a40ab0001cd8314aa591efb45e866f403
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-03-08 11:57:00 +0000

    Fixed openstack_sync command.
    TestOpenstackSync ignored timezone of created field.
    Now it doesn't.

--- src/ralph/virtual/management/commands/openstack_sync.py
+++ src/ralph/virtual/management/commands/openstack_sync.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from functools import lru_cache
 
 import reversion as revisions
+from dateutil import parser
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
@@ -306,8 +307,7 @@ class Command(BaseCommand):
 
         # workaround - created field has auto_now_add attribute
         new_server.save()
-        new_server.created = datetime.strptime(openstack_server['created'],
-                                               self.DATETIME_FORMAT)
+        new_server.created = parser.parse(openstack_server['created'])
         self._save_object(new_server, 'add server %s'
                           % new_server.hostname)
 
