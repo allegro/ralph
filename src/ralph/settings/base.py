c77c9e7962bf52a57998d29abc97feab214840b0
Yongzhi Pan <panyongzhi@gmail.com>
2016-03-11 11:22:37 +0800

    Honor TIME_ZONE variable.

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -122,7 +122,7 @@ LOGIN_URL = '/login/'
 
 LANGUAGE_CODE = 'en-us'
 LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
-TIME_ZONE = 'Europe/Warsaw'
+TIME_ZONE = os.environ.get('TIME_ZONE', 'Europe/Warsaw')
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
