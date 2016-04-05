392f4fbc84c62c51c9f90c380993e4854db945ba
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-04-05 15:41:27 +0200

    Fixed service-objects tab bugs
    * added PermByFieldMixin to service
    * Service fields are specified explicitly
    * fixed objects tab number of queries

---
+++ src/ralph/assets/migrations/0010_auto_20160405_1531.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assets', '0009_auto_20160307_1138'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='service',
+            options={},
+        ),
+    ]
