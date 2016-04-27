ac407f170139534b335936d4ce4ca4cfe4b4a6d0
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-04-27 07:33:05 +0200

    fixing docker build - wip

--- src/ralph/settings/dev.py
+++ src/ralph/settings/dev.py
@@ -30,3 +30,8 @@ LOGGING['handlers']['console']['level'] = 'DEBUG'
 for logger in LOGGING['loggers']:
     LOGGING['loggers'][logger]['level'] = 'DEBUG'
     LOGGING['loggers'][logger]['handlers'].append('console')
+
+INTERNAL_IPS = (
+        '0.0.0.0',
+        '127.0.0.1',
+)
