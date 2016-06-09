02aa1dca9847d847b8f5b2099ec3dda06a83d9aa
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-06-09 16:32:06 +0200

    WIP

--- src/ralph/cross_validator/migrations/0001_initial.py
+++ src/ralph/cross_validator/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import ralph.lib.mixins.fields
 import django_extensions.db.fields.json
 
 
@@ -14,16 +15,28 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Ignored',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('object_pk', models.IntegerField(db_index=True)),
+                ('field', models.CharField(max_length=50, db_index=True)),
+                ('old', ralph.lib.mixins.fields.NullableCharField(max_length=255, null=True, db_index=True)),
+                ('new', ralph.lib.mixins.fields.NullableCharField(max_length=255, null=True, db_index=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+        migrations.CreateModel(
             name='Result',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('created', models.DateTimeField(verbose_name='date created', auto_now_add=True)),
                 ('modified', models.DateTimeField(verbose_name='last modified', auto_now=True)),
                 ('object_pk', models.IntegerField(db_index=True)),
                 ('result', django_extensions.db.fields.json.JSONField()),
+                ('ignored', django_extensions.db.fields.json.JSONField()),
                 ('errors', django_extensions.db.fields.json.JSONField()),
                 ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
-                ('old', models.ForeignKey(to='data_importer.ImportedObjects')),
+                ('old', models.ForeignKey(to='data_importer.ImportedObjects', null=True)),
             ],
             options={
                 'ordering': ('-modified', '-created'),
@@ -33,7 +46,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Run',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('created', models.DateTimeField(verbose_name='date created', auto_now_add=True)),
                 ('modified', models.DateTimeField(verbose_name='last modified', auto_now=True)),
                 ('checked_count', models.PositiveIntegerField(default=0)),
