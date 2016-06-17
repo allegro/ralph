aba0170377fdddda426827751f88c76b3485d808
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-06-17 13:09:48 +0200

    wip

--- src/ralph/assets/migrations/0014_memory.py
+++ src/ralph/assets/migrations/0014_memory.py
@@ -8,22 +8,22 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('assets', '0012_auto_20160606_1409'),
+        ('assets', '0013_auto_20160615_2140'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Memory',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
                 ('modified', models.DateTimeField(auto_now=True, verbose_name='last modified')),
                 ('label', models.CharField(max_length=255, verbose_name='name')),
                 ('size', models.PositiveIntegerField(verbose_name='size (MiB)')),
-                ('speed', models.PositiveIntegerField(blank=True, verbose_name='speed (MHz)', null=True)),
-                ('slot_no', models.PositiveIntegerField(blank=True, verbose_name='slot number', null=True)),
+                ('speed', models.PositiveIntegerField(null=True, blank=True, verbose_name='speed (MHz)')),
+                ('slot_no', models.PositiveIntegerField(null=True, blank=True, verbose_name='slot number')),
                 ('base_object', models.ForeignKey(related_name='memory_set', to='assets.BaseObject')),
-                ('model', models.ForeignKey(to='assets.ComponentModel', blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, default=None, verbose_name='model')),
+                ('model', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='assets.ComponentModel', blank=True, default=None, null=True, verbose_name='model')),
             ],
             options={
                 'verbose_name_plural': 'memories',
