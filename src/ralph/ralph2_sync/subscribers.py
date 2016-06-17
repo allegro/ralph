61bfbe06b4e87ec2c15c09f460e10ed5f4ced2ec
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-06-17 11:42:41 +0200

    wip

--- src/ralph/ralph2_sync/subscribers.py
+++ src/ralph/ralph2_sync/subscribers.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
+from functools import wraps
+
 import pyhermes
-
 from django.contrib.contenttypes.models import ContentType
+from django.db import transaction
 
-from ralph.assets.models import AssetModel
+from ralph.assets.models import AssetModel, Environment, ServiceEnvironment
 from ralph.data_center.models import DataCenterAsset
 from ralph.data_importer.models import ImportedObjects
 
@@ -14,6 +16,21 @@ model_mapping = {
     'Asset': DataCenterAsset,
     'AssetModel': AssetModel,
 }
+
+
+class sync_subscriber(pyhermes.subscriber):
+    """
+    Log additional exception when sync has failed.
+    """
+    def _get_wrapper(self, func):
+        @wraps(func)
+        @transaction.atomic
+        def exception_wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except:
+                logger.exception('Exception during syncing')
+        return exception_wrapper
 
 
 @pyhermes.subscriber(topic='ralph2_sync_ack')
@@ -41,3 +58,29 @@ def ralph2_sync_ack(data):
             old_object_pk=data['id'],
             object_pk=data['ralph3_id'],
         )
+
+
+@sync_subscriber(topic='sync_device_to_ralph3')
+def sync_device_to_ralph3(data):
+    """
+    Receive data about device from Ralph2
+
+    Supported fields:
+    * hostname
+    * service/env
+    * management ip/hostname
+    """
+    dca = ImportedObjects.get_object_from_old_pk(DataCenterAsset, data['id'])
+    dca.hostname = data['hostname']
+    dca.management_ip = data['management_ip']
+    dca.management_hostname = data['management_hostname']
+    if data['service'] and data['environment']:
+        dca.service_env = ServiceEnvironment.objects.get(
+            service__uid=data['service'],
+            environment=ImportedObjects.get_object_from_old_pk(
+                Environment, data['environment']
+            )
+        )
+    # don't trigger another publishing event
+    dca._handle_post_save = False
+    dca.save()
