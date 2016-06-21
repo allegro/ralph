65249b0404c504b52cc546ddef1886404ddd54d9
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-06-21 11:22:06 +0200

    wip

--- src/ralph/ralph2_sync/publishers.py
+++ src/ralph/ralph2_sync/publishers.py
@@ -5,7 +5,6 @@ from functools import wraps
 import pyhermes
 
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
