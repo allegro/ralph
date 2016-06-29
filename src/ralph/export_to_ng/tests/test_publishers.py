c7f28787a555bf8b685cbe54d80c314734c38724
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-06-29 11:06:32 +0200

    WIP

--- src/ralph/export_to_ng/tests/test_publishers.py
+++ src/ralph/export_to_ng/tests/test_publishers.py
@@ -179,7 +179,7 @@ class VentureRolePublisherTestCase(TestCase):
 @override_settings(
     RALPH3_HERMES_SYNC_ENABLED=True,
     RALPH3_HERMES_SYNC_FUNCTIONS=['sync_role_property_to_ralph3'],
-    RALPH2_HERMES_SYNC_ROLE_PROPERTY_WHITELIST=['test_symbol'],)
+    RALPH2_HERMES_ROLE_PROPERTY_WHITELIST=['test_symbol'],)
 class RolePropertyPublisherTestCase(TestCase):
     def setUp(self):
         self.prop = RoleProperty.objects.create(
