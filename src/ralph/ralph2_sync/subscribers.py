cf862561fc7dc30f8d503bf313ca876ef803e782
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-07-01 08:42:01 +0200

    WIP

--- src/ralph/ralph2_sync/subscribers.py
+++ src/ralph/ralph2_sync/subscribers.py
@@ -155,7 +155,7 @@ def sync_custom_fields_to_ralph3(data):
     Receive data about custom fields from Ralph2
 
     Supported fields:
-    * symbol (name)
+    * name (old symbol)
     * choices
     * default value
     """
@@ -175,9 +175,9 @@ def sync_venture_to_ralph3(data):
     Receive data about venture from Ralph2 (ConfigurationModule in Ralph3).
 
     Supported fields:
-    * symbol
-    * parent
-    * team
+    * name (old symbol)
+    * parent (old parent)
+    * team (old department)
     """
     creating = False
     try:
@@ -224,8 +224,8 @@ def sync_venture_role_to_ralph3(data):
     Receive data about venture role from Ralph2 (ConfigurationClass in Ralph3).
 
     Supported fields:
-    * symbol
-    * module
+    * name (old symbol)
+    * module (old venture)
     """
     creating = False
     try:
