668d9d7a5677a4d2f9d51fe548c899559a992e33
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-07-12 14:19:53 +0200

    Fix ``LookupError`` when RALPH2_RALPH3_CROSS_VALIDATION_ENABLED is off

--- src/ralph/cross_validator/views.py
+++ src/ralph/cross_validator/views.py
@@ -1,15 +1,19 @@
+from django.conf import settings
 from django.contrib import messages
 from django.utils.safestring import mark_safe
-
-from ralph.cross_validator.admin import get_html_diff
-from ralph.cross_validator.models import CrossValidationResult
 
 
 class ShowDiffMessageMixin(object):
     diff_message = 'Latest diff between Ralph 2 and Ralph 3:<br>{}'
 
     def changeform_view(self, request, object_id, *args, **kwargs):
-        response = super().changeform_view(request, object_id, *args, **kwargs)
+        response = super().changeform_view(
+            request, object_id, *args, **kwargs
+        )
+        if not settings.RALPH2_RALPH3_CROSS_VALIDATION_ENABLED:
+            return response
+        from ralph.cross_validator.models import CrossValidationResult
+        from ralph.cross_validator.admin import get_html_diff
         obj = self.get_object(request, object_id)
         if not obj:
             return response
@@ -22,5 +26,7 @@ class ShowDiffMessageMixin(object):
                 msg += '</ul>'
             else:
                 msg = get_html_diff(last_diff)
-            messages.info(request, mark_safe(self.diff_message.format(msg)))
+            messages.info(
+                request, mark_safe(self.diff_message.format(msg))
+            )
         return response
