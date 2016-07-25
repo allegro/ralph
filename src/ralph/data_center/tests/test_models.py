allegro/ralph

commit f703dc0bc053523ad11376844a7c69a2446a8ba7

Fix generic foreign key prefetch related in polymorphic api (#2584)
Bug: When there is more than one type on `BaseObject` API endpoint (`/api/base-objects/`), custom fields are returned only for objects of the first type

`CustomFieldValue` is model based on `ContentType`s, so it has `GenericForeignKey` to all other models. According to Django documentation for prefetch_related (https://docs.djangoproject.com/en/1.8/ref/models/querysets/#prefetch-related):
"It also supports prefetching of GenericRelation and GenericForeignKey, however, it must be restricted to a homogeneous set of results. For example, prefetching objects referenced by a GenericForeignKey is only supported if the query is restricted to one ContentType."

In case of `BaseObject`, there could be more than one type of object, so prefetch related was working only for the first of them (implicitly adding filter by content type to `CustomFieldValue` prefetch query), resulting in returning empty list of custom fields for objects of different type.

Since `ralph.api.utils.PolymorphicViewSetMixin` is injecting fields to be prefetched for every polymorphic descendant (in this case of BaseObject, ex. DataCenterAsset, BackOfficeAsset etc), the solution was to prefetch custom fields (and potentially any other field) in final viewset for the type (ex. `DataCenterAssetViewSet`), which is later injected to polymorphic (sub)query for objects of this type (instead of calling it once for all).

src/ralph/data_center/api/views.py
@@ -3,7 +3,10 @@ from django.db.models import Prefetch
 
 from ralph.api import RalphAPIViewSet
 from ralph.assets.api.filters import NetworkableObjectFilters
-from ralph.assets.api.views import BaseObjectViewSet, BaseObjectViewSetMixin
+from ralph.assets.api.views import (
+    base_object_descendant_prefetch_related,
+    BaseObjectViewSetMixin
+)
 from ralph.assets.models import Ethernet
 from ralph.data_center.admin import DataCenterAssetAdmin
 from ralph.data_center.api.serializers import (
@@ -47,7 +50,7 @@ class DataCenterAssetViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'rack', 'rack__server_room', 'rack__server_room__data_center',
         'property_of', 'budget_info', 'content_type'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
+    prefetch_related = base_object_descendant_prefetch_related + [
         'connections',
         'tags',
         'memory_set',
@@ -119,6 +122,6 @@ class ClusterViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'type', 'parent', 'service_env', 'service_env__service',
         'service_env__environment', 'configuration_path', 'content_type'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
-        'tags', 'baseobjectcluster_set',
+    prefetch_related = base_object_descendant_prefetch_related + [
+        'tags', 'baseobjectcluster_set'
     ]

src/ralph/data_center/models/virtual.py
@@ -72,11 +72,9 @@ class Cluster(BaseObject, models.Model):
 
     @cached_property
     def masters(self):
-        return BaseObject.polymorphic_objects.filter(
-            pk__in=self.baseobjectcluster_set.filter(
-                is_master=True
-            ).values_list('base_object_id', flat=True)
-        )
+        for obj in self.baseobjectcluster_set.all():
+            if obj.is_master:
+                yield obj
 
     def _validate_name_hostname(self):
         if not self.name and not self.hostname:

src/ralph/data_center/tests/test_api.py
@@ -449,7 +449,8 @@ class ClusterAPITests(RalphAPITestCase):
 
     def test_list_cluster(self):
         url = reverse('cluster-list')
-        response = self.client.get(url, format='json')
+        with self.assertNumQueries(12):
+            response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 2)
         for item in response.data['results']:
@@ -458,7 +459,8 @@ class ClusterAPITests(RalphAPITestCase):
 
     def test_get_cluster_details(self):
         url = reverse('cluster-detail', args=(self.cluster_1.id,))
-        response = self.client.get(url, format='json')
+        with self.assertNumQueries(10):
+            response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], self.cluster_1.name)
         self.assertEqual(response.data['hostname'], self.cluster_1.hostname)
@@ -481,7 +483,6 @@ class ClusterAPITests(RalphAPITestCase):
                     )
                 ),
                 'is_master': self.boc_1.is_master,
-                'cluster': self.get_full_url(url),
             },
             {
                 'id': self.boc_2.id,
@@ -492,6 +493,5 @@ class ClusterAPITests(RalphAPITestCase):
                     'baseobject-detail', args=(self.boc_2.base_object.id,)
                 )),
                 'is_master': self.boc_2.is_master,
-                'cluster': self.get_full_url(url),
             }
         ])

src/ralph/assets/api/views.py
@@ -76,21 +76,27 @@ class BaseObjectFilterSet(NetworkableObjectFilters):
         model = models.BaseObject
 
 
+base_object_descendant_prefetch_related = [
+    Prefetch('licences', queryset=BaseObjectLicence.objects.select_related(
+        *BaseObjectLicenceViewSet.select_related
+    )),
+    Prefetch(
+        'custom_fields',
+        queryset=CustomFieldValue.objects.select_related('custom_field')
+    ),
+    'service_env__service__business_owners',
+    'service_env__service__technical_owners',
+]
+
+
+class BaseObjectDescendantViewSetMixin(RalphAPIViewSet):
+    prefetch_related = base_object_descendant_prefetch_related
+
+
 class BaseObjectViewSet(PolymorphicViewSetMixin, RalphAPIViewSet):
     queryset = models.BaseObject.polymorphic_objects.all()
     serializer_class = serializers.BaseObjectPolymorphicSerializer
     http_method_names = ['get', 'options', 'head']
-    prefetch_related = [
-        Prefetch('licences', queryset=BaseObjectLicence.objects.select_related(
-            *BaseObjectLicenceViewSet.select_related
-        )),
-        Prefetch(
-            'custom_fields',
-            queryset=CustomFieldValue.objects.select_related('custom_field')
-        ),
-        'service_env__service__business_owners',
-        'service_env__service__technical_owners',
-    ]
     filter_fields = [
         'id', 'service_env', 'service_env', 'service_env__service__uid',
         'content_type'
@@ -200,7 +206,7 @@ class DCHostViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'service_env', 'service_env__service', 'service_env__environment',
         'configuration_path', 'configuration_path__module'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
+    prefetch_related = base_object_descendant_prefetch_related + [
         'tags',
         'memory_set',
         Prefetch(

src/ralph/assets/tests/test_api.py
@@ -42,6 +42,7 @@ from ralph.data_center.tests.factories import (
 )
 from ralph.domains.models import Domain
 from ralph.domains.tests.factories import DomainFactory
+from ralph.lib.custom_fields.models import CustomField
 from ralph.licences.models import Licence
 from ralph.licences.tests.factories import LicenceFactory
 from ralph.networks.tests.factories import IPAddressFactory
@@ -511,6 +512,23 @@ class BaseObjectAPITests(RalphAPITestCase):
             if 'barcode' in item
         ]
         self.assertCountEqual(barcodes, set(['12345', '12543']))
+
+    def test_get_base_objects_list_different_type_with_custom_fields(self):
+        CustomField.objects.create(name='test_field')
+        self.dc_asset.update_custom_field('test_field', 'abc')
+        self.bo_asset.update_custom_field('test_field', 'def')
+        url = reverse('baseobject-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        count = 0
+        for item in response.data['results']:
+            if item['id'] == self.dc_asset.id:
+                self.assertEqual(item['custom_fields'], {'test_field': 'abc'})
+                count += 1
+            if item['id'] == self.bo_asset.id:
+                self.assertEqual(item['custom_fields'], {'test_field': 'def'})
+                count += 1
+        self.assertEqual(count, 2)
 
     def test_get_asset_model_details(self):
         url = reverse('baseobject-detail', args=(self.bo_asset.id,))

src/ralph/back_office/api.py
@@ -2,7 +2,7 @@
 from ralph.accounts.api_simple import SimpleRalphUserSerializer
 from ralph.api import RalphAPISerializer, RalphAPIViewSet, router
 from ralph.assets.api.serializers import AssetSerializer
-from ralph.assets.api.views import BaseObjectViewSet
+from ralph.assets.api.views import base_object_descendant_prefetch_related
 from ralph.back_office.admin import BackOfficeAssetAdmin
 from ralph.back_office.models import (
     BackOfficeAsset,
@@ -53,7 +53,7 @@ class BackOfficeAssetViewSet(RalphAPIViewSet):
         'user', 'owner', 'property_of', 'office_infrastructure',
         'budget_info'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
+    prefetch_related = base_object_descendant_prefetch_related + [
         'user__groups', 'user__user_permissions',
         'service_env__service__environments',
         'service_env__service__business_owners',

src/ralph/data_center/api/serializers.py
@@ -36,6 +36,12 @@ class ClusterSimpleSerializer(BaseObjectSerializer):
         depth = 1
 
 
+class BaseObjectClusterSimpleSerializer(RalphAPISerializer):
+    class Meta:
+        model = BaseObjectCluster
+        fields = ('id', 'url', 'base_object', 'is_master')
+
+
 class BaseObjectClusterSerializer(RalphAPISerializer):
     class Meta:
         model = BaseObjectCluster
@@ -45,7 +51,7 @@ class BaseObjectClusterSerializer(RalphAPISerializer):
 class ClusterSerializer(
     OwnersFromServiceEnvSerializerMixin, ClusterSimpleSerializer
 ):
-    base_objects = BaseObjectClusterSerializer(
+    base_objects = BaseObjectClusterSimpleSerializer(
         many=True, read_only=True, source='baseobjectcluster_set'
     )
     masters = serializers.HyperlinkedRelatedField(

src/ralph/virtual/api.py
@@ -12,7 +12,10 @@ from ralph.assets.api.serializers import (
     NetworkComponentSerializerMixin,
     ServiceEnvironmentSimpleSerializer
 )
-from ralph.assets.api.views import BaseObjectViewSet, BaseObjectViewSetMixin
+from ralph.assets.api.views import (
+    base_object_descendant_prefetch_related,
+    BaseObjectViewSetMixin
+)
 from ralph.assets.models import Ethernet
 from ralph.data_center.api.serializers import DataCenterAssetSimpleSerializer
 from ralph.data_center.models import DataCenterAsset
@@ -170,7 +173,7 @@ class CloudHostViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'parent', 'parent__cloudproject', 'cloudprovider', 'hypervisor',
         'service_env__service', 'service_env__environment', 'content_type'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
+    prefetch_related = base_object_descendant_prefetch_related + [
         'tags', 'cloudflavor__virtualcomponent_set__model', 'licences',
         Prefetch(
             'ethernet_set',
@@ -205,7 +208,7 @@ class VirtualServerViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'parent', 'service_env__service', 'service_env__environment',
         'configuration_path', 'content_type'
     ]
-    prefetch_related = BaseObjectViewSet.prefetch_related + [
+    prefetch_related = base_object_descendant_prefetch_related + [
         'tags',
         'memory_set',
         Prefetch(

commit cab63e7cb345c5a578b90804cff689ac8ff69f73

Fix masters for Cluster
* return proper type (BaseObject instead of BaseObjectCluster - broken in f703dc0bc053523ad11376844a7c69a2446a8ba7)
* refined prefetch_related for Cluster viewset to get final type of BaseObject

src/ralph/data_center/api/views.py
@@ -7,7 +7,7 @@ from ralph.assets.api.views import (
     base_object_descendant_prefetch_related,
     BaseObjectViewSetMixin
 )
-from ralph.assets.models import Ethernet
+from ralph.assets.models import BaseObject, Ethernet
 from ralph.data_center.admin import DataCenterAssetAdmin
 from ralph.data_center.api.serializers import (
     AccessorySerializer,
@@ -131,7 +131,19 @@ class ClusterViewSet(BaseObjectViewSetMixin, RalphAPIViewSet):
         'service_env__environment', 'configuration_path', 'content_type'
     ]
     prefetch_related = base_object_descendant_prefetch_related + [
-        'tags', 'baseobjectcluster_set',
+        'tags',
+        # prefetch baseobject using polymorphic_objects to get list of
+        # objects of final type (ex. DataCenterAsset) and to allow to use
+        # prefetched objects in `masters` property
+        Prefetch(
+            'baseobjectcluster_set',
+            queryset=BaseObjectCluster.objects.prefetch_related(
+                Prefetch(
+                    'base_object',
+                    queryset=BaseObject.polymorphic_objects.all()
+                )
+            )
+        ),
         Prefetch(
             'ethernet_set',
             queryset=Ethernet.objects.select_related('ipaddress')

src/ralph/data_center/models/virtual.py
@@ -6,6 +6,7 @@ from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
 from ralph.assets.models.base import BaseObject
+from ralph.data_center.models.physical import DataCenterAsset
 from ralph.data_center.models.mixins import WithManagementIPMixin
 from ralph.data_center.models.physical import (
     DataCenterAsset,
@@ -79,10 +80,21 @@ class Cluster(
 
     @cached_property
     def masters(self):
+        # prevents cyclic import
+        from ralph.virtual.models import CloudHost, VirtualServer  # noqa
+
         result = []
         for obj in self.baseobjectcluster_set.all():
             if obj.is_master:
-                result.append(obj)
+                bo = obj.base_object
+                # fetch final object if it's base object
+                if not isinstance(
+                    bo,
+                    # list equal to BaseObjectCluster.base_object.limit_models
+                    (Database, DataCenterAsset, CloudHost, VirtualServer)
+                ):
+                    bo = bo.last_descendant
+                result.append(bo)
         return result
 
     @cached_property

src/ralph/data_center/tests/test_api.py
@@ -401,8 +401,9 @@ class ClusterAPITests(RalphAPITestCase):
         self.boc_1 = BaseObjectCluster.objects.create(
             cluster=self.cluster_1, base_object=DataCenterAssetFactory()
         )
+        self.master = DataCenterAssetFactory()
         self.boc_2 = BaseObjectCluster.objects.create(
-            cluster=self.cluster_1, base_object=DataCenterAssetFactory(),
+            cluster=self.cluster_1, base_object=self.master,
             is_master=True
         )
         self.cluster_2 = ClusterFactory()
@@ -460,7 +461,7 @@ class ClusterAPITests(RalphAPITestCase):
 
     def test_get_cluster_details(self):
         url = reverse('cluster-detail', args=(self.cluster_1.id,))
-        with self.assertNumQueries(11):
+        with self.assertNumQueries(14):
             response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], self.cluster_1.name)
@@ -504,4 +505,10 @@ class ClusterAPITests(RalphAPITestCase):
         )
         self.assertEqual(
             response.data['ipaddresses'], ['10.20.30.40']
+        )
+        self.assertEqual(
+            response.data['masters'][0],
+            self.get_full_url(
+                reverse('baseobject-detail', args=(self.master.id,))
+            )
         )

src/ralph/data_center/tests/test_models.py
@@ -7,8 +7,11 @@ from ralph.back_office.models import BackOfficeAsset
 from ralph.back_office.tests.factories import WarehouseFactory
 from ralph.data_center.models.choices import DataCenterAssetStatus, Orientation
 from ralph.data_center.models.physical import DataCenterAsset
+from ralph.data_center.models.virtual import BaseObjectCluster
 from ralph.data_center.tests.factories import (
     DataCenterAssetFactory,
+    ClusterFactory,
+    ClusterTypeFactory,
     RackFactory
 )
 from ralph.networks.models import IPAddress, IPAddressStatus
@@ -393,3 +396,20 @@ class RackTest(RalphTestCase):
             orientation=Orientation.back.id, **asset_common_kwargs
         )
         self.assertEqual(rack.get_free_u(), 47)
+
+
+class ClusterTest(RalphTestCase):
+    def setUp(self):
+        self.cluster_type = ClusterTypeFactory()
+        self.cluster_1 = ClusterFactory()
+        self.boc_1 = BaseObjectCluster.objects.create(
+            cluster=self.cluster_1, base_object=DataCenterAssetFactory()
+        )
+        self.master = DataCenterAssetFactory()
+        self.boc_2 = BaseObjectCluster.objects.create(
+            cluster=self.cluster_1, base_object=self.master,
+            is_master=True
+        )
+
+    def test_masters(self):
+        self.assertEqual(self.cluster_1.masters, [self.master])