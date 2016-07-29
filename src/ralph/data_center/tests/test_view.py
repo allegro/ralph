952e2890b2f77465e43ffa48c748fe93c1ec9704
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-07-29 13:03:46 +0200

    WIP

--- src/ralph/data_center/tests/test_view.py
+++ src/ralph/data_center/tests/test_view.py
@@ -9,7 +9,7 @@ class DataCentrAssetViewTest(ClientMixin, TestCase):
     def test_changelist_view(self):
         self.login_as_user()
         DataCenterAssetFullFactory.create_batch(10)
-        with self.assertNumQueries(14):
+        with self.assertNumQueries(16):
             self.client.get(
                 reverse('admin:data_center_datacenterasset_changelist'),
             )
