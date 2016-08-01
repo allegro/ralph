9358ebbed3380056f73a6368cbbe519eb7a75b46
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-08-01 10:41:17 +0200

    WIP

--- src/ralph/data_center/models/hosts.py
+++ src/ralph/data_center/models/hosts.py
@@ -4,17 +4,8 @@ from ralph.assets.models.base import BaseObject
 
 
 class DCHostManager(models.Manager):
-    select_related_fields = [
-        'configuration_path',
-        'service_env',
-        'service_env__environment',
-        'service_env__service',
-    ]
-
     def get_queryset(self):
-        return BaseObject.polymorphic_objects.dc_hosts().select_related(
-            *self.select_related_fields
-        )
+        return BaseObject.polymorphic_objects.dc_hosts()
 
 
 class DCHost(BaseObject):
