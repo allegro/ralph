3690565e50a4f70b509b4043a86ad0b4f83e9083
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-08-01 17:13:01 +0200

    Add cached templates loader to prod's settings

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -98,16 +98,17 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
             'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-                'ralph.lib.template.loaders.AppTemplateLoader',
-            ]
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                    'ralph.lib.template.loaders.AppTemplateLoader',
+                ]),
+            ],
         },
     },
 ]
