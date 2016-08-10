b4b53fb0ddb8df7c9f66f1e2e9d57fcd065372ac
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-08-10 09:31:24 +0000

    wip

--- src/ralph/networks/filters.py
+++ src/ralph/networks/filters.py
@@ -2,7 +2,11 @@ import ipaddress
 
 from django.utils.translation import ugettext_lazy as _
 
-from ralph.admin.filters import TextListFilter
+from ralph.admin.filters import ChoicesListFilter, TextListFilter
+
+PRIVATE_NETWORK_CIDRS = [
+    '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16',
+]
 
 
 class IPRangeFilter(TextListFilter):
@@ -44,4 +48,20 @@ class NetworkRangeFilter(TextListFilter):
             max_ip = int(network.broadcast_address)
 
             queryset = queryset.filter(min_ip__gte=min_ip, max_ip__lte=max_ip)
+        return queryset
+
+
+class NetworkClassFilter(ChoicesListFilter):
+    _choices_list = [
+        ('private', _('Private')),
+        ('public', _('Public')),
+    ]
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            pass
+        elif self.value().lower() == 'private':
+            queryset = queryset.filter(address__in=PRIVATE_NETWORK_CIDRS)
+        elif self.value().lower() == 'public':
+            queryset = queryset.exclude(address__in=PRIVATE_NETWORK_CIDRS)
         return queryset
