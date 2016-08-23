7c46abb1026568b74e0164ce1865e290781d984c
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-08-23 09:23:12 +0200

    wip

--- src/ralph/lib/transitions/tests/test_async.py
+++ src/ralph/lib/transitions/tests/test_async.py
@@ -7,8 +7,8 @@ from django.test import RequestFactory, TransactionTestCase
 from ralph.lib.external_services.models import JobStatus
 from ralph.lib.transitions.models import (
     run_transition,
-    TransitionsHistory,
-    TransitionJob
+    TransitionJob,
+    TransitionsHistory
 )
 from ralph.lib.transitions.tests import TransitionTestCaseMixin
 from ralph.tests.models import AsyncOrder, Foo, OrderStatus
