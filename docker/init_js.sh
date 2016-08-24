56f965699fd3d5b4b23617443c974924dfafe764
Bryan Taylor <b.taylor.engineer@gmail.com>
2016-08-24 08:16:18 -0600

    Updated Docker (#2748)

--- docker/init_js.sh
+++ docker/init_js.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+export NVM_DIR="/root/.nvm"
+source $NVM_DIR/nvm.sh
 cd $RALPH_DIR
 $RALPH_DIR/node_modules/.bin/gulp build
--- docker/provision_js.sh
+++ docker/provision_js.sh
@@ -2,6 +2,11 @@
 set -e
 
 cd $RALPH_DIR
-ln -s /usr/bin/nodejs /usr/bin/node  # fix node.js on ubuntu
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+export NVM_DIR="/root/.nvm"
+source ~/.bashrc
+source $NVM_DIR/nvm.sh
+nvm install node
 npm install -g bower
 npm install --skip-installed
+npm install gulp
