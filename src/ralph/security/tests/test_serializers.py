c726bbcb7787e6221e2d54b4cb2e5c3172f6ca09
xliiv <tymoteusz.jankowski@gmail.com>
2016-08-26 12:58:06 +0200

    Security Info API fixes (#2772)
    Add new tests checking creation of scans by `external_vulnerabilities`.

--- src/ralph/security/tests/test_serializers.py
+++ src/ralph/security/tests/test_serializers.py
@@ -17,25 +17,22 @@ class SaveSecurityScanSerializerTests(RalphAPITestCase):
         super().setUp()
         self.security_scan = SecurityScanFactory()
 
-    def _dict2QueryDict(self, data_dict):
-        qdict = QueryDict('', mutable=True)
-        qdict.update(data_dict)
-        return qdict
-
     def test_external_id_is_converted_to_local(self):
         ip = IPAddressFactory(address="192.168.128.10")
         vulnerability_1 = VulnerabilityFactory()
         vulnerability_2 = VulnerabilityFactory()
-        data = self._dict2QueryDict({
+        data = {
             'last_scan_date': '2015-01-01T00:00:00',
             'scan_status': 'ok',
             'next_scan_date': '2016-01-01T00:00:00',
             'details_url': 'https://example.com/scan-deatils',
             'rescan_url': 'https://example.com/rescan-url',
             'host_ip': ip.address,
-            'vulnerabilities': vulnerability_1.id,
-            'external_vulnerabilities': vulnerability_2.external_vulnerability_id,  # noqa
-        })
+            'vulnerabilities': [vulnerability_1.id],
+            'external_vulnerabilities': [
+                vulnerability_2.external_vulnerability_id
+            ],
+        }
         scan_serializer = SaveSecurityScanSerializer(
             context={'request': None})
         deserialized = scan_serializer.to_internal_value(data)
@@ -48,16 +45,16 @@ class SaveSecurityScanSerializerTests(RalphAPITestCase):
     def test_error_raised_when_unknown_external_id(self):
         ip = IPAddressFactory(address="192.168.128.10")
         vulnerability = VulnerabilityFactory()
-        data = self._dict2QueryDict({
+        data = {
             'last_scan_date': '2015-01-01T00:00:00',
             'scan_status': 'ok',
             'next_scan_date': '2016-01-01T00:00:00',
             'details_url': 'https://example.com/scan-deatils',
             'rescan_url': 'https://example.com/rescan-url',
             'host_ip': ip.address,
-            'vulnerabilities': vulnerability.id,
-            'external_vulnerabilities': '12345678',
-        })
+            'vulnerabilities': [vulnerability.id],
+            'external_vulnerabilities': ['12345678'],
+        }
         scan_serializer = SaveSecurityScanSerializer(
             context={'request': None})
         with self.assertRaises(serializers.ValidationError):
