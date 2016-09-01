056fef08b436b4fbced79a328808a54553dc9fc6
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-09-01 14:37:22 +0200

    WIP

--- src/ralph/networks/receivers.py
+++ src/ralph/networks/receivers.py
@@ -4,16 +4,20 @@ from ralph.dns.dnsaas import DNSaaS
 def update_dns_record(instance, created, *args, **kwargs):
     keys = ['address', 'hostname']
     data_to_send = {
-        key: instance.__dict__[key]
-        for key in keys
+        'new': {
+            key: instance.__dict__[key] for key in keys
+        },
+        'old': {
+            key: instance._previous_state[key] for key in keys
+        }
     }
     data_to_send['action'] = 'add' if created else 'update'
-    data_to_send['ip'] = instance._previous_state['address']
     DNSaaS().send_ipaddress_data(data_to_send)
 
 
 def delete_dns_record(instance, *args, **kwargs):
     DNSaaS().send_ipaddress_data({
-        'ip': instance.address,
+        'address': instance.address,
+        'hostname': instance.hostname,
         'action': 'delete'
     })
