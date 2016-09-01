518662f1a142172d75202396c4be89ae463b8acf
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-09-01 09:47:47 +0200

    flake

--- src/ralph/networks/apps.py
+++ src/ralph/networks/apps.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 
 from ralph.apps import RalphAppConfig
 
