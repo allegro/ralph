4bc429efe7a6820176b9d839931bccd36c2327a8
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-09-09 12:17:01 +0200

    Hide DCs without server rooms

--- src/ralph/dc_view/views/ui.py
+++ src/ralph/dc_view/views/ui.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+
 from ralph.admin.mixins import RalphTemplateView
 from ralph.data_center.models.physical import DataCenter
 
@@ -7,6 +9,8 @@ class ServerRoomView(RalphTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data_centers'] = DataCenter.objects.all()
+        context['data_centers'] = DataCenter.objects.annotate(
+            rooms_count=Count('serverroom')
+        ).filter(rooms_count__gt=0)
         context['site_header'] = "Ralph 3"
         return context
