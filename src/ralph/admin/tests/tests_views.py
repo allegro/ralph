9e11d06cf4b608c74883b4fb6bbff6af40db1172
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-09-13 13:43:32 +0200

    Remove add permission for all on BaseObject and DCHost (#2782)
    Remove add permission for all on BaseObject and DCHost

--- src/ralph/admin/tests/tests_views.py
+++ src/ralph/admin/tests/tests_views.py
@@ -104,6 +104,11 @@ EXCLUDE_MODELS = [
     'ralph.tests.models.PolymorphicTestModel'
 ]
 
+EXCLUDE_ADD_VIEW = [
+    'ralph.assets.models.base.BaseObject',
+    'ralph.data_center.models.hosts.DCHost'
+]
+
 SQL_QUERY_LIMIT = 30
 
 
@@ -145,8 +150,13 @@ class ViewsTest(TestCase):
                 self.assertEqual(query_count, len(cqc))
                 self.assertFalse(len(cqc) > SQL_QUERY_LIMIT)
 
+            if model_class_path not in EXCLUDE_ADD_VIEW:
+                change_form = model_admin.changeform_view(
+                    self.request, object_id=None
+                )
+                self.assertEqual(change_form.status_code, 200)
             change_form = model_admin.changeform_view(
-                self.request, object_id=None
+                self.request, object_id=str(model.objects.first().id)
             )
             self.assertEqual(change_form.status_code, 200)
 
