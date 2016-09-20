4b78d10f3428f3c2495970369fcaf21b28f67522
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-09-20 09:40:00 +0000

    Oo

---
+++ src/ralph/virtual/migrations/0009_auto_20160920_1139.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('virtual', '0008_auto_20160823_0921'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='virtualservertype',
+            options={},
+        ),
+    ]
