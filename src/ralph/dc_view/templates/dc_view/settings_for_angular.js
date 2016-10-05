dd954c446deaedd9a9ac606ea92cdc4633f5ff1d
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-10-05 15:47:50 +0200

    WIP

--- src/ralph/dc_view/templates/dc_view/settings_for_angular.js
+++ src/ralph/dc_view/templates/dc_view/settings_for_angular.js
@@ -1,9 +1,5 @@
-{% load cache staticfiles %}
-
-{% cache 3600 settings_js %}
-    (function(angular, undefined) {
-        'use strict';
-        var app = angular.module('serverRoomVisualizationApp')
-        app.constant('SETTINGS', {{ settings | safe }});
-    })(angular);
-{% endcache %}
+(function(angular, undefined) {
+    'use strict';
+    var app = angular.module('serverRoomVisualizationApp')
+    app.constant('SETTINGS', {{ settings | safe }});
+})(angular);
