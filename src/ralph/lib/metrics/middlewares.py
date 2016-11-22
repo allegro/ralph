58bc58428850915d77a653dfb8ae37924aba5f6a
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-11-22 15:12:41 +0100

    Remove metrics for DB
    They work only in debug mode.

--- src/ralph/lib/metrics/middlewares.py
+++ src/ralph/lib/metrics/middlewares.py
@@ -2,18 +2,11 @@ import logging
 import time
 
 from django.conf import settings
-from django.db import connection
 from django.utils.text import slugify
 from metrology import Metrology
 
 PROCESSING_TIME_METRIC_PREFIX = getattr(
     settings, 'PROCESSING_TIME_METRIC_PREFIX', 'processing_time'
-)
-DB_QUERIES_COUNT_METRIC_PREFIX = getattr(
-    settings, 'DB_QUERIES_COUNT_METRIC_PREFIX', 'db_queries_count'
-)
-DB_QUERIES_TIME_METRIC_PREFIX = getattr(
-    settings, 'DB_QUERIES_TIME_METRIC_PREFIX', 'db_queries_time'
 )
 REQUESTS_COUNTER_METRIC_PREFIX = getattr(
     settings, 'REQUESTS_COUNTER_METRIC_PREFIX', 'requests_count'
@@ -30,8 +23,7 @@ logger = logging.getLogger(__name__)
 
 class RequestMetricsMiddleware(object):
     """
-    Middleware reporting request metrics (such as processing time, database
-    queries count) to metrology.
+    Middleware reporting request metrics (such as processing time) to metrology
 
     How to use it:
     * add this middleware at the beginning of MIDDLEWARE_CLASSES in your
@@ -68,33 +60,6 @@ class RequestMetricsMiddleware(object):
         )
         processing_timer = Metrology.timer(processing_time_metric_name)
         processing_timer.update(processing_time)  # in miliseconds
-
-        # database queries count and time
-        queries_time = 0.0  # in miliseconds
-        queries_count = 0
-        for q in connection.queries:
-            queries_time += float(q.get('time', 0.0)) * 1000
-            queries_count += 1
-
-        logger.info(
-            'DB queries count: {}'.format(queries_count),
-            extra=dict(db_queries_count=queries_count, **common_log_params)
-        )
-        db_queries_count_metric_name = METRIC_NAME_TMPL.format(
-            prefix=DB_QUERIES_COUNT_METRIC_PREFIX, **common_log_params
-        )
-        db_queries_counter = Metrology.meter(db_queries_count_metric_name)
-        db_queries_counter.mark(value=queries_count)
-
-        logger.info(
-            'DB queries time [ms]: {}'.format(queries_time),
-            extra=dict(db_queries_time=queries_time, **common_log_params)
-        )
-        db_queries_time_metric_name = METRIC_NAME_TMPL.format(
-            prefix=DB_QUERIES_TIME_METRIC_PREFIX, **common_log_params
-        )
-        db_queries_timer = Metrology.timer(db_queries_time_metric_name)
-        db_queries_timer.update(int(queries_time))
 
         # requests counter
         requests_counter_metric_name = METRIC_NAME_TMPL.format(
