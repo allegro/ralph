f99917527a8b821b8bb447e60b75eadf21fc046b
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-12-30 15:34:36 +0100

    wip

--- src/ralph/lib/custom_fields/tests/urls.py
+++ src/ralph/lib/custom_fields/tests/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import include, url
 
 from . import admin, api
 
+cf_tests_urls, cf_tests_app_name, cf_tests_namespace = admin.site.urls
+
 urlpatterns = [
-    url(r'^cf_tests_admin/', include(admin.site.urls)),
+    url(r'^cf_tests_admin/', include((cf_tests_urls, 'ralph', cf_tests_namespace))),
     url(r'^cf_test_api/', include(api.urlpatterns))
 ]
