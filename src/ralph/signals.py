7b99988306ddb35e3c9281a787c78a124e26f6af
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2017-01-24 14:23:24 +0100

    wip

--- src/ralph/signals.py
+++ src/ralph/signals.py
@@ -5,7 +5,29 @@ from django.dispatch import receiver
 
 def post_commit(func, model, prevent_multiple_calls=True):
     """
-    TODO
+    Post commit signal for specific model.
+
+    It's better than Django's post_save, because:
+    * it handles transaction rollback (transaction could be rolled back
+      after calling post_save)
+    * it handles M2M relations (post_save is (usually) called when main model
+      is saved, before related M2M instances are saved)
+
+    Requirements:
+    * you have to use database supporting transactions (ex. MySQL)
+    * you have to use django-transaction-hooks
+      (https://github.com/carljm/django-transaction-hooks) for Django<=1.8
+      (it was merged into Django 1.9)
+
+    Notice that this feature will work whether or not you're using transactions
+    in your code. Possible scenarios are as follows:
+    * `ATOMIC_REQUESTS` is set to True in settings - then every request is
+      wrapped in transaction - at the end of processing each (saving) request,
+      this hook will be processed (for models which were saved)
+    * view is decorated using `transaction.atomic` - at the end of processing
+      the view, this hook will be called (if any of registered models was saved)
+    * if transaction is not started for current request, then this hook will
+      behave as post_save (will be called immediately)
     """
     @receiver(post_save, sender=model, weak=False)
     def wrap(sender, instance, **kwargs):
