08e867dbdfc585c915fa834dd23ac883a619cad4
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2017-01-26 10:37:40 +0100

    wip

--- src/ralph/dns/publishers.py
+++ src/ralph/dns/publishers.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import pyhermes
+from pyhermes import publish
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from threadlocals.threadlocals import get_current_user
 
 from ralph.data_center.models.physical import DataCenterAsset
 from ralph.data_center.models.virtual import Cluster
 from ralph.virtual.models import VirtualServer
+from ralph.signals import post_commit
+
+logger = logging.getLogger(__name__)
 
 
-def _publish_data_to_dnsaaas(obj):
+def _get_txt_data_to_publish_to_dnsaas(obj):
     publish_data = []
     current_user = get_current_user()
     username = current_user.username if current_user else ''
@@ -21,27 +25,20 @@ def _publish_data_to_dnsaaas(obj):
     return publish_data
 
 
-def _send_once(instance):
-    publish_data_to_dnsaaas(instance)
+@pyhermes.publisher(
+    topic=settings.DNSAAS_AUTO_TXT_RECORD_TOPIC_NAME or '',
+    # call publish directly to make testing (overriding settings) easier
+    auto_publish_result=False,
+)
+def publish_data_to_dnsaaas(obj):
+    if settings.DNSAAS_AUTO_TXT_RECORD_TOPIC_NAME:
+        logger.info('Publishing DNS TXT records update for {}'.format(obj))
+        publish(
+            settings.DNSAAS_AUTO_TXT_RECORD_TOPIC_NAME,
+            _get_txt_data_to_publish_to_dnsaas(obj)
+        )
 
 
-if settings.DNSAAS_AUTO_TXT_RECORD_TOPIC_NAME:
-    @pyhermes.publisher(
-        topic=settings.DNSAAS_AUTO_TXT_RECORD_TOPIC_NAME,
-        auto_publish_result=True
-    )
-    def publish_data_to_dnsaaas(obj):
-        return _publish_data_to_dnsaaas(obj)
-
-    # TODO(mkurek): consider changing it to `ralph.signals.post_commit`
-    @receiver(post_save, sender=DataCenterAsset)
-    def post_save_dc_asset(sender, instance, **kwargs):
-        _send_once(instance)
-
-    @receiver(post_save, sender=Cluster)
-    def post_save_cluster(sender, instance, **kwargs):
-        _send_once(instance)
-
-    @receiver(post_save, sender=VirtualServer)
-    def post_save_virtual_server(sender, instance, **kwargs):
-        _send_once(instance)
+post_commit(publish_data_to_dnsaaas, DataCenterAsset)
+post_commit(publish_data_to_dnsaaas, Cluster)
+post_commit(publish_data_to_dnsaaas, VirtualServer)
