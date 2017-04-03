8cd83db7d3cd177412244c0d3d893ab79b411cad
Roman Prykhodchenko <roman.prykhodchenko@allegrogroup.com>
2017-04-03 17:50:43 +0200

    Expose operations over API

---
+++ src/ralph/operations/api.py
@@ -0,0 +1,45 @@
+from django.db.models import Prefetch
+
+from ralph.accounts.api_simple import SimpleRalphUserSerializer
+from ralph.api import RalphAPISerializer, RalphAPIViewSet, router
+from ralph.assets.models import BaseObject
+from ralph.operations.models import Operation, OperationType
+
+
+class OperationTypeSerializer(RalphAPISerializer):
+
+    class Meta:
+        model = OperationType
+        depth = 1
+
+
+class OperationSerializer(RalphAPISerializer):
+    type = OperationTypeSerializer()
+    assignee = SimpleRalphUserSerializer()
+
+    class Meta:
+        model = Operation
+        depth = 1
+
+
+class OperationViewSet(RalphAPIViewSet):
+    queryset = Operation.objects.all().prefetch_related(
+        Prefetch(
+            lookup='base_objects',
+            queryset=BaseObject.objects.select_related('parent', 'tags')
+        )
+    )
+    serializer_class = OperationSerializer
+    select_related = [
+        'type', 'assignee'
+    ]
+
+
+class OperationTypeViewSet(RalphAPIViewSet):
+    queryset = OperationType.objects.all()
+    serializer_class = OperationTypeSerializer
+
+
+router.register(r'operation', OperationViewSet)
+router.register(r'operationtype', OperationTypeViewSet)
+urlpatterns = []
