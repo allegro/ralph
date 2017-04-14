4db2078f46edf28bebb28a3f994aad04f865ce59
Mateusz Kurek <mkurek@users.noreply.github.com>
2017-04-14 09:42:36 +0200

    Graphs fixes (#3022)
    * when there is no reverse match for model changelist, graph will be non-clickable
    * fix for filtering by None value

--- src/ralph/dashboards/admin_filters.py
+++ src/ralph/dashboards/admin_filters.py
@@ -25,6 +25,8 @@ class ByGraphFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         graph_pk, graph_item = self.tokenize(self.value() or '')
         if graph_pk and graph_item:
+            if graph_item == 'None':
+                graph_item = None
             graph = get_object_or_404(Graph, pk=graph_pk)
             queryset = graph.apply_parital_filtering(queryset)
             queryset = queryset.filter(
