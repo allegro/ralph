9692e43668e885a8ddf86b252c2d209d04f4963d
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-04-19 06:06:23 +0000

    fixed data migration

--- src/ralph/security/migrations/0007_auto_20170331_0934.py
+++ src/ralph/security/migrations/0007_auto_20170331_0934.py
@@ -10,19 +10,23 @@ from django.db.models import Count
 
 def update_is_patched(apps, schema_editor):
     SecurityScan = apps.get_model("security", "SecurityScan")
-    is_patched_ids = SecurityScan.vulnerabilities.through.objects.filter(
-        vulnerability__patch_deadline__gt=datetime.now()
+
+    # mark all as patched
+    patched = SecurityScan.objects.update(is_patched=True)
+
+    # mark as not patched
+    not_patched_ids = SecurityScan.vulnerabilities.through.objects.filter(
+        vulnerability__patch_deadline__lte=datetime.now()
     ).values_list(
         'securityscan_id', flat=True
     ).distinct()
-
-    patched_scans = SecurityScan.objects.filter(id__in=is_patched_ids).update(
-        is_patched=True
+    not_patched = SecurityScan.objects.filter(id__in=not_patched_ids).update(
+        is_patched=False
     )
 
     print()
     print("All scans: {}".format(SecurityScan.objects.count()))
-    print("Scans marked as patched: {}".format(patched_scans))
+    print("Scans marked as patched: {}".format(patched - not_patched))
 
 
 class Migration(migrations.Migration):
