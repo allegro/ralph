dcfeaa35025818ff1ffd02864f17726180212f7c
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-05-23 09:56:48 +0000

    ..

--- src/ralph/admin/autocomplete.py
+++ src/ralph/admin/autocomplete.py
@@ -196,9 +196,10 @@ class AutocompleteList(SuggestView):
                         Q(**{'{}__icontains'.format(field): value})
                         for field in search_fields
                     ]
-                    queryset = queryset.filter(
-                        reduce(operator.or_, query_filters)
-                    )
+                    if query_filters:
+                        queryset = queryset.filter(
+                            reduce(operator.or_, query_filters)
+                        )
         else:
             query_filters = [
                 Q(**{'{}__icontains'.format(field): query})
