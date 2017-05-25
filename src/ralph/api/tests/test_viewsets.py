92d862ca61e200cfedd81a2d16828dbaf4cefef2
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-05-25 07:15:44 +0000

    Fix randomness of test_options_filtering

--- src/ralph/api/tests/test_viewsets.py
+++ src/ralph/api/tests/test_viewsets.py
@@ -81,8 +81,9 @@ class TestRalphViewset(RalphTestCase):
 
     def test_options_filtering(self):
         response = self.client.options('/api/manufacturers/')
-        self.assertListEqual(
-            response.data['filtering'], ['name', 'manufacturer_kind']
+        self.assertEqual(
+            set(response.data['filtering']),
+            set(['name', 'manufacturer_kind']),
         )
 
 
