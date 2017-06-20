d02003b184436a59f56b180849bd6a1363978dde
Roman Prykhodchenko <roman.prykhodchenko@allegrogroup.com>
2017-06-20 11:01:25 +0200

    Updated the migration

--- src/ralph/configuration_management/migrations/0001_initial.py
+++ src/ralph/configuration_management/migrations/0001_initial.py
@@ -14,11 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SCMStatusCheck',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('created', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('created', models.DateTimeField(verbose_name='date created', auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True, verbose_name='last modified')),
                 ('last_checked', models.DateTimeField(verbose_name='Last SCM check')),
-                ('check_result', models.PositiveIntegerField(choices=[(1, 'scm_ok'), (2, 'check_failed'), (3, 'scm_error')], verbose_name='SCM check result')),
+                ('check_result', models.PositiveIntegerField(verbose_name='SCM check result', choices=[(1, 'OK'), (2, 'Check failed'), (3, 'Error')])),
                 ('base_object', models.OneToOneField(to='assets.BaseObject')),
             ],
             options={
