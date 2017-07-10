dafb553a0fd5372ba41a6dceb034027691112f18
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2017-07-10 13:15:55 +0200

    Send dchost update event to Hermes when related model has changed

--- src/ralph/assets/signals.py
+++ src/ralph/assets/signals.py
@@ -1,5 +1,19 @@
-from ralph.data_center.publishers import publish_host_update
+
+import logging
+from functools import partial
+
+from django.contrib.admin.utils import get_model_from_relation
+
+from ralph.admin.helpers import get_field_by_relation_path
+from ralph.assets.models import BaseObject
+from ralph.data_center.publishers import (
+    publish_host_update,
+    publish_host_update_from_related_model
+)
 from ralph.lib.custom_fields.signals import api_post_create, api_post_update
+from ralph.signals import post_commit
+
+logger = logging.getLogger(__name__)
 
 
 def custom_field_change(sender, instance, **kwargs):
@@ -12,3 +26,20 @@ def custom_field_change(sender, instance, **kwargs):
 
 api_post_create.connect(custom_field_change)
 api_post_update.connect(custom_field_change)
+
+# trigger publish_host_update when related model change
+for model_path in [
+    'ethernet_set',
+    'ethernet_set__ipaddress',
+    'configuration_path',
+    'configuration_path__module',
+]:
+    field = get_field_by_relation_path(BaseObject, model_path)
+    model = get_model_from_relation(field)
+    logger.debug('Setting up handler for {} change of BaseObject'.format(
+        model.__name__,
+    ))
+    post_commit(partial(
+        publish_host_update_from_related_model,
+        field_path=model_path
+    ), model)
