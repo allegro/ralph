a808862cfd24217dfdf850185a725a073d583e82
Arkadiusz Adamski <ar4s@users.noreply.github.com>
2017-07-24 09:33:51 +0200

    Change language form in user's profile (#3123)
    Change language form in user's profile

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -85,6 +85,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
