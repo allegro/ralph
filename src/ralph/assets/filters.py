64f3baf65dfae72e6afe621eb795de7d85ce0468
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2017-07-26 13:01:30 +0200

    Fix

--- src/ralph/assets/filters.py
+++ src/ralph/assets/filters.py
@@ -4,4 +4,6 @@ from ralph.admin.filters import DateListFilter
 class BuyoutDateFilter(DateListFilter):
     def queryset(self, request, queryset):
         queryset = super().queryset(request, queryset)
-        return queryset.filter(model__category__show_buyout_date=True)
+        if queryset is not None:
+            queryset = queryset.filter(model__category__show_buyout_date=True)
+        return queryset
