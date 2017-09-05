d264625e49293cb2cf58ad4a211b5a751fca5827
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2017-09-05 08:34:05 +0200

    Fix path to graphs in statsd

--- src/ralph/dashboards/management/commands/push_graphs_to_statsd.py
+++ src/ralph/dashboards/management/commands/push_graphs_to_statsd.py
@@ -11,7 +11,7 @@ from ralph.lib.metrics import build_statsd_client
 
 logger = logging.getLogger(__name__)
 PREFIX = settings.STATSD_GRAPHS_PREFIX
-STATSD_PATH = '{}.{{}}.{{}}'.format(PREFIX)
+PATH = '{{}}.{{}}'.format(PREFIX)
 
 
 def normalize(s):
@@ -24,11 +24,11 @@ class Command(BaseCommand):
     help = textwrap.dedent(__doc__).strip()
 
     def handle(self, *args, **kwargs):
-        statsd = build_statsd_client(prefix=STATSD_PATH)
+        statsd = build_statsd_client(prefix=settings.STATSD_GRAPHS_PREFIX)
         graphs = Graph.objects.filter(push_to_statsd=True)
         for graph in graphs:
             graph_data = graph.get_data()
             graph_name = normalize(graph.name)
             for label, value in zip(graph_data['labels'], graph_data['series']):
-                path = STATSD_PATH.format(graph_name, normalize(label))
+                path = PATH.format(graph_name, normalize(label))
                 statsd.gauge(path, value)
