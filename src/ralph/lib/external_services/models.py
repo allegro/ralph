4d140bda02eab63c0b254914306af2ae8c4e23ad
Arkadiusz Adamski <arkadiusz.adamski@allegrogroup.com>
2017-12-19 12:17:38 +0100

    Remove print

--- src/ralph/lib/external_services/models.py
+++ src/ralph/lib/external_services/models.py
@@ -195,7 +195,6 @@ class Job(TimeStampMixin):
         """
         Mark job as failed.
         """
-        print(reason)
         self._update_dumped_params()
         logger.info('Job {} has failed. Reason: {}'.format(self, reason))
         self.status = JobStatus.FAILED
