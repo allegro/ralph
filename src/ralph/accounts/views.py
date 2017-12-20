23288d65c773e054f05e82c481a20f8c1b6e15d7
Arkadiusz Adamski <ar4s@users.noreply.github.com>
2017-12-20 07:52:55 +0100

    Add acceptance mechanism via transitions (#3199)
    Add acceptance mechanism via transitions

--- src/ralph/accounts/views.py
+++ src/ralph/accounts/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import date
+from urllib.parse import urlencode
 
 import reversion
 from django.conf import settings
@@ -12,7 +13,9 @@ from django.views.generic import View
 
 from ralph.accounts.admin import AssetList, AssignedLicenceList, UserInfoMixin
 from ralph.admin.mixins import RalphBaseTemplateView, RalphTemplateView
+from ralph.admin.sites import ralph_site
 from ralph.back_office.models import BackOfficeAsset
+from ralph.lib.transitions.models import Transition
 from ralph.licences.models import BaseObjectLicence
 
 
@@ -124,7 +127,64 @@ class InventoryTagView(View):
         return HttpResponseRedirect(reverse('current_user_info'))
 
 
-class CurrentUserInfoView(UserInfoMixin, RalphBaseTemplateView):
+class _AcceptanceProcessByCurrentUserMixin(object):
+    _config = settings.ACCEPT_ASSETS_FOR_CURRENT_USER_CONFIG
+
+    @property
+    def acceptance_transition_id(self):
+        return self._config['TRANSITION_ID']
+
+    @property
+    def back_office_status(self):
+        return self._config['BACK_OFFICE_ACCEPT_STATUS']
+
+    @property
+    def acceptance_transition_exists(self):
+        return Transition.objects.filter(
+            id=self.acceptance_transition_id
+        ).exists()
+
+    def post(self, request, *args, **kwargs):
+        assets_to_accept = self.get_assets_to_accept()
+        admin_instance = ralph_site.get_admin_instance_for_model(
+            BackOfficeAsset
+        )
+        url_name = admin_instance.get_transition_bulk_url_name()
+        if assets_to_accept:
+            url = reverse(url_name, args=(self.acceptance_transition_id,))
+            query = urlencode([('select', a.id) for a in assets_to_accept])
+            return HttpResponseRedirect('?'.join((url, query)))
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['acceptance_transition_id'] = self.acceptance_transition_id
+        context['acceptance_transition_exists'] = self.acceptance_transition_exists  # noqa: E501
+        context['assets_to_accept'] = self.get_assets_to_accept()
+        return context
+
+    def get_assets_to_accept(self):
+        return BackOfficeAsset.objects.filter(
+            status=self.back_office_status
+        ).filter(user=self.request.user)
+
+
+class _DummyMixin(object):
+    pass
+
+
+AcceptAssetsForCurrentUserMixin = (
+    _AcceptanceProcessByCurrentUserMixin
+    if settings.ENABLE_ACCEPT_ASSETS_FOR_CURRENT_USER
+    else _DummyMixin
+)
+
+
+class CurrentUserInfoView(
+    AcceptAssetsForCurrentUserMixin,
+    UserInfoMixin,
+    RalphBaseTemplateView
+):
     template_name = 'ralphuser/my_equipment.html'
 
     def get_context_data(self, **kwargs):
