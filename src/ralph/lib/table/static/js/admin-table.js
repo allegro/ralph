5e5d21132b47c437dcdcc5b3880cde34f3a0c73d
Arkadiusz Adamski <ar4s@users.noreply.github.com>
2018-01-26 13:26:09 +0100

    Fix "Cannot read property 'addEventListener' of null" (#3225)

--- src/ralph/lib/table/static/js/admin-table.js
+++ src/ralph/lib/table/static/js/admin-table.js
@@ -3,20 +3,23 @@ document.addEventListener('DOMContentLoaded', function(){
   admin_tables.forEach(function(table) {
     var expand = table.querySelector('.expand');
     var collapse = table.querySelector('.collapse');
-    expand.addEventListener('click', function(){
-        expand.closest('.control').classList.add('hide');
-        collapse.closest('.control').classList.remove('hide');
-        table.querySelectorAll('tr.can-hide').forEach(function(row){
-          row.classList.remove('hide');
+    if(expand) {
+        expand.addEventListener('click', function(){
+            expand.closest('.control').classList.add('hide');
+            collapse.closest('.control').classList.remove('hide');
+            table.querySelectorAll('tr.can-hide').forEach(function(row){
+              row.classList.remove('hide');
+            });
         });
-    });
-    collapse.addEventListener('click', function(){
-        collapse.closest('.control').classList.add('hide');
-        expand.closest('.control').classList.remove('hide');
-          table.querySelectorAll('tr.can-hide').forEach(function(row){
-          row.classList.add('hide');
+    }
+    if(collapse) {
+        collapse.addEventListener('click', function(){
+            collapse.closest('.control').classList.add('hide');
+            expand.closest('.control').classList.remove('hide');
+              table.querySelectorAll('tr.can-hide').forEach(function(row){
+              row.classList.add('hide');
+            });
         });
-    });
+    }
   });
 }, false);
-
