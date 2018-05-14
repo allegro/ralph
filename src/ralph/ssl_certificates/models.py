c04c4e6402062ed58050ce9c8ffd61ad0d8c3ec8
Maciej Skorczewski <maciej.skorczewski@allegro.pl>
2018-05-14 15:56:12 +0200

    WIP

--- src/ralph/ssl_certificates/models.py
+++ src/ralph/ssl_certificates/models.py
@@ -49,14 +49,19 @@ class SSLCertificate(AdminAbsoluteUrlMixin, BaseObject):
         AssetHolder,
         blank=True,
         null=True,
-        help_text=_("Company which receives certificate")
+        help_text=_("Company which delivered certificate")
     )
     date_from = models.DateField(null=True, blank=True)
     date_to = models.DateField(null=False, blank=False)
     san = models.TextField(
         blank=True,
-        help_text=_("All Subject Alternative Name"),
+        help_text=_("All Subject Alternative Names"),
     )
     price = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=True, blank=True,
     )
+
+    def __str__(self):
+        return '{} from {} to {}'.format(
+            self.name, self.date_from, self.date_to
+        ) or None
