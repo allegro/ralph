d2ade801eff20a0bbc654bff931a0a3690b399a4
Maciej Skorczewski <maciej.skorczewski@allegro.pl>
2018-05-31 16:19:49 +0200

    Repaired error after travis chek

--- src/ralph/ssl_certificates/management/commands/import_ssl_certificates.py
+++ src/ralph/ssl_certificates/management/commands/import_ssl_certificates.py
@@ -1,6 +1,7 @@
 import datetime
 import fnmatch
 import os
+import re
 import sys
 from pathlib import Path
 
@@ -11,23 +12,80 @@ from cryptography.x509.oid import ExtensionOID, NameOID
 from django.core.management.base import BaseCommand
 
 from ralph.assets.models.assets import Manufacturer
-from ralph.ssl_certificates.models import SSLCertificate
+from ralph.ssl_certificates.models import SSLCertificate, CertificateType
 
 
-class Command(BaseCommand):
+def extract_domain_from_filename(filename):
+    """
+    >>> extract_domain_from_filename('wildcard_allegro.pl.crt')
+    wildcard_allegro.pl
+    """
+    return Path(filename).stem
 
+
+def get_domain_ssl(cert):
+    domain_subject = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+    domain_ssl = domain_subject[0].value
+    if domain_ssl.startswith('*.'):
+        domain_ssl = domain_ssl[2:]
+    return domain_ssl
+
+def get_ssl_type(issuer_name, san, filename):
+    type_ssl = CertificateType.ov.id
+    if re.match(r'^(wildcard.+)', filename):
+        type_ssl = CertificateType.wildcard.id
+    elif san is not '':
+        type_ssl = CertificateType.multisan.id
+    elif issuer_name == 'CA ENT':
+        type_ssl = CertificateType.internal.id
+    return type_ssl
+
+class Command(BaseCommand, ):
     help = 'Import data to application from dir'
 
     def add_arguments(self, parser):
         parser.add_argument('certs_dir', type=str)
 
+    def get_san(self, cert):
+        san = ''
+        extension = None
+        try:
+            extension = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+        except ExtensionNotFound:
+            pass
+        if extension and extension.value:
+            san = extension.value.get_values_for_type(x509.DNSName)
+        return san
+
+    def ssl_values(self, cert, san, filename):
+        issuer = cert.issuer.get_attributes_for_oid(
+            NameOID.ORGANIZATION_NAME
+        )
+        domain = extract_domain_from_filename(filename)
+        issuer_name = 'CA ENT'
+        if issuer and issuer[0].value:
+            issuer_name = issuer[0].value
+        domain_ssl = get_domain_ssl(cert)
+        type_ssl = get_ssl_type(issuer_name, san, filename)
+        manufacturer, _ = Manufacturer.objects.get_or_create(
+            name=issuer_name
+        )
+        SSLCertificate.objects.get_or_create(
+            name=domain,
+            domain_ssl=domain_ssl,
+            certificate_type=type_ssl,
+            san=san,
+            date_to=cert.not_valid_after,
+            date_from=cert.not_valid_before,
+            issued_by=manufacturer
+        )
+
     def handle(self, *args, **options):
+        self.stderr.write('Dir not found')
         certs_dir = options['certs_dir']
         for root, dirs, files in os.walk(certs_dir):
             for filename in fnmatch.filter(files, '*.crt'):
-                san = ''
                 cert = None
-                extension = None
                 pem_data = None
                 try:
                     with open(os.path.join(root, filename)) as f:
@@ -43,32 +101,7 @@ class Command(BaseCommand):
                         '{}/{} is not valid\n'.format(root, filename)
                     )
                     continue
-
                 if cert.not_valid_after < datetime.datetime.now():
                     continue
-                try:
-                    extension = cert.extensions.get_extension_for_oid(
-                        ExtensionOID.SUBJECT_ALTERNATIVE_NAME
-                    )
-                except ExtensionNotFound:
-                    pass
-
-                if extension and extension.value:
-                    san = extension.value.get_values_for_type(x509.DNSName)
-                issuer = cert.issuer.get_attributes_for_oid(
-                    NameOID.ORGANIZATION_NAME
-                )
-                domain = Path(filename).stem
-                issuer_name = 'CA ENT'
-                if issuer and issuer[0].value:
-                    issuer_name = issuer[0].value
-                manufacturer, _ = Manufacturer.objects.get_or_create(
-                    name=issuer_name
-                )
-                SSLCertificate.objects.get_or_create(
-                    name=domain,
-                    date_to=cert.not_valid_after,
-                    date_from=cert.not_valid_before,
-                    san=san,
-                    issued_by=manufacturer
-                )
+                san = self.get_san(cert)
+                self.ssl_values(cert, san, filename)
