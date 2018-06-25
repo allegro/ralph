e528a29e9748762ec7e6bfc512113f8c61d91ddf
Maciej Skorczewski <maciej.skorczewski@allegro.pl>
2018-06-25 11:11:35 +0200

    Changes for flake

--- src/ralph/ssl_certificates/tests/tests.py
+++ src/ralph/ssl_certificates/tests/tests.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from ralph.assets.models import Manufacturer, ServiceEnvironment
-from ralph.settings import DNSAAS_URL, DNSAAS_TOKEN
+from ralph.settings import DNSAAS_TOKEN, DNSAAS_URL
 from ralph.ssl_certificates.models import CertificateType, SSLCertificate
 
 
@@ -19,7 +19,7 @@ class ImportSSLCertificatesTest(TestCase):
         call_command('import_ssl_certificates', '404', stderr=out)
         self.assertIn('Dir not found\n', out.getvalue())
 
-    def test_wildcard_certificate_domain_ssl_should_by_without_prefix(self):
+    def test_wildcard_certificate_domain_ssl_should_by_without_prefix(self):  #noqa
         out = StringIO()
         dir = os.path.join(
             self.base_dir, 'tests', 'samples'
@@ -33,7 +33,9 @@ class ImportSSLCertificatesTest(TestCase):
             self.base_dir, 'tests', 'samples'
         )
         call_command('import_ssl_certificates', dir, stderr=out)
-        self.assertTrue(SSLCertificate.objects.get(certificate_type=CertificateType.wildcard.id))
+        self.assertTrue(SSLCertificate.objects.get(
+            certificate_type=CertificateType.wildcard.id
+        ))
 
     def test_command_should_read_issuer(self):
         out = StringIO()
@@ -51,7 +53,11 @@ class ImportSSLCertificatesTest(TestCase):
             self.base_dir, 'tests', 'samples'
         )
         call_command('import_ssl_certificates', dir, stderr=out)
-        self.assertTrue(SSLCertificate.objects.get(san="['*.allegro.pl', 'allegro.pl']"))
+        self.assertTrue(
+            SSLCertificate.objects.get(
+                san="['*.allegro.pl', 'allegro.pl']"
+            )
+        )
 
     def test_certificate_domain_name(self):
         out = StringIO()
@@ -59,7 +65,11 @@ class ImportSSLCertificatesTest(TestCase):
             self.base_dir, 'tests', 'samples'
         )
         call_command('import_ssl_certificates', dir, stderr=out)
-        self.assertTrue(SSLCertificate.objects.get(name='wildcard_allegro.pl'))
+        self.assertTrue(
+            SSLCertificate.objects.get(
+                name='wildcard_allegro.pl'
+            )
+        )
 
     def test_command_rise_break_certificate(self):
         out = StringIO()
@@ -79,10 +89,16 @@ class UpdateServiceEnvTest(TestCase):
 
     def test_command_should_informed_if_service_not_exist(self):
         out = StringIO()
-        self.dns = os.environ.get('DNSAAS_URL', 'https://dnsaas.allegrogroup.com/')
-        self.token = os.environ.get('DNSAAS_TOKEN', '59bbbfa312feb2fcd32300f7a47babe7c43a7d36')
+        self.dns = os.environ.get(
+            'DNSAAS_URL', 'https://dnsaas.allegrogroup.com/'
+        )
+        self.token = os.environ.get(
+            'DNSAAS_TOKEN', '59bbbfa312feb2fcd32300f7a47babe7c43a7d36'
+        )
         call_command('update_service_env', stderr=out)
-        self.assertIn('Service with name OLX does not exist\n', out.getvalue())
+        self.assertIn(
+            'Service with name OLX does not exist\n', out.getvalue()
+        )
 
     def test_command_should_update_service_env_in_cert(self):
         out = StringIO()
@@ -90,4 +106,6 @@ class UpdateServiceEnvTest(TestCase):
         expected = ServiceEnvironment.objects.get(
                     service__name='Ceneo', environment__name='prod'
         )
-        self.assertTrue(SSLCertificate.objects.get(service_environment=expected))
+        self.assertTrue(
+            SSLCertificate.objects.get(service_environment=expected)
+        )
