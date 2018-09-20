1e6ffa4c71bd759b060d2e76ebe2dcb00b30b06c
Bruno  Santeramo <bruno.santeramo@gmail.com>
2018-09-20 11:51:24 +0200

    create ralph as superuser if not yet exists

--- docker/createsuperuser.py
+++ docker/createsuperuser.py
@@ -7,8 +7,14 @@ django.setup()
 
 from django.contrib.auth import get_user_model
 print('Updating superuser info')
-u = get_user_model().objects.get(username='ralph')
-u.set_password('ralph')
-u.is_superuser = True
-u.is_staff = True
-u.save()
+try:
+    u = get_user_model().objects.get(username='ralph')
+    u.set_password('ralph')
+    u.is_superuser = True
+    u.is_staff = True
+    u.save()
+    print('Updated superuser')
+except:
+    u = get_user_model().objects.create_superuser('ralph', '', 'ralph')
+    u.save()
+    print('Created superuser')
