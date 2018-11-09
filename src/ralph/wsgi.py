3157a062e35fa4e2023e667c80e32c32e2795d41
xsuu <xasa@4js.com>
2018-11-09 18:48:37 +0100

    Handle environment variables from the WSGI process into the django application

--- src/ralph/wsgi.py
+++ src/ralph/wsgi.py
@@ -7,16 +7,34 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+## Loading WSGI environment variables into the django application:
+## https://exceptionshub.com/access-apache-setenv-variable-from-django-wsgi-py-file.html
+
 import os
-import django
-
 from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIHandler
 
-class WSGIEnvironment(WSGIHandler):
-    def __call__(self, environ, start_response):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ralph.settings")
-        django.setup()
-        return super(WSGIEnvironment, self).__call__(environ, start_response)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ralph.settings")
 
-application = WSGIEnvironment()
+KEYS_TO_LOAD = [
+    # A list of the keys you'd like to load from the WSGI environ
+    # into os.environ
+	'DATABASE_HOST',
+	'DATABASE_USER',
+	'DATABASE_NAME',
+	'DATABASE_PASSWORD'
+]
+
+def loading_app(wsgi_environ, start_response):
+    global real_app
+    for key in KEYS_TO_LOAD:
+        try:
+            os.environ[key] = wsgi_environ[key]
+        except KeyError:
+            # The WSGI environment doesn't have the key
+            pass
+    real_app = get_wsgi_application()
+    return real_app(wsgi_environ, start_response)
+
+real_app = loading_app
+
+application = lambda env, start: real_app(env, start)
