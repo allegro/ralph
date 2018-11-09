d40f15e1cdcdf9262e67db48c59614edd538c9c0
xsuu <xasa@4js.com>
2018-11-09 17:46:58 +0100

    Updating wsgi to load environment from outside

--- src/ralph/wsgi.py
+++ src/ralph/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ralph.settings")
+class WSGIEnvironment(WSGIHandler):
+    def __call__(self, environ, start_response):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ralph.settings")
+        django.setup()
+        return super(WSGIEnvironment, self).__call__(environ, start_response)
 
-application = get_wsgi_application()
+application = WSGIEnvironment()
