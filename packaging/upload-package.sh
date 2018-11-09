8259c46b28a59f5dac58283237aaae104ddee8fc
xsuu <xasa@4js.com>
2018-11-09 16:34:27 +0100

    Modifying BINTRAY_APIKEY condition to not fail the build if we don't use the Bintray's service

--- packaging/upload-package.sh
+++ packaging/upload-package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Uploading the package to bintray
+
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -13,6 +15,8 @@ if [ ! -z "$BINTRAY_APIKEY" ]; then
 	GENERIC_DEB_NAME=`basename $DEB_NAME`
 	curl -T $DEB_NAME -uvi4m:$BINTRAY_APIKEY "https://api.bintray.com/content/vi4m/ralph/ralph/3-snapshot/dists/wheezy/main/binary-amd64/$GENERIC_DEB_NAME;deb_distribution=wheezy;deb_component=main;deb_architecture=amd64?publish=1"
 else
-	echo "Error. BINTRAY_APIKEY is missing"
-	exit 1
+	echo "ERROR: Unable to upload package to BINTRAY" 2>&1
+	echo "BINTRAY_APIKEY is missing." 2>&1
+	## This should not fail the build
+	#exit 1
 fi
