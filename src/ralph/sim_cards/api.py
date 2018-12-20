f3e968350ba441ea407f66236dcb4453e13a780a
sekuranka <anngab@st.amu.edu.pl>
2018-12-20 14:22:04 +0100

    Add API for SIM cards (#3357)
    * Add API for SIM cards
    
    * Fixed make flake bugs

---
+++ src/ralph/sim_cards/api.py
@@ -0,0 +1,53 @@
+from ralph.accounts.api_simple import SimpleRalphUserSerializer
+from ralph.api import RalphAPIViewSet, router
+from ralph.assets.api.serializers import RalphAPISerializer
+from ralph.sim_cards.models import CellularCarrier, SIMCard, SIMCardFeatures
+
+
+class CellularCarrierSerializer(RalphAPISerializer):
+    class Meta:
+        model = CellularCarrier
+        fields = ['name']
+
+
+class SIMCardFeaturesSerializer(RalphAPISerializer):
+    class Meta:
+        model = SIMCardFeatures
+        fields = ['name']
+
+
+class SIMCardSerializer(RalphAPISerializer):
+    carrier = CellularCarrierSerializer()
+    features = SIMCardFeaturesSerializer(many=True)
+    user = SimpleRalphUserSerializer()
+    owner = SimpleRalphUserSerializer()
+
+    class Meta:
+        model = SIMCard
+        fields = ['status', 'card_number', 'phone_number', 'pin1', 'puk1',
+                  'user', 'owner', 'warehouse', 'carrier', 'features',
+                  'quarantine_until', 'modified']
+
+
+class CellularCarrierViewSet(RalphAPIViewSet):
+    queryset = CellularCarrier.objects.all()
+    serializer_class = CellularCarrierSerializer
+
+
+class SIMCardFeatureViewSet(RalphAPIViewSet):
+    queryset = SIMCardFeatures.objects.all()
+    serializer_class = SIMCardFeaturesSerializer
+
+
+class SIMCardViewSet(RalphAPIViewSet):
+    queryset = SIMCard.objects.all()
+    serializer_class = SIMCardSerializer
+    select_related = ['carrier', 'user', 'owner']
+    prefetch_related = ['features']
+    filter_fields = ['user__username', 'features__name']
+
+
+router.register(r'sim-card-feature', SIMCardFeatureViewSet)
+router.register(r'sim-card-cellular-carrier', CellularCarrierViewSet)
+router.register(r'sim-card', SIMCardViewSet)
+urlpatterns = []
