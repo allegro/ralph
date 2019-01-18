5f7f0f5ccc94872c10ba49cea6124f50840f5250
Roman Prykhodchenko <roman.prykhodchenko@allegro.pl>
2019-01-18 14:49:59 +0100

    Store default configuration of a superuser in the script

--- docker/provision/createsuperuser.py
+++ docker/provision/createsuperuser.py
@@ -7,11 +7,12 @@ django.setup()
 
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
-username = os.environ['RALPH_ADMIN_USERNAME']
-password = os.environ['RALPH_ADMIN_PASSWORD']
-email = os.environ['RALPH_ADMIN_EMAIL']
+username = os.getenv('RALPH_DEFAULT_SUPERUSER_NAME', 'ralph')
+password = os.getenv('RALPH_DEFAULT_SUPERUSER_PASSWORD', 'ralph')
+email = os.getenv('RALPH_DEFAULT_SUPERUSER_EMAIL', '')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
