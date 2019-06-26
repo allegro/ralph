b9de10184a368321e13def4b83df92219be1ba9f
Szymon Jasinski <szymon.jasinski@allegro.pl>
2019-06-26 12:13:53 +0200

    internationalize 404 page

--- src/ralph/lib/error_pages/views.py
+++ src/ralph/lib/error_pages/views.py
@@ -1,12 +1,12 @@
 from django import http
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import requires_csrf_token
 
 
 @requires_csrf_token
 def page_not_found(request):
-    body = (
-        "<h1>Not Found</h1>"
-        "<p>The requested resource was not found on this server.</p>"
-    )
+    header = _('Not Found')
+    paragraph = _('The requested resource was not found on this server.')
+    body = "<h1>{}</h1><p>{}</p>".format(header, paragraph)
     content_type = 'text/html'
     return http.HttpResponseNotFound(body, content_type=content_type)
