5eeb2bd5f1c8ae5362e3fc8658cfaed694929394
Marek Bleschke <marek.bleschke@allegro.pl>
2019-12-13 07:58:48 +0100

    allow manually specify dates for failure operations (#3494)
    * allow manually specify dates for failure operations

--- src/ralph/static/src/js/foundation-datepicker-init.js
+++ src/ralph/static/src/js/foundation-datepicker-init.js
@@ -1,4 +1,9 @@
 (function ($) {
     'use strict';
     $('.datepicker').fdatepicker({format: 'yyyy-mm-dd'});
+    $('.datepicker-with-time').fdatepicker({
+        format: 'yyyy-mm-dd hh:ii:ss',
+        disableDblClickSelection: true,
+        pickTime: true
+    });
 }(ralph.jQuery));
