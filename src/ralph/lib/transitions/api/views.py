77de88e0c2902d08e0190756b4de40d72fbf77d4
lukasz.karykowski <lukasz.karykowski@allegro.pl>
2020-10-21 11:11:27 +0200

    Added Filter Fields by barcode to Transitions History Serializer

--- src/ralph/lib/transitions/api/views.py
+++ src/ralph/lib/transitions/api/views.py
@@ -63,7 +63,7 @@ FIELD_MAP = {
 class TransitionJobViewSet(RalphReadOnlyAPIViewSet):
     queryset = TransitionJob.objects.all()
     serializer_class = TransitionJobSerializer
-    filter_fields = ['object_id']
+    filter_fields = ['object_id', 'barcode']
 
 
 class TransitionModelViewSet(RalphReadOnlyAPIViewSet):
