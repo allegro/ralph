26a0b845465c0a16bf193ec92cb39418f345613b
Przemyslaw Bromber <przemyslaw.bromber@allegro.pl>
2020-10-27 18:55:49 +0100

    dnsaas initial mod - fixes and typehints

--- src/ralph/dns/dnsaas.py
+++ src/ralph/dns/dnsaas.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
-from urllib.parse import urlencode, urljoin
+from urllib.parse import urlencode, urljoin, parse_qsl
 
 import requests
 from dj.choices import Choices
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-
+from typing import Optional, List, Tuple
 
 logger = logging.getLogger(__name__)
+
+QueryParams = List[Tuple[str, str]]
+
 
 class RecordType(Choices):
     _ = Choices.Choice
@@ -21,8 +24,8 @@ class RecordType(Choices):
 
 class DNSaaS:
 
-    def __init__(self, headers=None):
-        # TODO(pbromber): Oauth comes here
+    def __init__(self, headers: dict = None):
+        # TODO: ograć tu oautha
         self.session = requests.Session()
         _headers = {
             'Authorization': 'Token {}'.format(settings.DNSAAS_TOKEN),
@@ -33,14 +36,14 @@ class DNSaaS:
             _headers.update(headers)
         self.session.headers.update(_headers)
 
-    def build_url(self, resource_name, id=None, get_params=None):
+    @staticmethod
+    def build_url(resource_name: str, id: int = None, get_params: QueryParams = None) -> str:
         """
         Return Url for DNSAAS endpoint
 
         Args:
             resource_name: Resource name (example: domains)
             id: Record ID,
-            version: Api Version
             get_params: List of tuple to URL GET params: (example:
                 [('name', 'value')]
             )
@@ -57,41 +60,54 @@ class DNSaaS:
             result_url = "{}?{}".format(result_url, urlencode(get_params))
         return result_url
 
-    def _add_query_params(self, url, query_params):
-        result_url = "{}?{}".format(url, urlencode(query_params))
+    @staticmethod
+    def _set_page_qp(url: str, page: int):
+        url_qp = url.split('?')
+        url = url_qp[0]
+        qp = []
+        if len(url_qp) == 2:
+            qp = parse_qsl(url_qp[1])
+            for index, query_param in enumerate(qp):
+                if query_param[0] == 'page':
+                    qp.pop(index)
+        qp.extend([('page', str(page)), ])
+
+        result_url = "{}?{}".format(url, urlencode(qp))
         return result_url
 
-    def get_api_result(self, url, query_params=None, page=0):
+    def get_api_result(self, url: str, page: int = 0) -> List[dict]:
         """
         Returns 'results' from DNSAAS API.
 
         Args:
-            url: Url to API
+            :str url: Url to API
+            :int page:
 
         Returns:
             list of records
+
         """
-        if query_params:
-            get_url = "{}?{}".format(url, urlencode(query_params))
-        status_code, json_data = self._get(get_url)
+        status_code, json_data = self._get(url)
         api_results = json_data.get('content', [])
         if not json_data.get('last', None):
             page = page + 1
-            next_url = self._add_query_params(url, ('page', page))
+            next_url = self._set_page_qp(url, page)
             _api_results = self.get_api_result(next_url, page=page)
             api_results.extend(_api_results)
         return api_results
 
-    def get_dns_records(self, ipaddresses):
+    def get_dns_records(self, ipaddresses: List[str]) -> List[dict]:
         """Gets DNS Records for `ipaddresses` by API call"""
         dns_records = []
         if not ipaddresses:
             return []
         ipaddresses = [('ip', i) for i in ipaddresses]
         url = self.build_url(
-            'records'
+            'records',
+            get_params=[
+                           ('size', '100'),
+                       ] + ipaddresses
         )
-        url = self._add_query_params(url, [('size', 100), ] + ipaddresses)
         api_results = self.get_api_result(url)
         ptrs = set([i['content'] for i in api_results if i['type'] == 'PTR'])
 
@@ -107,7 +123,7 @@ class DNSaaS:
                 })
         return sorted(dns_records, key=lambda x: x['type'])
 
-    def update_dns_record(self, record):
+    def update_dns_record(self, record: dict) -> Optional[dict]:
         """
         Update DNS Record in DNSAAS
 
@@ -128,31 +144,22 @@ class DNSaaS:
         if status_code != 202:
             return response_data
 
-    def _response2result(self, response):
+    @staticmethod
+    def _response2result(response: requests.Response) -> Optional[dict]:
         if response.status_code == 500:
             return {
                 'non_field_errors': ['Internal Server Error from DNSAAS']
             }
-        elif response.status_code == 202:
-            logger.error(
-                "User '{}' has insufficient permission".format(
-                    settings.DNSAAS_OWNER
-                )
-            )
-            return {
-                'non_field_errors': [
-                    "Your request couldn't be handled, try later."
-                ]
-            }
         elif response.status_code != 201 and response.status_code != 204:
             return response.json()
 
-    def create_dns_record(self, record, service=None):
+    def create_dns_record(self, record: dict, service=None) -> Optional[dict]:
         """
         Create new DNS record.
 
         Args:
-            records: Record cleaned data
+            record: Record cleaned data
+            service:
 
         Returns:
             Validation error from API or None if create correct
@@ -174,7 +181,7 @@ class DNSaaS:
             return {'name': [_('Service not found.')]}
         return self._post(url, data)[1]
 
-    def _send_request_to_dnsaas(self, request_method, url, json_data=None):
+    def _send_request_to_dnsaas(self, request_method: str, url: str, json_data: dict = None) -> requests.Response:
         try:
             response = self.session.request(
                 method=request_method,
@@ -205,7 +212,7 @@ class DNSaaS:
             )
             raise
 
-    def _post(self, url, data):
+    def _post(self, url: str, data: dict) -> [int, Optional[dict]]:
         """
         Send post data to URL.
 
@@ -219,26 +226,26 @@ class DNSaaS:
         response = self._send_request_to_dnsaas('POST', url, json_data=data)
         return response.status_code, self._response2result(response)
 
-    def _delete(self, url):
+    def _delete(self, url: str) -> [int, Optional[dict]]:
         response = self._send_request_to_dnsaas('DELETE', url)
 
         return response.status_code, self._response2result(response)
 
-    def _get(self, url):
+    def _get(self, url: str) -> [int, Optional[dict]]:
         response = self._send_request_to_dnsaas('GET', url)
 
         return response.status_code, self._response2result(response)
 
-    def _patch(self, url, data):
+    def _patch(self, url: str, data: dict) -> [int, Optional[dict]]:
         response = self._send_request_to_dnsaas('PATCH', url, json_data=data)
         return response.status_code, self._response2result(response)
 
-    def delete_dns_record(self, record_id):
+    def delete_dns_record(self, record_id: int) -> dict:
         """
         Delete record in DNSAAS.
 
         Args:
-            record_ids: ID's to delete
+            record_id: ID to delete
 
         Returns:
             Validation error from API or None if delete correct
@@ -248,7 +255,7 @@ class DNSaaS:
 
         return data
 
-    def send_ipaddress_data(self, ip_record_data):
+    def send_ipaddress_data(self, ip_record_data: dict):
         """
         Send data about IP address and hostname.
 
