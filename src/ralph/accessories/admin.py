c075a3db06860de31c04ce7ad7d352922cfcdfb0
lukasz.karykowski <lukasz.karykowski@allegro.pl>
2020-11-23 13:33:38 +0100

    Added Assigned to User site, and adding calculating (free and Used).

--- src/ralph/accessories/admin.py
+++ src/ralph/accessories/admin.py
@@ -1,8 +1,23 @@
 from django.utils.translation import ugettext_lazy as _
 
-from ralph.accessories.models import Accessories
-from ralph.admin import RalphAdmin, register
+from ralph.accessories.models import Accessories, AccessoriesUser
+from ralph.admin import RalphAdmin, RalphTabularInline, register
+from ralph.admin.views.extra import RalphDetailViewAdmin
 from ralph.lib.transitions.admin import TransitionAdminMixin
+
+
+class AccessoriesUserView(RalphDetailViewAdmin):
+    icon = 'user'
+    name = 'users'
+    label = _('Assigned to users')
+    url_name = 'assigned-to-users'
+
+    class AccessoriesUserInline(RalphTabularInline):
+        model = AccessoriesUser
+        raw_id_fields = ('user',)
+        extra = 1
+
+    inlines = [AccessoriesUserInline]
 
 
 @register(Accessories)
@@ -16,6 +31,7 @@ class AccessoriesAdmin(TransitionAdminMixin, RalphAdmin):
     list_filter = ['status', 'manufacturer', 'accessories_name',
                    'product_number', 'user', 'owner', 'user__segment',
                    'user__company', 'user__department']
+    change_views = [AccessoriesUserView, ]
     search_fields = ['manufacturer', 'accessories_name',
                      'user__first_name', 'user__last_name', 'user__username',
                      'product_number']
--- src/ralph/accessories/models.py
+++ src/ralph/accessories/models.py
@@ -1,16 +1,14 @@
 import reversion
 from dj.choices import Choices
-from django.core.exceptions import ValidationError
+from django.conf import settings
 from django.db import models
-from django.db.models import Sum, Prefetch
+from django.db.models import Prefetch, Sum
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
 from ralph.accounts.models import RalphUser, Regionalizable
-from ralph.admin.helpers import getattr_dunder
 from ralph.assets.models import BaseObject
 from ralph.back_office.models import Warehouse
-from ralph.lib.mixins.fields import BaseObjectForeignKey
 from ralph.lib.mixins.models import AdminAbsoluteUrlMixin, TimeStampMixin
 from ralph.lib.polymorphic.models import PolymorphicQuerySet
 from ralph.lib.transitions.fields import TransitionField
@@ -20,75 +18,33 @@ from ralph.lib.transitions.models import TransitionWorkflowBaseWithPermissions
 _SELECT_USED_ACCESSORIES_QUERY = """
     SELECT COALESCE(SUM({assignment_table}.{quantity_column}), 0)
     FROM {assignment_table}
-    WHERE {assignment_table}.{licence_id_column} = {licence_table}.{id_column}
+    WHERE {assignment_table}.{accessories_id_column} = {accessories_table}.{id_column}
 """
 
 
 class AccessoriesUsedFreeManager(models.Manager):
     def get_queryset(self):
-        """
-        Use subqueries in select to calculate licences used by users and
-        base objects.
-        """
-        # Coalesce is used here to provide default value for Sum (in other
-        # case None value is returned)
-        # read https://code.djangoproject.com/ticket/10929 for more info
-        # about default value for Sum
         id_column = Accessories.baseobject_ptr.field.column
 
-        user_quantity_field = Accessories.users.through._meta.get_field('quantity')
-        user_accessories_field = Accessories.users.through._meta.get_field('accessories')
+        user_quantity_field = Accessories.users.through._meta.get_field('quantity') # noqa
+        user_accessories_field = Accessories.users.through._meta.get_field('accessories') # noqa
         user_count_query = _SELECT_USED_ACCESSORIES_QUERY.format(
             assignment_table=Accessories.users.through._meta.db_table,
             quantity_column=user_quantity_field.db_column or user_quantity_field.column,  # noqa
-            licence_id_column=user_accessories_field.db_column or user_accessories_field,  # noqa
-            licence_table=Accessories._meta.db_table,
-            id_column=id_column,
-        )
-
-        base_object_quantity_field = Accessories.base_objects.through._meta.get_field('quantity')  # noqa
-        base_object_accessories_field = Accessories.base_objects.through._meta.get_field('licence')  # noqa
-        base_object_count_query = _SELECT_USED_ACCESSORIES_QUERY.format(
-            assignment_table=Accessories.base_objects.through._meta.db_table,
-            quantity_column=base_object_quantity_field.db_column or base_object_quantity_field.column,  # noqa
-            licence_id_column=base_object_accessories_field.db_column or base_object_accessories_field.column,  # noqa
-            licence_table=Accessories._meta.db_table,
+            accessories_id_column=user_accessories_field.db_column or user_accessories_field,  # noqa
+            accessories_table=Accessories._meta.db_table,
             id_column=id_column,
         )
 
         return super().get_queryset().extra(
             select={
                 'user_count': user_count_query,
-                'baseobject_count': base_object_count_query,
             }
-        )
-
-ACCESSORIES_RELATED_OBJECTS_PREFETCH_RELATED = [
-    'users',
-    # prefetch all baseobjects related with licence; this allows to call
-    # [bol.base_object for bol in licence.baseobjectlicence_set.all()]
-    # without additional queries
-    Prefetch(
-        'baseobjectlicence_set__base_object',
-        # polymorphic manager is used to get final instance of the object
-        # (ex. DataCenterAsset)
-        queryset=BaseObject.polymorphic_objects.all()
-    )
-]
-
-
-class AccessoriesRelatedObjectsManager(models.Manager):
-    """
-    Prefetch related objects by-default
-    """
-    def get_queryset(self):
-        return super().get_queryset().prefetch_related(
-            *ACCESSORIES_RELATED_OBJECTS_PREFETCH_RELATED
         )
 
 
 class AccessoriesUsedFreeRelatedObjectsManager(
-    AccessoriesUsedFreeManager, AccessoriesRelatedObjectsManager
+    AccessoriesUsedFreeManager
 ):
     pass
 
@@ -173,12 +129,6 @@ class Accessories(
         Warehouse,
         on_delete=models.PROTECT
     )
-    base_objects = models.ManyToManyField(
-        BaseObject,
-        verbose_name=_('assigned base objects'),
-        through='BaseObjectAccessories',
-        related_name='+',
-    )
 
     @cached_property
     def used(self):
@@ -186,12 +136,12 @@ class Accessories(
             return 0
         try:
             # try use fields from objects_used_free manager
-            return (self.user_count or 0) + (self.baseobject_count or 0)
+            return (self.user_count or 0)
         except AttributeError:
             base_objects_qs = self.base_objects.through.objects.filter(
-                Accessories=self
+                accessories=self
             )
-            users_qs = self.users.through.objects.filter(Accessories=self)
+            users_qs = self.users.through.objects.filter(accessories=self)
 
             def get_sum(qs):
                 return qs.aggregate(sum=Sum('quantity'))['sum'] or 0
@@ -205,10 +155,8 @@ class Accessories(
         return self.number_bought - self.used
     free._permission_field = 'number_bought'
 
-
     polymorphic_objects = PolymorphicQuerySet.as_manager()
     objects_used_free = AccessoriesUsedFreeManager()
-    objects_with_related = AccessoriesRelatedObjectsManager()
     objects_used_free_with_related = AccessoriesUsedFreeRelatedObjectsManager()
 
     def __str__(self):
@@ -229,32 +177,18 @@ class Accessories(
 
 
 @reversion.register()
-class BaseObjectAccessories(models.Model):
+class AccessoriesUser(models.Model):
     accessories = models.ForeignKey(Accessories)
-    base_object = BaseObjectForeignKey(
-        BaseObject,
-        related_name='accessories',
-        verbose_name=_('Asset'),
-        limit_models=['back_office.BackOfficeAsset',]
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='accessories'
     )
     quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
-        unique_together = ('accessories', 'base_object')
+        unique_together = ('accessories', 'user')
 
     def __str__(self):
         return '{} of {} assigned to {}'.format(
-            self.quantity, self.accessories, self.base_object
+            self.quantity, self.accessories, self.user,
         )
-
-    def clean(self):
-        bo_asset = getattr_dunder(
-            self.base_object, 'asset__backofficeasset'
-        )
-        if (
-            bo_asset and self.accessories and
-            self.accessories.region_id != bo_asset.region_id
-        ):
-            raise ValidationError(
-                _('Asset region is in a different region than Accessories.')
-            )
