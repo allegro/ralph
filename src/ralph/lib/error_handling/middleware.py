dbf6dfaea247b179eb1732fedb2d1a2370cf4d13
Pawel Szulc <pawel.szulc@allegro.pl>
2021-02-18 11:39:25 +0100

    log start timestamp

--- src/ralph/lib/error_handling/middleware.py
+++ src/ralph/lib/error_handling/middleware.py
@@ -1,6 +1,7 @@
 import logging
 import traceback
 
+from django.conf import settings
 from django.db import OperationalError
 
 from ralph.lib.error_handling.exceptions import WrappedOperationalError
@@ -14,16 +15,20 @@ class OperationalErrorHandlerMiddleware:
         if exception:
             if isinstance(exception, OperationalError):
                 logger.error("OperationalError occured. URI: %s, "
-                             "user: %s, exception: %s",
+                             "user: %s, exception: %s, "
+                             "django running since: %s",
                              request.build_absolute_uri(), request.user,
-                             exception, exc_info=True, stack_info=True)
+                             exception, settings.START_TIMESTAMP,
+                             exc_info=True, stack_info=True)
             elif isinstance(exception, WrappedOperationalError):
                 inner_exc = exception.__context__
                 logger.error("WrappedOperationalError occured. URI: %s, "
                              "user: %s, SQL query: %s, "
-                             "model object: %s, inner exception traceback: %s",
+                             "model object: %s, inner exception traceback: %s, "
+                             "django running since: %s",
                              request.build_absolute_uri(), request.user,
                              exception.query, exception.model.__dict__,
                              traceback.format_tb(inner_exc.__traceback__),
+                             settings.START_TIMESTAMP,
                              exc_info=True, stack_info=True)
         return None
