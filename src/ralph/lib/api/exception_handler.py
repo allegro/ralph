21985d7b5729b53656a634e9cd28a81c141d1fe2
Pawel Szulc <pawel.szulc@allegro.pl>
2021-03-29 13:15:08 +0200

    Fix line length

--- src/ralph/lib/api/exception_handler.py
+++ src/ralph/lib/api/exception_handler.py
@@ -7,8 +7,8 @@ from rest_framework.views import exception_handler as drf_exception_handler
 def validation_error_exception_handler(exc, context):
     """
     Handle Django ValidationError as an accepted exception,
-    There are 2 ValidationErrors, problem occurs when using same validator in form and API,
-    e.g. MACAddressField
+    There are 2 ValidationErrors, problem occurs
+    when using same validator in form and API e.g. MACAddressField
     """
 
     if isinstance(exc, ValidationError):
