dc5fc54a785212044e9b6bc51bc5d788d581ea89
lukasz.karykowski <lukasz.karykowski@allegro.pl>
2021-05-07 09:49:29 +0200

    changed release accessories.

--- src/ralph/accessories/models.py
+++ src/ralph/accessories/models.py
@@ -112,16 +112,13 @@ class Accessory(
     )
     def release_accessories(cls, instances, **kwargs):
         user = get_user_model().objects.get(pk=int(kwargs['user']))
-        accessory_user = AccessoryUser.objects.filter(user=user, accessory=instances[0]) # noqa
-        if len(accessory_user) > 0:
-            user_accessory = accessory_user[0]
-            user_accessory.quantity += kwargs['quantity']
-            AccessoryUser.save(user_accessory, force_update=True)
-        else:
-            AccessoryUser.objects.create(
-                user=user, quantity=kwargs['quantity'],
-                accessory_id=instances[0].id
-            )
+        accessory_user, created = AccessoryUser.objects.get_or_create(
+            user=user,
+            accessory=instances[0],
+        )
+        if created:
+            accessory_user.quantity += kwargs['quantity']
+            accessory_user.save()
 
 
 @reversion.register()
