7f1933a3952669f9c50b73ae1d0813abfc0e1f68
Szymon Jasiński <szymon.jasinski@allegro.pl>
2021-11-12 13:45:49 +0100

    Data Center Asset email report (#3674)
    * Data Center Asset email report

---
+++ src/ralph/reports/tests/test_email_reports.py
@@ -0,0 +1,73 @@
+import mimetypes
+from unittest import mock
+
+from ddt import data, ddt, unpack
+from django.core import management
+from django.core.management import CommandError
+
+from ralph.data_center.tests.factories import DataCenterAssetFactory
+from ralph.tests import RalphTestCase
+
+EXAMPLE_EMAIL = 'example@example.com'
+
+
+@ddt
+class TestEmailReports(RalphTestCase):
+
+    def setUp(self):
+        DataCenterAssetFactory.create_batch(10)
+
+    def call_command(
+        self,
+        output_format='.csv',
+        recipient_email=EXAMPLE_EMAIL,
+        sender_email=EXAMPLE_EMAIL
+    ):
+        management.call_command(
+            'send_data_center_asset_export',
+            output_format=output_format,
+            recipient_email=recipient_email,
+            sender_email=sender_email
+        )
+
+    @unpack
+    @data(
+        (
+            {'recipient_email': 'this_is_not_an_email'},
+            "recipient-email: Enter a valid email address."
+        ),
+        (
+            {'sender_email': 'this_is_not_an_email'},
+            "sender-email: Enter a valid email address."
+        ),
+    )
+    def test_data_center_asset_report_validates_recipient_email(
+        self, command_kwargs, error_message
+    ):
+        with self.assertRaises(CommandError) as error:
+            self.call_command(
+                **command_kwargs
+            )
+            self.assertEqual(
+                error_message,
+                error
+            )
+
+    @data('.csv', '.ods', '.xlsx')
+    @mock.patch(
+        'ralph.reports.management.commands.'
+        'send_data_center_asset_export.send_email_with_attachment'
+    )
+    def test_data_center_asset_report_expected_formats(
+        self, output_format, send_email_with_attachment
+    ):
+        mimetype = mimetypes.types_map[output_format]
+        filename = 'report' + output_format
+        self.call_command(output_format=output_format)
+
+        call_args = send_email_with_attachment.call_args[0]
+
+        self.assertEqual(EXAMPLE_EMAIL, call_args[0])
+        self.assertEqual(EXAMPLE_EMAIL, call_args[1])
+        self.assertEqual(filename, call_args[5])
+        self.assertEqual(mimetype, call_args[6])
---
+++ src/ralph/reports/tests/test_resources.py
@@ -0,0 +1,86 @@
+from random import randint
+
+from ralph.assets.tests.factories import (
+    DataCenterAssetModelFactory,
+    EthernetFactory
+)
+from ralph.data_center.models import Orientation, RackOrientation
+from ralph.data_center.tests.factories import (
+    DataCenterAssetFactory,
+    RackFactory
+)
+from ralph.networks.tests.factories import IPAddressFactory
+from ralph.reports.resources import DataCenterAssetTextResource
+from ralph.tests import RalphTestCase
+
+
+class TestEmailReports(RalphTestCase):
+
+    def setUp(self):
+        self.rack = RackFactory()
+        self.asset = DataCenterAssetFactory(
+            rack=self.rack, position=randint(0, 100)
+        )
+        self.ethernets = EthernetFactory.create_batch(
+            2, base_object=self.asset
+        )
+        self.ip = IPAddressFactory(
+            base_object=self.asset, is_management=False,
+            ethernet=self.ethernets[0]
+        )
+        self.management = IPAddressFactory(
+            base_object=self.asset, is_management=True,
+            ethernet=self.ethernets[1]
+        )
+
+    def test_dataset_contains_asset_data(self):
+        dataset = DataCenterAssetTextResource().export()
+        lines = dataset.csv.split('\r\n')
+
+        expected_headers = "dc,server_room,rack,rack_orientation," \
+                           "orientation,position,model,hostname," \
+                           "management_ip,ip,barcode,sn,service_uid,service," \
+                           "environment,configuration_path"
+        expected_line_data = [
+            self.rack.server_room.data_center.name,
+            self.rack.server_room.name,
+            self.rack.name,
+            RackOrientation.from_id(self.rack.orientation).name,
+            Orientation.from_id(self.asset.orientation).name,
+            str(self.asset.position),
+            self.asset.model.name,
+            self.asset.hostname,
+            self.management.address,
+            self.ip.address,
+            self.asset.barcode,
+            self.asset.sn,
+            self.asset.service_env.service.uid,
+            self.asset.service_env.service.name,
+            self.asset.service_env.environment.name,
+            self.asset.configuration_path.path
+        ]
+        expected_line = ','.join(expected_line_data)
+        self.assertEqual(expected_headers, lines[0])
+        self.assertEqual(expected_line, lines[1])
+
+    def test_queries_number(self):
+        for _ in range(0, 10):
+            rack = RackFactory()
+            for position in range(1, 6):
+                model = DataCenterAssetModelFactory(has_parent=True)
+                asset = DataCenterAssetFactory(
+                    rack=rack, position=position, model=model
+                )
+                ethernets = EthernetFactory.create_batch(
+                    2, base_object=asset
+                )
+                IPAddressFactory(
+                    base_object=asset, is_management=False,
+                    ethernet=ethernets[0]
+                )
+                IPAddressFactory(
+                    base_object=asset, is_management=True,
+                    ethernet=ethernets[1]
+                )
+        with self.assertNumQueries(103):
+            DataCenterAssetTextResource().export()
