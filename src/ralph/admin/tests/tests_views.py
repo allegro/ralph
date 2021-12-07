7dcc533a3fd72e2821cd7af713ea312d48e176ad
Pawel Szulc <pawel.szulc@allegro.pl>
2021-12-07 11:16:10 +0100

    Exclude TradeMark from view test

--- src/ralph/admin/tests/tests_views.py
+++ src/ralph/admin/tests/tests_views.py
@@ -132,6 +132,7 @@ EXCLUDE_MODELS = [
     'ralph.tests.models.Manufacturer',
     'ralph.tests.models.Order',
     'ralph.tests.models.PolymorphicTestModel',
+    'ralph.trade_marks.models.TradeMark',
     'ralph.trade_marks.models.TradeMarkKind',
 ]
 
