177f18f3c24511928c51bbcb3ea83030317c6252
Szymon Jasiński <szymon.jasinski@allegro.pl>
2022-01-11 16:53:29 +0100

    Openstack sync changed since parameter 2 (#3697)
    * incremental openstack sync

--- src/ralph/lib/openstack/client.py
+++ src/ralph/lib/openstack/client.py
@@ -1,5 +1,11 @@
 import logging
+import re
 from functools import lru_cache
+
+from django.conf import settings
+
+from ralph.lib import network
+from ralph.settings import DEFAULT_OPENSTACK_PROVIDER_NAME
 
 try:
     from keystoneclient.v2_0 import client as ks_v2_client
@@ -60,7 +66,11 @@ class RalphIronicClient:
 
 
 class RalphOpenstackClient:
-
+    """
+    This is a single OpenStack instance client. It handles keystone
+    authentication. It has methods to fetch OpenStack data useful from
+    Ralph's perspective from openstack compute and openstack keystone.
+    """
     def __init__(self, site):
         if not nova_client_exists:
             logger.error("novaclient module is not installed")
@@ -176,7 +186,9 @@ class RalphOpenstackClient:
                 break
 
         servers = list(map(lambda server: server.__dict__, servers))
-        if len(servers) == 0:
+        # If only an incremental update is being performed, it is possible
+        # that no servers are returned. Otherwise, zero servers is an error.
+        if len(servers) == 0 and not search_opts.get('changes-since'):
             raise EmptyListError(
                 'Got an empty list of instances from {}'.format(
                     self.site['auth_url']
@@ -193,3 +205,154 @@ class RalphOpenstackClient:
         else:
             projects_resource = self.keystone_client.tenants
         yield from projects_resource.list()
+
+
+class RalphOpenStackInfrastructureClient:
+    """
+    This is an OpenStack client designed to connect with multiple (or all)
+    OpenStack instances that may exist in your local infrastructure.
+    It instantiates a separate RalphOpenStackClient instance to communicate
+    with each OpenStack instance. It has methods to extract
+    and accumulate data from all instances in one place.
+    """
+    def __init__(self, openstack_provider_name):
+        super().__init__()
+        self.DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+        self.openstack_provider_name = openstack_provider_name
+        self.clients = self._get_instances_from_settings()
+
+    def _get_instances_from_settings(self):
+        """
+        Filter instances from OPENSTACK_INSTANCES (from settings) and
+        initialize clients only for the ones matching current
+        openstack provider.
+        """
+        clients = []
+        for os_instance in settings.OPENSTACK_INSTANCES:
+            if os_instance.get(
+                'provider', DEFAULT_OPENSTACK_PROVIDER_NAME
+            ) == self.openstack_provider_name:
+                clients.append(RalphOpenstackClient(os_instance))
+        return clients
+
+    @classmethod
+    def _get_flavor_data(cls, client, flavor):
+        return {
+            'name': flavor['name'],
+            'cores': flavor['vcpus'],
+            'memory': flavor['ram'],
+            'disk': flavor['disk'] * 1024,
+            'tag': client.site['tag'],
+        }
+
+    def get_openstack_flavors(self):
+        openstack_flavors = {}
+        for client in self.clients:
+            logger.info('Processing {} ({})'.format(
+                client.site['auth_url'], client.site['tag']
+            ))
+            for flavor in client.get_flavors_list():
+                openstack_flavors[flavor['id']] = self._get_flavor_data(
+                    client, flavor
+                )
+        return openstack_flavors
+
+    def get_openstack_projects(self):
+        openstack_projects = {}
+        for client in self.clients:
+            logger.info('Processing {} ({})'.format(
+                client.site['auth_url'], client.site['tag']
+            ))
+            for project in client.get_keystone_projects():
+                if project.id not in openstack_projects:
+                    openstack_projects[project.id] = {
+                        'name': project.name,
+                        'servers': {},
+                        'tags': []
+                    }
+                openstack_projects[project.id]['tags'].append(
+                    client.site['tag']
+                )
+        return openstack_projects
+
+    def get_openstack_instances_data(
+        self, openstack_projects, openstack_flavors, search_opts=None
+    ):
+        """
+        Take openstack_projects dictionary and populate it with instances
+        (servers). If any flavor is missing, add it to the openstack_flavors
+        dictionary.
+
+        :param openstack_flavors: dictionary of openstack flavors
+        :param openstack_projects: dictionary of openstack projects
+        :return: updated openstack_projects and openstack_flavors
+        :param search_opts: dictionary of search options
+        """
+        default_search_opts = {'all_tenants': True}
+        if not search_opts:
+            search_opts = {}
+        search_opts.update(default_search_opts)
+
+        for client in self.clients:
+            logger.info('Processing {} ({})'.format(
+                client.site['auth_url'], client.site['tag']
+            ))
+            for server in client.get_servers_list(search_opts=search_opts):
+                project_id = server['tenant_id']
+                host_id = server['id']
+                image_name = client.get_image_name(
+                     server['image']['id']
+                ) if server['image'] else None
+                flavor_id = server['flavor']['id']
+                new_server = {
+                    'hostname': None,
+                    'id': server['id'],
+                    'flavor_id': flavor_id,
+                    'tag': client.site['tag'],
+                    'ips': {},
+                    'created': server['created'],
+                    'hypervisor': server['OS-EXT-SRV-ATTR:hypervisor_hostname'],  # noqa: E501
+                    'image': image_name,
+                    'status': server['status']
+                }
+                for zone in server['addresses']:
+                    if (
+                        'network_regex' in client.site and
+                        not re.match(client.site['network_regex'], zone)
+                    ):
+                        continue
+                    for ip in server['addresses'][zone]:
+                        addr = ip['addr']
+                        # fetch FQDN from DNS by IP address
+                        hostname = network.hostname(addr)
+                        logger.debug('Get IP {} ({}) for {}'.format(
+                            addr, hostname, server['id']
+                        ))
+                        new_server['ips'][addr] = hostname
+                        if not new_server['hostname']:
+                            new_server['hostname'] = hostname
+                # fallback to default behavior if FQDN could not be fetched
+                # from DNS
+                new_server['hostname'] = (
+                    new_server['hostname'] or server['name']
+                )
+                try:
+                    openstack_projects[project_id]['servers'][host_id] = (
+                        new_server
+                    )
+                except KeyError:
+                    logger.warning(
+                        'Project %s not found for server %s',
+                        project_id, host_id
+                    )
+
+                if flavor_id not in openstack_flavors:
+                    logger.warning(
+                        'Flavor %s (found in host %s) not in flavors list.'
+                        ' Fetching it', flavor_id, host_id)
+                    flavor = client.nova_client.flavors.get(flavor_id).__dict__
+                    openstack_flavors[flavor['id']] = self._get_flavor_data(
+                        client,
+                        flavor,
+                    )
+        return openstack_projects, openstack_flavors
