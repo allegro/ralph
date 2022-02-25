f2fb9ce29c5eca7105a712715474c0cbc39df56f
Szymon Jasiński <szymon.jasinski@allegro.pl>
2022-02-25 13:26:39 +0100

    trace request id if operational error occurs (#3713)
    * trace request id if operational error occurs

--- src/ralph/lib/error_handling/middleware.py
+++ src/ralph/lib/error_handling/middleware.py
@@ -13,13 +13,18 @@ logger = logging.getLogger(__name__)
 class OperationalErrorHandlerMiddleware:
     def process_exception(self, request, exception):
         if exception:
+            request_id = request.META.get('HTTP_X_REQUEST_ID', '-')
             if isinstance(exception, OperationalError):
                 logger.error("OperationalError occured. URI: %s, "
                              "user: %s, exception: %s, "
-                             "django running since: %s",
+                             "django running since: %s, "
+                             "request id: %s",
                              request.build_absolute_uri(), request.user,
                              exception, settings.START_TIMESTAMP,
-                             exc_info=True, stack_info=True)
+                             request_id,
+                             exc_info=True, stack_info=True,
+                             extra={'request_id': request_id}
+                             )
                 raise exception
             elif isinstance(exception, WrappedOperationalError):
                 inner_exc = exception.__context__
@@ -27,12 +32,15 @@ class OperationalErrorHandlerMiddleware:
                              "user: %s, SQL query: %s, "
                              "model object: %s, original_error: %s, "
                              "inner exception traceback: %s, "
-                             "django running since: %s",
+                             "django running since: %s, "
+                             "request id: %s",
                              request.build_absolute_uri(), request.user,
                              str(exception.query), exception.model.__dict__,
                              exception.original_error_str,
                              traceback.format_tb(inner_exc.__traceback__),
-                             settings.START_TIMESTAMP,
-                             exc_info=True, stack_info=True)
+                             settings.START_TIMESTAMP, request_id,
+                             exc_info=True, stack_info=True,
+                             extra={'request_id': request_id}
+                             )
                 raise inner_exc
         return None
