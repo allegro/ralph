e79756b8b15bd767e8fe24d26ec25152a9d1aad3
lukasz.karykowski <lukaszkarykowski@gmail.com>
2022-04-15 11:11:07 +0200

    ITS-368993 | removed new status, and renamed, old unused.

--- src/ralph/back_office/migrations/0013_auto_20220415_1110.py
+++ src/ralph/back_office/migrations/0013_auto_20220415_1110.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='backofficeasset',
             name='status',
-            field=ralph.lib.transitions.fields.TransitionField(default=1, choices=[(1, 'new'), (2, 'in progress'), (3, 'waiting for release'), (4, 'in use'), (5, 'loan'), (6, 'damaged'), (7, 'liquidated'), (8, 'in service'), (9, 'installed'), (10, 'free'), (11, 'reserved'), (12, 'sale'), (13, 'loan in progress'), (14, 'return in progress'), (15, 'to find'), (16, 'sent')]),
+            field=ralph.lib.transitions.fields.TransitionField(default=1, choices=[(1, 'new'), (2, 'in progress'), (3, 'waiting for release'), (4, 'in use'), (5, 'loan'), (6, 'damaged'), (7, 'liquidated'), (8, 'in service'), (9, 'sent'), (10, 'free'), (11, 'reserved'), (12, 'sale'), (13, 'loan in progress'), (14, 'return in progress'), (15, 'to find')]),
         ),
     ]
