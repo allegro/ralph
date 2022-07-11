6ad74fbd24586045c20e8e76dbcd0188077d3543
awieckowski <aleksander.wieckowski@allegro.pl>
2022-07-11 15:40:55 +0200

    Revert "Add additional transitions history filter fields"
    This reverts commit edc50b12266e3a32b4bd56433d6cd5f3076ad7b9.

--- src/ralph/lib/transitions/api/views.py
+++ src/ralph/lib/transitions/api/views.py
@@ -70,7 +70,7 @@ class TransitionJobViewSet(RalphReadOnlyAPIViewSet):
 class TransitionsHistoryViewSet(RalphReadOnlyAPIViewSet):
     queryset = TransitionsHistory.objects.all()
     serializer_class = TransitionsHistorySerializer
-    filter_fields = ['created', 'modified', 'transition_name', 'source', 'target', 'object_id']
+    filter_fields = ['object_id']
 
 
 class TransitionModelViewSet(RalphReadOnlyAPIViewSet):
