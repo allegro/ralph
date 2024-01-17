267e5e39bbfd066ad8b9c367dc0f6db70089ca89
awieckowski <aleksander.wieckowski@allegro.pl>
2024-01-17 10:19:21 +0100

    Additional fields in data center assets list view and bulk edit

--- src/ralph/data_center/admin.py
+++ src/ralph/data_center/admin.py
@@ -411,14 +411,18 @@ class DataCenterAssetAdmin(
         'service_env',
         'configuration_path',
         'scan_status',
-        'scm_status_check'
+        'scm_status_check',
+        'property_of',
+        'order_no',
+        'remarks'
     ]
     multiadd_summary_fields = list_display + ['rack']
     one_of_mulitvalue_required = ['sn', 'barcode']
     bulk_edit_list = [
         'hostname', 'status', 'barcode', 'model', 'sn', 'invoice_date',
         'invoice_no', 'rack', 'orientation', 'position', 'slot_no', 'price',
-        'provider', 'service_env', 'configuration_path', 'tags', 'start_usage'
+        'provider', 'service_env', 'configuration_path', 'tags', 'start_usage',
+        'depreciation_end_date', 'depreciation_rate', 'order_no'
     ]
     bulk_edit_no_fillable = ['barcode', 'sn']
     search_fields = [
