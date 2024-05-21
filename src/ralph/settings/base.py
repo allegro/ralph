7d9d4d8de065e3407f6247f6b6f2441460bf1f85
Paweł Szulc <pawel.szulc@allegro.com>
2024-05-21 12:28:30 +0200

    Fix IPNetwork filtering

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'django_rq',
     'import_export',
     'mptt',
