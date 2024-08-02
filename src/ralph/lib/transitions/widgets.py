7c9aa870882dd5fec47c424de27023bfa59810f4
Paweł Szulc <pawel.szulc@allegro.com>
2024-08-02 08:15:14 +0200

    Fix checkboxes

---
+++ src/ralph/lib/transitions/widgets.py
@@ -0,0 +1,21 @@
+from django.forms.widgets import ChoiceInput, CheckboxFieldRenderer, CheckboxSelectMultiple
+
+
+class _CheckboxQuerySetChoiceInput(ChoiceInput):
+    input_type = 'checkbox'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.value = set(str(v.id) for v in self.value)
+
+    def is_checked(self):
+        return self.choice_value in self.value
+
+
+class _QuerySetCheckboxRenderer(CheckboxFieldRenderer):
+    choice_input_class = _CheckboxQuerySetChoiceInput
+
+
+class ActionSelectWidget(CheckboxSelectMultiple):
+    renderer = _QuerySetCheckboxRenderer
+
