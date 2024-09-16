976dd5dc78309fb1622245868ee779044f554669
Olga Matyla <olga.matyla@allegro.com>
2024-09-16 14:06:56 +0200

    Fix user permissions form

--- src/ralph/admin/widgets.py
+++ src/ralph/admin/widgets.py
@@ -77,9 +77,7 @@ class PermissionsSelectWidget(forms.Widget):
         attr_value = ','.join(map(str, value or []))
         if not attrs:
             attrs = {}
-        final_attrs = self.build_attrs(
-            attrs, extra_attrs={name: attr_value}
-        )
+        final_attrs = self.build_attrs(attrs, {'type': 'hidden', 'name': name, 'value': attr_value})
         return mark_safe(
             '<a class="expand action-expand">Expand all</a>'
             '<ul class="accordion" data-multi="{}" data-accordion>{}</ul>'
