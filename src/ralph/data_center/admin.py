0af3060d6a24910721f1e57311530fc722339cab
Paweł Szulc <pawel.szulc@allegro.com>
2024-09-20 16:13:02 +0200

    Remove unused inline

--- src/ralph/data_center/admin.py
+++ src/ralph/data_center/admin.py
@@ -269,22 +269,6 @@ class DataCenterAdmin(RalphAdmin):
                    'shortcut']
 
 
-class NetworkTerminatorReadOnlyInline(RalphTabularM2MInline):
-    model = Network
-    extra = 0
-    show_change_link = True
-    verbose_name_plural = _('Terminators of')
-    fields = [
-        'name', 'address',
-    ]
-
-    def get_readonly_fields(self, request, obj=None):
-        return self.get_fields(request, obj)
-
-    def has_add_permission(self, request):
-        return False
-
-
 class DataCenterAssetNetworkView(NetworkWithTerminatorsView):
     pass
 
