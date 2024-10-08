77ba36c136d76e309144830aaf10bbf6aec8b6b0
Paweł Szulc <pawel.szulc@allegro.com>
2024-10-08 12:21:26 +0200

    Revert change in transition serializer

--- src/ralph/lib/transitions/api/serializers.py
+++ src/ralph/lib/transitions/api/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 
 from ralph.api import RalphAPISerializer
+from ralph.lib.transitions.conf import TRANSITION_ORIGINAL_STATUS
 from ralph.lib.transitions.models import (
     Action,
     Transition,
@@ -35,7 +36,21 @@ class TransitionSerializer(RalphAPISerializer):
         fields = "__all__"
 
     def get_source(self, obj):
-        return [source for source in obj.source]
+        # It gets all possible values for the field and not all source values.
+        # But I'm not sure fixing it won't break something else.
+        choices = obj.model.content_type.model_class()._meta.get_field(
+            obj.model.field_name
+        ).choices
+
+        return [i[1] for i in choices]
+
+    def get_target(self, obj):
+        choices = obj.model.content_type.model_class()._meta.get_field(
+            obj.model.field_name
+        ).choices
+        if obj.target == str(TRANSITION_ORIGINAL_STATUS[0]):
+            return TRANSITION_ORIGINAL_STATUS[1]
+        return [i[1] for i in choices if str(i[0]) == obj.target][0]
 
 
 class TransitionJobSerializer(RalphAPISerializer):
