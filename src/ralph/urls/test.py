d706e317e33352bb72d2d822def914630454d687
Olga Matyla <olga.matyla@allegro.com>
2024-10-16 14:14:00 +0200

    Try to fix isort

--- src/ralph/urls/test.py
+++ src/ralph/urls/test.py
@@ -4,9 +4,7 @@ from django.conf.urls import include, url
 from ralph.api.tests import api as ralph_api
 from ralph.lib.custom_fields.tests import urls as custom_fields_tests_urls
 from ralph.lib.permissions.tests import api as lib_api
-from ralph.lib.permissions.tests.test_permission_view import (
-    urls as perm_view_url,
-)  # noqa
+from ralph.lib.permissions.tests.test_permission_view import urls as perm_view_url
 from ralph.urls.base import urlpatterns as base_urlpatterns
 
 
