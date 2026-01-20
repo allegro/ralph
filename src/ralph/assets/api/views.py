233811e128ff504fab11829e27cbcc82332885df
Olga Matyla <73503512+matyldv@users.noreply.github.com>
2026-01-20 11:44:14 +0100

    Add additional filters for base-objects endpoint (#3923)
    * Add additional filters for base-objects endpoint
    
    * Update test
    
    * Change filter to deletion_check

--- src/ralph/assets/api/views.py
+++ src/ralph/assets/api/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import django_filters
-from django.db.models import Prefetch
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Prefetch, Q
 from django_filters import Filter
 from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.permissions import SAFE_METHODS
@@ -17,8 +18,10 @@ from ralph.data_center.models import Cluster, DataCenterAsset
 from ralph.lib.api.utils import renderer_classes_without_form
 from ralph.lib.visibility_scope.filters import visibility_scope_filter
 from ralph.licences.api import BaseObjectLicenceViewSet
-from ralph.licences.models import BaseObjectLicence
+from ralph.licences.models import BaseObjectLicence, Licence
 from ralph.networks.models import IPAddress
+from ralph.supports.models import Support
+from ralph.ssl_certificates.models import SSLCertificate
 from ralph.virtual.models import CloudHost, VirtualServer
 
 
@@ -104,8 +107,23 @@ class AssetModelViewSet(RalphAPIViewSet):
 
 
 class BaseObjectFilterSet(NetworkableObjectFilters):
+    deletion_check = django_filters.DateFilter(method="filter_deletion_check")
+
     class Meta(NetworkableObjectFilters.Meta):
         model = models.BaseObject
+
+    def filter_deletion_check(self, queryset, name, value):
+        licence_ct = ContentType.objects.get_for_model(Licence)
+        support_ct = ContentType.objects.get_for_model(Support)
+        ssl_ct = ContentType.objects.get_for_model(SSLCertificate)
+        data_center_asset_ct = ContentType.objects.get_for_model(DataCenterAsset)
+
+        return queryset.filter(
+            Q(content_type=data_center_asset_ct)
+            | Q(content_type=licence_ct, licence__valid_thru__gte=value)
+            | Q(content_type=support_ct, support__date_to__gte=value)
+            | Q(content_type=ssl_ct, sslcertificate__date_to__gte=value)
+        )
 
 
 base_object_descendant_prefetch_related = [
