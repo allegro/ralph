41de2296def589ae21dc8220d25ba2595d211eb4
Paweł Szulc <pawel.szulc@allegro.com>
2026-08-12 14:55:30 +0200

    Record missing history

--- src/ralph/virtual/management/commands/openstack_sync.py
+++ src/ralph/virtual/management/commands/openstack_sync.py
@@ -136,7 +136,7 @@ class RalphClient:
 
     def match_physical_and_cloud_hosts(self):
         """Connect CloudHosts and DC assets according to data from Ironic."""
-
+        os_conf: dict
         for os_conf in settings.OPENSTACK_INSTANCES:
             if (
                 os_conf.get("provider", DEFAULT_OPENSTACK_PROVIDER_NAME)
@@ -187,7 +187,7 @@ class RalphClient:
                 logger.info("Cloud host %s matched DC asset %s.", host.id, asset.id)
                 if host.hypervisor != asset:
                     host.hypervisor = asset
-                    host.save()
+                    self._save_object(host, "Modify hypervisor (_match_nodes_to_hosts)")
 
     @lru_cache()
     def _get_flavor_objects(self):
